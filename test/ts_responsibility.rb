ac75544aea6e499c847f3c224e9efedef8990104
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-03-04 20:31:12 -0800

    added a few more tests, made failure output for some tests more intuitive

--- test/ts_responsibility.rb
+++ test/ts_responsibility.rb
@@ -3,13 +3,14 @@ require 'test/unit'
 class TS_Responsibility < Test::Unit::TestCase
   def setup
     @server = StorageServer.new
-    @evensfunc = UserFunction.new(:lang=> Language::RUBY, :func=>"def evens(val) val.to_i%2==0 end")
+    @evensfunc = UserFunction.new(:lang=> Language::RUBY, :func=>"Proc.new {|val| val.to_i%2==0}")
+    @favlist = ["1","18","45","32","22"]
   end
-  
+
   def teardown
     @server.stop
   end
-  
+
   def test_responsibility_limit_nil 
     policy = RecordSet.new(
       :type =>RecordSetType::RANGE,
@@ -21,9 +22,8 @@ class TS_Responsibility < Test::Unit::TestCase
     end
     
   end
-  
-  def test_get_responsibility
-      
+
+  def test_get_responsibility      
       # try a range responsibility
       policy = RecordSet.new(
         :type =>RecordSetType::RANGE,
@@ -47,11 +47,9 @@ class TS_Responsibility < Test::Unit::TestCase
         
       server_rs = @server.get_responsibility_policy("getresp2")  
       assert_equal(RecordSetType::KEY_FUNC, server_rs.type)
-      assert_equal("def evens(val) val.to_i%2==0 end",server_rs.func.func)
-
-      
+      assert_equal("Proc.new {|val| val.to_i%2==0}",server_rs.func.func)
   end
-  
+
   def test_range 
     policy = RecordSet.new(
       :type =>RecordSetType::RANGE,
@@ -73,8 +71,12 @@ class TS_Responsibility < Test::Unit::TestCase
     end
     
   end
-  
-  def test_list 
+
+  def test_list
+    # TODO
+  end
+
+  def test_set 
     desired = RecordSet.new(
       :type =>RecordSetType::RANGE,
       :range => RangeSet.new(:start_key=>"05",:end_key=>"09",:start_limit => nil,:end_limit => nil)
@@ -92,16 +94,6 @@ class TS_Responsibility < Test::Unit::TestCase
     record_list = @server.get_set("listresp",desired) # request a set of records
     
     assert_equal((5..8).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}, record_list)
-    
-    assert_equal(4,record_list.size,"# records expected")
-  
-    # check get back all *existing* values in the desired range
-    assert(!list_contains(record_list,Record.new(:key => "04", :value => "val04")),"list has extra value") # not desired
-    (5..8).each do |i| # got all desired that exist
-      assert(list_contains(record_list,Record.new(:key => "0#{i}", :value => "val0#{i}")),"list missing value")
-    end
-    assert(!list_contains(record_list,Record.new(:key => "09", :value => "val09")),"list has extra value") # desired, but doesn't exist
-    assert(!list_contains(record_list,Record.new(:key => "10", :value => "val10")),"list has extra value") # not desired
 
     # now check if part of desired records are out of this server's responsibility
     # weird semantics, but since server is not responsibility for ALL the keys...
@@ -117,35 +109,21 @@ class TS_Responsibility < Test::Unit::TestCase
     assert_raise(NotResponsible, "asked for set that's partly out of server's responsibility") do
       record_list = @server.get_set("listresp2",desired)
     end
+  end
 
-  end
-  
-  
-  def list_contains(list,record) # helper function for checking test_list
-    list.each do |rec|
-      if rec.key.eql?(record.key) and rec.value.eql?(record.value)
-        return true
-      end
-    end
-    return false
-  end
-  
   def test_user_function
     policy = RecordSet.new(
       :type =>RecordSetType::KEY_FUNC,
       :func =>@evensfunc
-      )
-      
+      ) 
     @server.set_responsibility_policy("funcresp", policy)
   
     assert(@server.put("funcresp", Record.new(:key => "04", :value => "val04"))) # inside    
     assert_raise(NotResponsible) do     # outside
       @server.put("funcresp", Record.new(:key => "01", :value => "val01"))
     end
-  
   end
-  
-  
+
   def test_right_responsibility
     policy = RecordSet.new(
       :type =>RecordSetType::RANGE,
--- test/ts_sets.rb
+++ test/ts_sets.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 class TS_Sets < Test::Unit::TestCase
   def setup
     @server = StorageServer.new
-    @evensfunc = UserFunction.new(:lang=> Language::RUBY, :func=>"def evens(val) val.to_i%2==0 end")
+    @evensfunc = UserFunction.new(:lang=> Language::RUBY, :func=>"Proc.new {|val| val.to_i%2==0}")
   end
   
   def teardown
@@ -21,18 +21,9 @@ class TS_Sets < Test::Unit::TestCase
       )
     record_list = @server.get_set("rangeset",desired)
     
-    assert_equal(4,record_list.size,"# records expected")
-  
-    # check get back all *existing* values in the desired range
-    assert(!list_contains(record_list,Record.new(:key => "04", :value => "val04")),"list has extra value") # not desired
-    (5..8).each do |i| # got all desired that exist
-      assert(list_contains(record_list,Record.new(:key => "0#{i}", :value => "val0#{i}")),"list missing value")
-    end
-    assert(!list_contains(record_list,Record.new(:key => "09", :value => "val09")),"list has extra value") # desired, but doesn't exist
-    assert(!list_contains(record_list,Record.new(:key => "10", :value => "val10")),"list has extra value") # not desired
-
+    assert_equal((5..8).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}, record_list)
   end
-  
+
   def test_empty_range
     (1..8).each do |i| # set some values
       @server.put("emptyrange", Record.new(:key => "0#{i}", :value => "val0#{i}"))
@@ -44,13 +35,12 @@ class TS_Sets < Test::Unit::TestCase
       :range => RangeSet.new(:start_key=>"09",:end_key=>"11",:start_limit => nil,:end_limit => nil)
       )
     record_list = @server.get_set("emptyrange",desired)
-    assert_equal(0,record_list.size,"# records expected")
+    assert_equal([], record_list)
     
     # test both sides
     # ?
-    
   end
-  
+
   def test_trivial_range 
     (1..8).each do |i| # set some values
       @server.put("trivialrange", Record.new(:key => "0#{i}", :value => "val0#{i}"))
@@ -62,11 +52,9 @@ class TS_Sets < Test::Unit::TestCase
       :range => RangeSet.new(:start_key=>"07",:end_key=>"07",:start_limit => nil,:end_limit => nil)
       )
     record_list = @server.get_set("trivialrange",desired)
-    assert_equal(1,record_list.size,"# records expected")
-    assert(list_contains(record_list,Record.new(:key => "08", :value => "val08")),"list missing value")
-    
+    assert_equal([Record.new(:key => "07", :value => "val07")], record_list)  
   end
-  
+
   def test_range_limit
     (0..9).each do |i| # set some values
       @server.put("rangelimit", Record.new(:key => "0#{i}", :value => "val0#{i}"))
@@ -78,12 +66,7 @@ class TS_Sets < Test::Unit::TestCase
       :range => RangeSet.new(:start_key=>"01",:end_key=>"08",:start_limit => 0,:end_limit => 3)
       )
     record_list = @server.get_set("rangelimit",desired)
-    (1..4).each do |i| # got all desired in limit
-      assert(list_contains(record_list,Record.new(:key => "0#{i}", :value => "val0#{i}")),"list missing value")
-    end
-    (5..8).each do |i| # didn't get keys not in limit
-      assert(!list_contains(record_list,Record.new(:key => "0#{i}", :value => "val0#{i}")),"extra value")
-    end
+    assert_equal((1..4).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}, record_list)
     
    # should return last two values from values matching the range
     desired = RecordSet.new(
@@ -91,17 +74,9 @@ class TS_Sets < Test::Unit::TestCase
       :range => RangeSet.new(:start_key=>"01",:end_key=>"08",:start_limit => 6,:end_limit => 10)
       )
     record_list = @server.get_set("rangelimit",desired)
-    (7..8).each do |i| # got all desired in limit
-      assert(list_contains(record_list,Record.new(:key => "0#{i}", :value => "val0#{i}")),"list missing value")
-    end
-    (1..6).each do |i| # didn't get keys not in limit
-      assert(!list_contains(record_list,Record.new(:key => "0#{i}", :value => "val0#{i}")),"extra value")
-    end
-    # make sure didn't extend beyond range definition
-    assert(!list_contains(record_list,Record.new(:key => "09", :value => "val09")),"extra value")
-  
+    assert_equal((7..8).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}, record_list)
   end
-  
+
   def test_user_function
     (1..8).each do |i| # set some values
       @server.put("userrange", Record.new(:key => "0#{i}", :value => "val0#{i}"))
@@ -113,17 +88,29 @@ class TS_Sets < Test::Unit::TestCase
       :func => @evensfunc
       )
     record_list = @server.get_set("userrange",desired)
-    assert_equal(4,record_list.size,"# records expected")
-    
-    (1..8).each do |i|
-      if i%2==0
-        assert(list_contains(record_list,Record.new(:key => "0#{i}", :value => "val0#{i}")),"list missing value")
-      else
-        assert(!list_contains(record_list,Record.new(:key => "0#{i}", :value => "val0#{i}")),"extra value")
-      end
-    end
+    assert_equal([2,4,6,8].map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}, record_list)
   end
-  
+
+  def test_nil_range_ends
+    (1..8).each do |i| # set some values
+      @server.put("nilrange", Record.new(:key => "0#{i}", :value => "val0#{i}"))
+    end
+    
+    desired = RecordSet.new(
+      :type =>RecordSetType::RANGE,
+      :range => RangeSet.new(:start_key=>nil,:end_key=>"05",:start_limit => nil,:end_limit => nil)
+      )
+    record_list = @server.get_set("nilrange",desired) # get everything up to 05
+    assert_equal((1..5).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}, record_list)  
+    
+    desired = RecordSet.new(
+      :type =>RecordSetType::RANGE,
+      :range => RangeSet.new(:start_key=>"02",:end_key=>nil,:start_limit => nil,:end_limit => nil)
+      )
+    record_list = @server.get_set("nilrange",desired) # get from 02 to end
+    assert_equal((2..8).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}, record_list)
+  end
+
   def test_invalid_description
     desired = RecordSet.new(
       :type =>RecordSetType::RANGE,
@@ -134,23 +121,6 @@ class TS_Sets < Test::Unit::TestCase
       record_list = @server.get_set("invalidset",desired)
     end
     
-    desired = RecordSet.new(
-      :type =>RecordSetType::RANGE,
-      :range => RangeSet.new(:start_key=>nil,:end_key=>"10",:start_limit => nil,:end_limit => nil)
-      )
-      
-    assert_raise(InvalidSetDescription,"start key is nil") do
-      record_list = @server.get_set("invalidset",desired)
-    end
-    
-    desired = RecordSet.new(
-      :type =>RecordSetType::RANGE,
-      :range => RangeSet.new(:start_key=>"1",:end_key=>nil,:start_limit => nil,:end_limit => nil)
-      )
-      
-    assert_raise(InvalidSetDescription,"end key is nil") do
-      record_list = @server.get_set("invalidset",desired)
-    end
     
     desired = RecordSet.new(
       :type =>RecordSetType::RANGE,
@@ -169,17 +139,7 @@ class TS_Sets < Test::Unit::TestCase
       
     assert_raise(InvalidSetDescription,"can't specify range and func in RecordSet") do
       record_list = @server.get_set("invalidset",desired)
-    end
-    
+    end    
   end
-  
-  def list_contains(list,record) # helper function for checking list contents
-    list.each do |rec|
-      if rec.key.eql?(record.key) and rec.value.eql?(record.value)
-        return true
-      end
-    end
-    return false
-  end
-  
+
 end
