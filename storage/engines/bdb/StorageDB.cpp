d6f6fd8030401c83b3f61a86426d4ee7172a6df6
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-03-07 17:56:44 -0800

    New copies of thrift will be in the apache namespace

--- storage/engines/bdb/StorageDB.cpp
+++ storage/engines/bdb/StorageDB.cpp
@@ -20,10 +20,10 @@
 #include "gen-cpp/Storage.h"
 
 using namespace std;
-using namespace facebook::thrift;
-using namespace facebook::thrift::protocol;
-using namespace facebook::thrift::transport;
-using namespace facebook::thrift::server;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+using namespace apache::thrift::server;
 
 using namespace boost;
 
