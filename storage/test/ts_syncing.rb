5c058d3f5410ddc30fc3079a93e0d171c193d88b
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-03-09 20:08:54 -0700

    fixed typo, changed conflict function to be a merge of arrays

--- storage/test/ts_syncing.rb
+++ storage/test/ts_syncing.rb
@@ -8,8 +8,8 @@ class TS_Syncing < Test::Unit::TestCase
     @server2 = $ENGINE.new
     
     @policy_greater = ConflictPolicy.new(:type=> ConflictPolicyType::GREATER)
-    @policy_evens= ConflictPolicy.new(:type=> ConflictPolicyType::FUNC, 
-    :func=>"Proc.new {|val1,val2| if val1.to_i%2==0 then return val1 else return val2 end}")
+    @policy_merge= ConflictPolicy.new(:type=> ConflictPolicyType::FUNC, 
+    :func=>"Proc.new {|val1,val2| return [val1, val2].flatten.uniq.sort}")
   end
   
   def teardown
@@ -90,7 +90,7 @@ class TS_Syncing < Test::Unit::TestCase
       )
     record_list1 = @server1.get_set("syncgreater",desired) # vals 01,02,03,04,06,07,08,09
     assert_equal(
-      (1..4).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}.concat((5..9).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i+1}")}), 
+      (1..4).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i}")}.concat((5..8).map{|i| Record.new(:key => "0#{i}", :value => "val0#{i+1}")}), 
       record_list1)
     
     record_list2 = @server2.get_set("syncgreater",desired) # vals 02,03,04,05,05,06,08,09
@@ -102,10 +102,13 @@ class TS_Syncing < Test::Unit::TestCase
   
   def test_sync_user_function
     (1..8).each do |i| # set some values for one server
-      @server1.put("syncfunc", Record.new(:key => "0#{i}", :value => "0#{i}"))
+      @server1.put("syncfunc", Record.new(:key => "0#{i}", :value => ["0#{i}"]))
     end
-    (1..8).each do |i| # set some values for other server
-      @server2.put("syncfunc", Record.new(:key => "0#{i}", :value => "0#{i*2}"))
+    (1..4).each do |i| # set some values for other server
+      @server2.put("syncfunc", Record.new(:key => "0#{i}", :value => ["0#{i*2}"]))
+    end
+    (5..8).each do |i| # set some values for other server
+      @server2.put("syncfunc", Record.new(:key => "0#{i}", :value => ["#{i*2}"]))
     end
     
     # sync some of the values from server1 and server2
@@ -113,20 +116,22 @@ class TS_Syncing < Test::Unit::TestCase
       :type =>RecordSetType::RANGE,
       :range => RangeSet.new(:start_key=>"05",:end_key=>"08",:start_limit => nil,:end_limit => nil)
       )
-    @server1.sync_set("syncfunc", to_sync,@server2.host, @policy_evens)
+    @server1.sync_set("syncfunc", to_sync,@server2.host, @policy_merge)
   
     # try to get values from both servers
     desired = RecordSet.new(
       :type =>RecordSetType::RANGE,
-      :range => RangeSet.new(:start_key=>"01",:end_key=>"09",:start_limit => nil,:end_limit => nil)
+      :range => RangeSet.new(:start_key=>"01",:end_key=>"08",:start_limit => nil,:end_limit => nil)
       )
-    record_list1 = @server1.get_set("syncfunc",desired) # vals 01,02,03,04,010,012,014,018
+    record_list1 = @server1.get_set("syncfunc",desired) # vals [01],[02],[03],[04],[05,10],[06,12],[07,14],[08,16]
     assert_equal(
-      (1..4).map{|i| Record.new(:key => "0#{i}", :value => "0#{i}")}.concat((5..8).map{|i| Record.new(:key => "0#{i}", :value => "0#{i*2}")}), 
+      (1..4).map{|i| Record.new(:key => "0#{i}", :value => ["0#{i}"])}.concat((5..8).map{|i| Record.new(:key => "0#{i}", :value => ["0#{i}","#{i*2}"])}), 
       record_list1)
   
-    record_list2 = @server2.get_set("syncfunc",desired) # vals 02,04,06,08,010,012,014,018
-    assert_equal((1..8).map{|i| Record.new(:key => "0#{i}", :value => "0#{i*2}")},record_list2)
+    record_list2 = @server2.get_set("syncfunc",desired) # vals [02],[04],[06],[08],[05,10],[06,12],[07,14],[08,16]
+    assert_equal(
+      (1..4).map{|i| Record.new(:key => "0#{i}", :value => ["0#{i*2}"])}.concat((5..8).map{|i| Record.new(:key => "0#{i}", :value => ["0#{i}","#{i*2}"])}), 
+      record_list2)
   end
   
 end
