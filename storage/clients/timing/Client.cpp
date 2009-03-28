667533dd1e2bf7091c4ffb5b277e9ffcc03fbbf1
jesse trutna <jtrutna@irisbiotech.com>
2009-03-28 00:07:08 -0700

    Initial (very non-robust) implementation of a get/put client.  Includes loverly mixing of C & C++.

--- storage/clients/timing/Client.cpp
+++ storage/clients/timing/Client.cpp
@@ -6,7 +6,7 @@
 #include <transport/TSocket.h>
 #include <transport/TTransportUtils.h>
 
-#include "../gen-cpp/Storage.h"
+#include "gen-cpp/Storage.h"
 
 #include <iostream>
 
@@ -20,128 +20,68 @@ using namespace SCADS;
 using namespace boost;
 
 int main(int argc, char** argv) {
-  shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
+	char *host;
+	int port;
+	int c;
+	while ((c = getopt(argc, argv, "h:p:")) != -1) {
+		switch (c) {
+			case 'h':
+				host = optarg;
+				break;
+			case 'p':
+				port = atoi(optarg);
+				break;
+			case '?':
+				if (optopt == 'h') {
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				} else if (isprint (optopt)) {
+					fprintf (stderr, "Unknown option `-%c`.\n", optopt);
+				} else {
+					fprintf (stderr, "Unknown option character `\\x%x`.\n", optopt);
+				}
+				return 1;
+			default:
+				abort();
+		}
+	}
+
+	//defaults
+	host = "localhost";
+	port = 9090;
+		
+  shared_ptr<TTransport> socket(new TSocket(host, port));
   shared_ptr<TTransport> transport(new TBufferedTransport(socket));
   shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
   StorageClient client(protocol);
-  
-  int op = -1; // 0 = get, 1 = put, 2 = getall, 3 = getrange
 
-  if (argc < 2) {
-    fprintf(stderr,"Too few args.  Need [get|getall|getrange|getruby|put] [key] [value|startkey] [endkey]\n");
-    exit(1);
-  }
+	//TODO: Remove magic numbers (!including in scanf length limiters!)
+	char cmd[16]; //get, put
+	char table[512];
+	char key[512];
+	char value[4096];
 
-  if (!strcmp(argv[1],"get"))
-    op = 0;
-  if (!strcmp(argv[1],"put"))
-    op = 1;
-  if (!strcmp(argv[1],"getall"))
-    op = 2;
-  if (!strcmp(argv[1],"getrange"))
-    op = 3;
-  if (!strcmp(argv[1],"getruby"))
-    op = 4;
-
-  if (op == -1) {
-    fprintf(stderr,"Invalid op: %s\n",argv[1]);
-    exit(1);
-  }
-
-  if (op == 1 && argc < 4) {
-    fprintf(stderr,"Put needs arg to put\n");
-    exit(1);
-  }
-
-  if (op == 3 && argc < 4) {
-    fprintf(stderr,"getrange needs start and end keys\n");
-    exit(1);
-  }
-
-  if (op == 4 && argc < 3) {
-    fprintf(stderr,"getruby needs ruby string\n");
-    exit(1);
-  }
-
-  Record r;
-  if (op == 1) {
-    r.key = string(argv[2]);
-    r.value = string(argv[3]);
-  }
-
-  try {
-    transport->open();
-
-    switch (op) {
-    case 0:
-      client.get(r,"my_NS",string(argv[2]));
-      cout << "kset: "<<r.__isset.key<<" vset: "<<r.__isset.value<<endl;
-      if (!r.__isset.value) 
-	cout << "key "<<argv[2]<<" not found"<<endl;
-      else
-	cout << "Key:\t"<<argv[2]<<"\nValue:\t"<<r.value<<endl;
-      break;
-    case 1:
-      cout << "Putting Key: "<<r.key<<" Value: "<<r.value<<endl;
-      r.__isset.value = true;
-      if (client.put("my_NS",r))
-	cout << "Put okay"<<endl;
-      else
-	cout << "Put failed"<<endl;
-      break;
-    case 2: {
-      std::vector<Record> vals;
-      RecordSet rs;
-      rs.type = RST_ALL;
-      client.get_set(vals,"my_NS",rs);
-      std::vector<Record>::iterator it;
-      for (it = vals.begin();it!=vals.end();it++) 
-	cout << "\nKey:\t"<<((*it).key)<<"\nValue:\t"<<((*it).value)<<endl;	
-    }
-      break;
-    case 3: {
-      std::vector<Record> vals;
-      RecordSet rs;
-      rs.type = RST_RANGE;
-      RangeSet range;
-      range.offset = NULL;
-      range.limit = NULL;
-      range.start_key = string(argv[2]);
-      range.end_key = string(argv[3]);
-      rs.range = range;
-      client.get_set(vals,"my_NS",rs);
-      std::vector<Record>::iterator it;
-      for (it = vals.begin();it!=vals.end();it++) 
-	cout << "\nKey:\t"<<((*it).key)<<"\nValue:\t"<<((*it).value)<<endl;	
-    }
-      break;
-    case 4: {
-      std::vector<Record> vals;
-      RecordSet rs;
-      rs.type = RST_KEY_FUNC;
-      UserFunction uf;
-      uf.lang = LANG_RUBY;
-      uf.func = string(argv[2]);
-      rs.func = uf;
-      cout << "Passing ruby string: "<<argv[2]<<endl;
-      client.get_set(vals,"my_NS",rs);
-      std::vector<Record>::iterator it;
-      for (it = vals.begin();it!=vals.end();it++) 
-	cout << "\nKey:\t"<<((*it).key)<<"\nValue:\t"<<((*it).value)<<endl;	
-    }
-      break;
-    default:
-      cout << "Nothing to do"<<endl;
-    }
-
-
-    transport->close();
-  } 
-  catch (InvalidSetDescription &tx) {
-    printf("INVALID SET DESCRIPTION: %s\n", tx.info.c_str());
-  }
-  catch (TException &tx) {
-    printf("ERROR: %s\n", tx.what());
-  }
-
+	Record r;
+	
+	transport->open();
+	
+	while (EOF != scanf("%15s", cmd)) {
+		if (!strcmp(cmd,"get")) {
+			scanf("%511s %511s", table, key);
+			printf("get\t%s\t%s\t=>\t", table, key);
+			client.get(r, table, key);
+			cout << r.value << "\n";
+		} else if (!strcmp(cmd,"put")) {
+			scanf("%511s %511s %4095s", table, key, value);
+			r.key = key;
+			r.value = value;
+			r.__isset.value = true;
+			printf("put\t%s\t%s\t%s\t=>\t", table, key, value);
+			cout << client.put(table, r) << "\n";
+		} else if (!strcmp(cmd, "quit") or !strcmp(cmd, "exit")) {
+			exit(0);
+	  } else {
+			printf("Error: Unrecognized cmd %s\n", cmd);
+			exit(1);
+		}
+	}
 }
