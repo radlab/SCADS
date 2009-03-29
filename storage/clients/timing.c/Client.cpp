2671bae3f5173e123e3ed77b9d9a8ffd2a667816
jesse trutna <jtrutna@irisbiotech.com>
2009-03-29 03:06:02 -0700

    Added logging of start request time + a bit of cleanup.

--- storage/clients/timing.c/Client.cpp
+++ storage/clients/timing.c/Client.cpp
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/time.h>
-
 #include <protocol/TBinaryProtocol.h>
 #include <transport/TSocket.h>
 #include <transport/TTransportUtils.h>
-
 #include "gen-cpp/Storage.h"
-
 #include <iostream>
 
 using namespace std;
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
-
 using namespace SCADS;
-
 using namespace boost;
+
+#define start_timer() gettimeofday(&start_time,NULL)
+#define end_timer() { \
+    gettimeofday(&end_time,NULL); \
+    timersub(&end_time,&start_time,&diff_time); \
+    printf("%ld.%.6ld\t%ld.%.6ld", start_time.tv_sec, start_time.tv_usec, diff_time.tv_sec, diff_time.tv_usec); \
+  }
 
 int main(int argc, char** argv) {
 	char *host;
@@ -49,10 +51,10 @@ int main(int argc, char** argv) {
 	host = "localhost";
 	port = 9090;
 		
-  shared_ptr<TTransport> socket(new TSocket(host, port));
-  shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-  shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-  StorageClient client(protocol);
+	shared_ptr<TTransport> socket(new TSocket(host, port));
+	shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+	shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+	StorageClient client(protocol);
 
 	//TODO: Remove magic numbers (!including in scanf length limiters!)
 	char cmd[16]; //get, put
@@ -68,22 +70,20 @@ int main(int argc, char** argv) {
 		success = -1;
 		if (!strcmp(cmd,"get")) {
 			scanf("%511s %511s", table, key);
-			gettimeofday(&start_time,NULL);
+			start_timer();
 			client.get(r, table, key);
-			gettimeofday(&end_time,NULL);
-			timersub(&end_time,&start_time,&diff_time);
-			printf("%ld.%.6ld\tget\t%s\t%s\t=>\t", diff_time.tv_sec, diff_time.tv_usec, table, key);
+			end_timer();
+			printf("\tget\t%s\t%s\t=>\t", table, key);
 			cout << r.value << "\n";
 		} else if (!strcmp(cmd,"put")) {
 			scanf("%511s %511s %4095s", table, key, value);
 			r.key = key;
 			r.value = value;
 			r.__isset.value = true;	
-			gettimeofday(&start_time,NULL);
+			start_timer();
 			success = client.put(table, r);
-			gettimeofday(&end_time,NULL);
-			timersub(&end_time,&start_time,&diff_time);
-			printf("%ld.%.6ld\tput\t%s\t%s\t%s\t=>\t", diff_time.tv_sec, diff_time.tv_usec, table, key, value);
+			end_timer();
+			printf("\tput\t%s\t%s\t%s\t=>\t", table, key, value);
 			cout << success << "\n";
 		} else if (!strcmp(cmd, "quit") or !strcmp(cmd, "exit")) {
 			transport->close();
