7bd9cb019792646c75b1da70076b36b679518288
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-04-09 17:05:13 -0700

    log vmstats to file and stdout.

--- storage/perf/vmstats.rb
+++ storage/perf/vmstats.rb
@@ -1,11 +1,18 @@
+count = 0
+hostname = `hostname`.chomp
+
+file = File.new("/mnt/vmstats.#{hostname}.csv", "w+")
 stats = IO.popen('vmstat 1')
 stats.readline
 
-puts (["time", "host"] + stats.readline.split(" ")).join(",")
+file.puts((["time", "host"] + stats.readline.split(" ")).join(","))
 
-hostname = `hostname`.chomp
+
 
 while(true)
   line = stats.readline
-  puts ([Time.now.to_i, hostname] + line.split(" ")).join(",") if line =~ /\d/
+  if line =~ /\d/
+    file.puts(([Time.now.to_i, hostname] + line.split(" ")).join(","))
+    puts(([Time.now.to_i, hostname] + line.split(" ")).join(",")) if (count += 1) % 10 == 0
+  end
 end
