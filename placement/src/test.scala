15d9621815fc431144e4f5ec7348ad691b6a5d89
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-04-13 19:44:51 -0700

    fix testing whitespace

--- placement/src/test.scala
+++ placement/src/test.scala
@@ -9,32 +9,32 @@ class KeyRangeSuite extends Suite {
 		assert(KeyRange("a",null) + KeyRange("b", "d") == KeyRange("a", null))
 		assert(KeyRange("a","c") + KeyRange(null, "d") == KeyRange(null, "d"))
 		assert(KeyRange("a","c") + KeyRange("b", null) == KeyRange("a", null))
-		
+
 		assert(KeyRange("a","b") + KeyRange("b", "c") == KeyRange("a", "c"))
 		assert(KeyRange("b", "c") + KeyRange("a","b") == KeyRange("a", "c"))
-		
+
 		assert(KeyRange("a", "z") + KeyRange("m","n") == KeyRange("a", "z"))
 		assert(KeyRange("m", "n") + KeyRange("a","z") == KeyRange("a", "z"))
-		
+
 		intercept[NotContiguousException] {
 			KeyRange("a","b") + KeyRange("c", "d")
 		}
-		
+
 		intercept[NotContiguousException] {
 			KeyRange("c","d") + KeyRange("a", "b")
 		}
 	}
-	
+
 	def testSubtraction() {
 		assert(KeyRange("a", "c") - KeyRange("b", "c") == KeyRange("a", "b"))
 		assert(KeyRange("a", "c") - KeyRange("b", "d") == KeyRange("a", "b"))
-		
+
 		assert(KeyRange("b", "d") - KeyRange("a", "c") == KeyRange("c", "d"))
 		assert(KeyRange("b", "d") - KeyRange("b", "c") == KeyRange("c", "d"))
-		
+
 		assert(KeyRange("a", "b") - KeyRange("a", "b") == KeyRange.EmptyRange)
 	}
-	
+
 	def testAnd() {
 		assert((KeyRange("a","c") & KeyRange("b", "d")) == KeyRange("b", "c"))
 		assert((KeyRange("b", "d") & KeyRange("a","c")) == KeyRange("b", "c"))
