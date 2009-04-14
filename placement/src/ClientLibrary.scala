805ff9b56845ebe250fa630789f7d3bf4895ce0c
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-04-13 23:25:18 -0700

    implemented client library, a few confusing errors

--- placement/src/ClientLibrary.scala
+++ placement/src/ClientLibrary.scala
@@ -12,6 +12,8 @@ trait KeySpaceProvider {
 	def refreshKeySpace()
 }
 
+class NonCoveredRangeException extends Exception
+
 abstract class ClientLibrary extends SCADS.ClientLibrary.Iface {
 	def ns_map: Map[String,KeySpace]
 	
@@ -30,14 +32,14 @@ abstract class ClientLibrary extends SCADS.ClientLibrary.Iface {
 }
 
 class ROWAClientLibrary extends ClientLibrary with KeySpaceProvider {
-	var ns_map = new HashMap[String,InefficientKeySpace]
+	var ns_map = new HashMap[String,SimpleKeySpace]
 	var dp_map = new HashMap[String,DataPlacement]
 	
 	/**
 	* Asks key space provider for latest keyspace for the specified namespace.
 	* Updates the local copy's keyspace.
 	*/
-	def getKeySpace(ns: String) = {
+	override def getKeySpace(ns: String) = {
 		val ks = dp_map.get(ns).keySpace
 		ns_map.update(ns,ks)
 	}
@@ -46,7 +48,7 @@ class ROWAClientLibrary extends ClientLibrary with KeySpaceProvider {
 	* Asks key space provider for all known namespaces.
 	* Updates all the keyspaces.
 	*/
-	def refreshKeySpace() = {
+	override def refreshKeySpace() = {
 		ns_map.foreach({ case(ns,ks) => {
 			this.getKeySpace(ns)
 		}})
@@ -82,7 +84,7 @@ class ROWAClientLibrary extends ClientLibrary with KeySpaceProvider {
 
 		// determine which ranges to ask from which nodes
 		// assumes no gaps in range, but someone should tell user if entire range isn't covered
-		val query_nodes = this.get_set_queries(ns_keyspace.lookup(target_range))
+		val query_nodes = this.get_set_queries(ns_keyspace.lookup(target_range),target_range)
 
 		// now do the getting
 		query_nodes.foreach( {case (node,keyrange)=> {
@@ -92,16 +94,45 @@ class ROWAClientLibrary extends ClientLibrary with KeySpaceProvider {
 				records ++= records_subset.iterator()
 			} catch {
 				case e:NotResponsible => {
-					// TODO
+					this.getKeySpace(namespace)
+					val records_subset = node.get_set(namespace,rset)
+					records ++= records_subset.iterator()
 				}
 			}
 		}
 		})
+		// TODO: check for gaps in desired RecordSet, throw exception?
+		
 		java.util.Arrays.asList(records.toArray: _*) // shitty, but convert to java array
 	}
 	
-	private def get_set_queries(nodes: Map[Node, KeyRange]): Map[Node, KeyRange] = {
+	private def get_set_queries(nodes: Map[Node, KeyRange], target_range: KeyRange): HashMap[Node, KeyRange] = {
+		var resultmap = new HashMap[Node, KeyRange]
+		
+		var start = target_range.start
+		val end = target_range.end
+		
+		while (start < end) {
+			val node_tuple = this.find_node_at_start(nodes,start)
+			resultmap += node_tuple._1 -> node_tuple._2
+			start = node_tuple._2.end
+		}
+		resultmap
+	}
 	
+	private def find_node_at_start(nodes: Map[Node,KeyRange], start: String): (Node,KeyRange) = {
+		var potential_nodes = nodes.filter((entry) => entry._2.start <= start) // nodes that start at or before target start
+		var chosen_node = potential_nodes.elements.next // init to first one?
+		var end = chosen_node._2.end
+		
+		potential_nodes.foreach((entry) =>
+			if (entry._2.end > end) {
+				chosen_node = entry
+				end = chosen_node._2.end
+			}
+		)
+		val range_covered = new KeyRange(start,end)
+		(chosen_node._1, range_covered)
 	}
 	
 	/**
