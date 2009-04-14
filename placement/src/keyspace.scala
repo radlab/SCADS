5f860a71effcdff47b579e4cb21349ac04ccaee7
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-04-13 18:58:33 -0700

    Working KeyRanges and Tests

--- placement/src/keyspace.scala
+++ placement/src/keyspace.scala
@@ -1,20 +1,76 @@
-
 case class Node(host: String, port: Int)
 
+class NotContiguousException extends Exception
+
+object KeyRange {
+	val EmptyRange = new KeyRange("", "")
+}
+
 case class KeyRange(start: String, end: String) {
-	def + (that: KeyRange): KeyRange = null
-	def - (that: KeyRange): KeyRange = null
+	if(start != null && end != null)
+		assert(start <= end)
+
+	def + (that: KeyRange): KeyRange = {
+		if((this.start == null || that.start == null) && (this.end == null || that.end == null))
+			new KeyRange(null, null)
+		else if (this.start == null || that.start == null)
+			new KeyRange(null, max(this.end, that.end))
+		else if (this.end == null || that.end == null)
+			new KeyRange(min(this.start, that.start), null)
+		else if((this.end >= that.start) && (this.end < that.end) && (this.start < that.start))
+			new KeyRange(this.start, that.end)
+		else if((that.end >= this.start) && (that.end < this.end) && (that.start < this.start))
+			new KeyRange(that.start, this.end)
+		else if(this.start >= that.start && this.end <= that.end)
+			that
+		else if(that.start >= this.start && that.end <= this.end)
+			this
+		else
+			throw new NotContiguousException
+	}
+	
+	def - (that: KeyRange): KeyRange = {
+		if(this.end > that.start && this.end <= that.end && this.start < that.start)
+			new KeyRange(this.start, that.start)
+		else if(this.start >= that.start && this.start < that.end && this.end > that.end)
+			new KeyRange(that.end, this.end)
+		else if(this.end < that.start || this.start > that.end)
+			this
+		else if (this.start == this.start && this.end == this.end)
+			KeyRange.EmptyRange
+		else
+			throw new NotContiguousException
+	}
+	
+	def & (that: KeyRange): KeyRange = {
+		if((this.start == null || that.start == null) && (this.end == null || that.end == null))
+			new KeyRange(null, null)
+		else if (this.start == null || that.start == null)
+			new KeyRange(coalesce(this.start, that.start), min(this.end, that.end))
+		else if (this.end == null || that.end == null)
+			new KeyRange(max(this.start, that.start), coalesce(this.end, that.end))
+		else if((this.end >= that.start) && (this.end < that.end) && (this.start < that.start))
+			new KeyRange(that.start, this.end)
+		else if((that.end >= this.start) && (that.end < this.end) && (that.start < this.start))
+			new KeyRange(this.start, that.end)
+		else if(this.start >= that.start && this.end <= that.end)
+			this
+		else if(that.start >= this.start && that.end <= this.end)
+			that
+		else
+			KeyRange.EmptyRange
+	}
+
+	private def min(a: String, b: String) = if(a < b) a else b
+	private def max(a: String, b: String) = if(a > b) a else b
+	private def coalesce(a: String, b:String) = if(a != null) a else b
 }
 
 abstract class KeySpace {
 	def add(node: Node, range: KeyRange)
 	def remove(node: Node, range: KeyRange)
-	
+
 	def lookup(key: String):Set[Node]
 	def lookup(range: KeyRange): Map[Node, KeyRange]
 	def coverage: Set[KeyRange]
-}
-
-class InefficientKeySpace extends KeySpace {
-	//TODO: Michael
 }
---
+++ placement/src/test.scala
@@ -0,0 +1,52 @@
+import org.scalatest.Suite
+
+
+class KeyRangeSuite extends Suite {
+	def testAddition() {
+		assert(KeyRange("a","c") + KeyRange("b", "d") == KeyRange("a", "d"))
+		assert(KeyRange("b", "d") + KeyRange("a","c") == KeyRange("a", "d"))
+		assert(KeyRange(null,"c") + KeyRange("b", "d") == KeyRange(null, "d"))
+		assert(KeyRange("a",null) + KeyRange("b", "d") == KeyRange("a", null))
+		assert(KeyRange("a","c") + KeyRange(null, "d") == KeyRange(null, "d"))
+		assert(KeyRange("a","c") + KeyRange("b", null) == KeyRange("a", null))
+		
+		assert(KeyRange("a","b") + KeyRange("b", "c") == KeyRange("a", "c"))
+		assert(KeyRange("b", "c") + KeyRange("a","b") == KeyRange("a", "c"))
+		
+		assert(KeyRange("a", "z") + KeyRange("m","n") == KeyRange("a", "z"))
+		assert(KeyRange("m", "n") + KeyRange("a","z") == KeyRange("a", "z"))
+		
+		intercept[NotContiguousException] {
+			KeyRange("a","b") + KeyRange("c", "d")
+		}
+		
+		intercept[NotContiguousException] {
+			KeyRange("c","d") + KeyRange("a", "b")
+		}
+	}
+	
+	def testSubtraction() {
+		assert(KeyRange("a", "c") - KeyRange("b", "c") == KeyRange("a", "b"))
+		assert(KeyRange("a", "c") - KeyRange("b", "d") == KeyRange("a", "b"))
+		
+		assert(KeyRange("b", "d") - KeyRange("a", "c") == KeyRange("c", "d"))
+		assert(KeyRange("b", "d") - KeyRange("b", "c") == KeyRange("c", "d"))
+		
+		assert(KeyRange("a", "b") - KeyRange("a", "b") == KeyRange.EmptyRange)
+	}
+	
+	def testAnd() {
+		assert((KeyRange("a","c") & KeyRange("b", "d")) == KeyRange("b", "c"))
+		assert((KeyRange("b", "d") & KeyRange("a","c")) == KeyRange("b", "c"))
+		assert((KeyRange(null,"c") & KeyRange("b", "d")) == KeyRange("b", "c"))
+		assert((KeyRange("a",null) & KeyRange("b", "d")) == KeyRange("b", "d"))
+		assert((KeyRange("a","c") & KeyRange(null, "d")) == KeyRange("a", "c"))
+		assert((KeyRange("a","c") & KeyRange("b", null)) == KeyRange("b", "c"))
+	}
+}
+
+object RunTests {
+	def main(args: Array[String]) = {
+		(new KeyRangeSuite).execute()
+	}
+}
