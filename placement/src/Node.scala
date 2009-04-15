8b14e94f27bc0fc461cec2d5a8dae3aed4413d16
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-04-14 18:51:10 -0700

    * Add support for different sync and thrift ports * Add trait for easy keyRange to SCADS.RangeSet converstions

--- placement/src/Node.scala
+++ placement/src/Node.scala
@@ -1,6 +1,23 @@
 import org.apache.thrift.transport.TSocket
 import org.apache.thrift.protocol.TBinaryProtocol
 
-case class StorageNode(host: String, port: Int) extends SCADS.Storage.Client(new TBinaryProtocol(new TSocket(host, port))) {
+case class StorageNode(host: String, thriftPort: Int, syncPort: Int) extends SCADS.Storage.Client(new TBinaryProtocol(new TSocket(host, thriftPort))) {
 	iprot_.getTransport.open()
+
+	def this(host: String, port: Int) = this(host, port, port)
+
+	def syncHost = host + ":" + syncPort
+}
+
+trait ThriftConversions {
+	implicit def keyRangeToScadsRangeSet(x: KeyRange):SCADS.RecordSet = {
+		val recSet = new SCADS.RecordSet
+		val range = new SCADS.RangeSet
+		recSet.setType(SCADS.RecordSet.RANGE)
+		recSet.setRange(range)
+		range.setStart_key(x.start)
+		range.setEnd_key(x.end)
+
+		return recSet
+	}
 }
