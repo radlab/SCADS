68c5b5739063a67ad8b3840669fba3d0e3bcee96
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-05-12 17:47:46 -0700

    capture tx and rx stats for eth0

--- storage/perf/vmstats.rb
+++ storage/perf/vmstats.rb
@@ -5,15 +5,17 @@ file = File.new("/mnt/vmstats.#{hostname}.csv", "w+")
 stats = IO.popen('vmstat 1')
 stats.readline
 
-file.puts((["time", "host"] + stats.readline.split(" ")).join(","))
+file.puts((["time", "host", "tx", "rx"] + stats.readline.split(" ")).join(","))
 
 
 
 while(true)
   line = stats.readline
+  ifconfig = `ifconfig eth0`
+
   if line =~ /\d/
-    file.puts(([Time.now.to_i, hostname] + line.split(" ")).join(","))
-    puts(([Time.now.to_i, hostname] + line.split(" ")).join(",")) if (count += 1) % 10 == 0
+    file.puts(([Time.now.to_i, hostname, ifconfig[/RX bytes:(\d+)/,1], ifconfig[/TX bytes:(\d+)/,1]] + line.split(" ")).join(","))
+    puts(([Time.now.to_i, hostname, ifconfig[/RX bytes:(\d+)/,1], ifconfig[/TX bytes:(\d+)/,1]] + line.split(" ")).join(",")) if (count += 1) % 10 == 0
   end
   file.flush
   STDOUT.flush
