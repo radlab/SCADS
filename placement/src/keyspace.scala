fcea907efd03fe40ae70643237a4c6ef6f1c06c8
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-05-22 16:35:13 -0700

    Remove lots of crufty null handling code that we don't need anymore

--- placement/src/keyspace.scala
+++ placement/src/keyspace.scala
@@ -6,7 +6,7 @@ class NoNodeResponsibleException extends Exception
 class NullKeyLookupException extends Exception
 
 object KeyRange {
-	val EmptyRange = new KeyRange(MinKey, MinKey)
+	val EmptyRange = new KeyRange(new StringKey(""), new StringKey(""))
 }
 
 case class KeyRange(start: Key, end: Key) {
@@ -18,12 +18,6 @@ case class KeyRange(start: Key, end: Key) {
 			that
 		else if(that == KeyRange.EmptyRange)
 			this
-		else if((this.start == null || that.start == null) && (this.end == null || that.end == null))
-			new KeyRange(null, null)
-		else if (this.start == null || that.start == null)
-			new KeyRange(null, max(this.end, that.end))
-		else if (this.end == null || that.end == null)
-			new KeyRange(min(this.start, that.start), null)
 		else if((this.end >= that.start) && (this.end < that.end) && (this.start < that.start))
 			new KeyRange(this.start, that.end)
 		else if((that.end >= this.start) && (that.end < this.end) && (that.start < this.start))
@@ -50,43 +44,20 @@ case class KeyRange(start: Key, end: Key) {
 	}
 
 	def & (that: KeyRange): KeyRange = {
-		if((this.start == null || that.start == null) && (this.end == null || that.end == null))
-			new KeyRange(null, null)
-		else if ( (this.start == null && this.end >= that.start) || (that.start == null && that.end >= this.start))
-			new KeyRange(coalesce(this.start, that.start), min(this.end, that.end))
-		else if ( (this.end == null && this.start < that.end) || (that.end == null && that.start < this.end) )
-			new KeyRange(max(this.start, that.start), coalesce(this.end, that.end))			
-		else if (this.end != null && that.end!=null && this.start != null && that.start != null) {
-			if( (this.end >= that.start) && (this.end < that.end) && (this.start < that.start) )
-				new KeyRange(that.start, this.end)
-			else if( (that.end >= this.start) && (that.end < this.end) && (that.start < this.start) )
-				new KeyRange(this.start, that.end)
-			else if(this.start >= that.start && this.end <= that.end)
-				this
-			else if(that.start >= this.start && that.end <= this.end)
-				that
-			else
-				KeyRange.EmptyRange
-		 }
+		if( (this.end >= that.start) && (this.end < that.end) && (this.start < that.start) )
+			new KeyRange(that.start, this.end)
+		else if( (that.end >= this.start) && (that.end < this.end) && (that.start < this.start) )
+			new KeyRange(this.start, that.end)
+		else if(this.start >= that.start && this.end <= that.end)
+			this
+		else if(that.start >= this.start && that.end <= this.end)
+			that
 		else
 			KeyRange.EmptyRange
 	}
 
 	def includes(key: Key):Boolean = {
-		if (key==null) throw new NullKeyLookupException
-		else {
-		if ( this.start==null) {
-			if ( this.end==null ) true
-			else if ( key< this.end ) true
-			else false
-		}
-		else if (this.end==null) {
-			if ( key>=this.start ) true
-			else false
-		}
-		else 
-			key >= this.start && key < this.end	
-		}
+		key >= this.start && key < this.end
 	}
 
 	private def min(a: Key, b: Key) = if(a < b) a else b
@@ -116,7 +87,7 @@ class SimpleKeySpace extends KeySpace {
 		space = (space - node)
 
 	def lookup(node: StorageNode): KeyRange =
-		space.get(node).getOrElse(KeyRange(MinKey, MinKey))
+		space.get(node).getOrElse(KeyRange.EmptyRange)
 
 	def lookup(key: Key):List[StorageNode] =
 		space.toList.filter((pair) => pair._2.includes(key)).map((pair) => pair._1)
