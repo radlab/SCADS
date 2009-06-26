76354573b8cc6f4db4166ded73a4d550dc81b47a
Aaron Beitch <aaron.beitch@gmail.com>
2009-06-26 11:41:41 -0700

    Added tags to declare that methods service methods throw exceptions.

--- src/service.scala
+++ src/service.scala
@@ -4,6 +4,7 @@ class Service(id: String, instance: Instance) {
   
   def getId = id
   
+  @throws(classOf[IllegalStateException])
   def start {
     checkService
     instance.exec("sv start /mnt/services/" + id)
@@ -15,6 +16,7 @@ class Service(id: String, instance: Instance) {
    * @return      Returns true if service started after given number of checks,
    *              false if service is still not in run state.
    */
+  @throws(classOf[IllegalStateException])
   def blockingStart(wait: Int): Boolean = {
     checkService
     instance.exec("sv start /mnt/services/" + id)
@@ -28,36 +30,42 @@ class Service(id: String, instance: Instance) {
   /**
    * blockingStart(120)
    */
+  @throws(classOf[IllegalStateException])
   def blockingStart(): Boolean = {
     blockingStart(120)
   }
   
+  @throws(classOf[IllegalStateException])
   def once {
     checkService
     instance.exec("sv once /mnt/services/" + id)
   }
   
+  @throws(classOf[IllegalStateException])
   def stop {
     checkService
     instance.exec("sv stop /mnt/services/" + id)
   }
   
+  @throws(classOf[IllegalStateException])
   def forceStop {
     checkService
     instance.exec("sv force-stop /mnt/services/" + id)
   }
   
+  @throws(classOf[IllegalStateException])
   def status: ServiceStatus = {
     checkService
     new ServiceStatus("", "", 0, 0)
   }
   
+  @throws(classOf[IllegalStateException])
   def tailLog(): String = {
     checkService
     instance.exec("tail /mnt/services/" + id + "/log/current").getStdout
   }
   
-  def checkService = {
+  private def checkService = {
     if (instance.exec("ls /mnt/services/" + id).getExitStatus != 0) {
       throw new IllegalStateException("Service not present.")
     }
