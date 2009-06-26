0b81c3df930dfd2437214deb4276372dc75da5b7
Aaron Beitch <aaron.beitch@gmail.com>
2009-06-26 11:39:30 -0700

    Added check that service actually exists to all service commands.

--- src/service.scala
+++ src/service.scala
@@ -5,6 +5,7 @@ class Service(id: String, instance: Instance) {
   def getId = id
   
   def start {
+    checkService
     instance.exec("sv start /mnt/services/" + id)
   }
   
@@ -15,6 +16,7 @@ class Service(id: String, instance: Instance) {
    *              false if service is still not in run state.
    */
   def blockingStart(wait: Int): Boolean = {
+    checkService
     instance.exec("sv start /mnt/services/" + id)
     for (i <- 0 to wait) {
       if (status.getStatus() == "run") return true
@@ -31,23 +33,34 @@ class Service(id: String, instance: Instance) {
   }
   
   def once {
+    checkService
     instance.exec("sv once /mnt/services/" + id)
   }
   
   def stop {
+    checkService
     instance.exec("sv stop /mnt/services/" + id)
   }
   
   def forceStop {
+    checkService
     instance.exec("sv force-stop /mnt/services/" + id)
   }
   
   def status: ServiceStatus = {
+    checkService
     new ServiceStatus("", "", 0, 0)
   }
   
   def tailLog(): String = {
-    ""
+    checkService
+    instance.exec("tail /mnt/services/" + id + "/log/current").getStdout
+  }
+  
+  def checkService = {
+    if (instance.exec("ls /mnt/services/" + id).getExitStatus != 0) {
+      throw new IllegalStateException("Service not present.")
+    }
   }
   
 }
