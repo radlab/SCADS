dfb4cb91dfeb29340a97be28c33a65ab6409cc6f
Aaron Beitch <aaron.beitch@gmail.com>
2009-06-29 14:14:18 -0700

    Fixed some typos that was breaking the build.

--- src/util.scala
+++ src/util.scala
@@ -1,9 +1,9 @@
-package DeployLib
+package deploylib
 
 object Util {
   def responseError(response: ExecuteResponse): Boolean = {
     val exitStatus = response.getExitStatus()
-    if (exitStatus != null && != 0) return true
+    if (exitStatus != null && exitStatus != 0) return true
     if (exitStatus == null && response.getStderr.length() > 0) return true
     return false
   }
