46d639eb34eaf0debdcb8e521483628f0851c26a
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-07-06 21:39:05 -0700

    convert from scala key range to scads recordset

--- placement/src/main/scala/edu/berkeley/cs/scads/thrift/Conversion.scala
+++ placement/src/main/scala/edu/berkeley/cs/scads/thrift/Conversion.scala
@@ -13,3 +13,15 @@ trait Conversions extends AutoKey {
     (r.key, r.value)
   }
 }
+
+trait RangeConversion {
+	def keyRangeToScadsRangeSet(kr:KeyRange):RecordSet = {
+		val recSet = new RecordSet
+		val range = new RangeSet
+		recSet.setType(RecordSetType.RST_RANGE)
+		recSet.setRange(range)
+		range.setStart_key(kr.start.serialize)
+		range.setEnd_key(kr.end.serialize)
+		return recSet
+	}
+}
