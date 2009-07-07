997022ae79c2a335b7107be6c0d2c7e43a4d967f
Nick Lanham <nick@nick.(none)>
2009-07-07 13:29:26 -0700

    add some tests

--- placement/src/test/scala/edu/berkeley/cs/scads/test/keystoresuite.scala
+++ placement/src/test/scala/edu/berkeley/cs/scads/test/keystoresuite.scala
@@ -7,16 +7,145 @@ import edu.berkeley.cs.scads.nodes.TestableBdbStorageNode
 import edu.berkeley.cs.scads.nodes.TestableSimpleStorageNode
 
 import edu.berkeley.cs.scads.thrift.KeyStore
+import edu.berkeley.cs.scads.thrift.RangeSet
 import edu.berkeley.cs.scads.thrift.Record
+import edu.berkeley.cs.scads.thrift.RecordSetType
+import edu.berkeley.cs.scads.thrift.RecordSet
+
+import scala.util.Random
+
+object KeyStoreUtils {
+
+  def putNumericKeys(s: Int, count: Int, ns: String, connection: KeyStore.Client) {
+    val e = s+count
+    for (i <- s to e) {
+      val nk = new NumericKey(i)
+      val rec = new Record(nk.serialize,nk.serialize)
+      connection.put(ns,rec)
+    }
+  }
+  
+  /*
+  def assertNumericKeys(s: Int, count: Int, ns: String, connection: KeyStore.Client) {
+    val e = s+count
+    for (i <- 1 to e) {
+      val nk = new NumericKey(i)
+      assert(connection.get(ns,nk.serialize).value === nk.serialize)
+    }
+  }
+  */
+}
 
 abstract class KeyStoreSuite extends Suite { 
   val connection: KeyStore.Client
 
+  // == Simple get/put tests ==
   def testSimpleGetPut() = {
     def rec = new Record("test", "test")
     connection.put("simple", rec)
     assert(connection.get("simple", "test") === rec)
   }
+
+  def testUpdate() = {
+    /*
+    for (i <- (1 to 100)) {
+      val rec = new Record("test"+i,"data"+i)
+      connection.put("tu",rec)
+    }
+    * */
+    KeyStoreUtils.putNumericKeys(0,100,"tu",connection)
+    for (i <- (0 to 100)) {
+      val nk = new NumericKey(i)
+      assert(connection.get("tu",nk.serialize).value === nk.serialize)
+    }
+
+    for (i <- (0 to 100)) {
+      val nk = new NumericKey(i)
+      val rec = new Record(nk.serialize,"data"+(i*2))
+      connection.put("tu",rec)
+    }
+    for (i <- (0 to 100)) {
+      val nk = new NumericKey(i)
+      assert(connection.get("tu",nk.serialize).value === ("data"+(i*2)))
+    }
+  }
+
+  def testSetToNull() = {
+    for (i <- (1 to 100)) {
+      val rec = new Record("test"+i,"data"+i)
+      connection.put("tstn",rec)
+    }
+    for (i <- (1 to 100)) {
+      if ((i % 2) == 0) {
+        val rec = new Record("test"+i,null)
+        connection.put("tstn",rec)
+      }
+    }
+    for (i <- (1 to 100)) {
+      if ((i % 2) == 0)
+        assert(connection.get("tstn","test"+i).value === null)
+      else
+        assert(connection.get("tstn","test"+i).value === ("data"+i))
+    }
+  }
+
+  def testEmptyString() = {
+    for (i <- (1 to 100)) {
+      val rec = new Record("test"+i,"data"+i)
+      connection.put("tes",rec)
+    }
+    for (i <- (1 to 100)) {
+      if ((i % 2) == 0) {
+        val rec = new Record("test"+i,"")
+        connection.put("tes",rec)
+      }
+    }
+    for (i <- (1 to 100)) {
+      if ((i % 2) == 0)
+        assert(connection.get("tes","test"+i).value === "")
+      else
+        assert(connection.get("tes","test"+i).value === ("data"+i))
+    }
+  }
+
+
+  // == test various get_set functionality ==
+  def testSimpleGetSet() = {
+    for (i <- (1 to 100)) {
+      val nk = new NumericKey(i)
+      val rec = new Record(nk.serialize,nk.serialize)
+      connection.put("tsgs",rec)
+    }
+    val rangeSet = new RangeSet(new NumericKey(0).serialize,new NumericKey(20).serialize,0,0)
+    rangeSet.unsetOffset()
+    rangeSet.unsetLimit()
+    val targetSet = new RecordSet(RecordSetType.RST_RANGE,
+                                  rangeSet,null,null)
+                                  
+    val res: java.util.List[Record] = connection.get_set("tsgs",targetSet)
+    assert(res.size() === 20)
+    // would be nice if there was a foreach with position
+    for (i <- 1 to 20) {
+      val nk = new NumericKey(i)
+      assert(res.get(i-1).value === nk.serialize)
+    }
+  }
+
+  def testGetSetLimit() {
+    KeyStoreUtils.putNumericKeys(0,100,"tgsl",connection)
+    
+    val lim = (new Random).nextInt(90)+1
+    val rangeSet = new RangeSet(null,null,0,lim)
+    val targetSet = new RecordSet(RecordSetType.RST_RANGE,
+                                  rangeSet,null,null)
+    val res: java.util.List[Record] = connection.get_set("tgsl",targetSet)
+    assert(res.size() === lim)
+    for (i <- 0 to (lim-1)) {
+      val nk = new NumericKey(i)
+      assert(res.get(i).value === nk.serialize)
+    }
+  }
+
 }
 
 class BdbKeyStoreTest extends KeyStoreSuite {
