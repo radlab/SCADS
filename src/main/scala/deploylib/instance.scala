0dc9272a2f808c8e38014603b97950d855b5a950
Aaron Beitch <aaron.beitch@gmail.com>
2009-07-13 16:42:43 -0700

    Finished documentation for the Instance class.

--- src/main/scala/deploylib/instance.scala
+++ src/main/scala/deploylib/instance.scala
@@ -12,7 +12,9 @@ import com.amazonaws.ec2.model._
  * methods to interact with the EC2 Instance.
  * <br>
  * <br>
- * One thing to note is that any method requiring an SSH connection will are
+ * There are two things to note:
+ * <br>
+ * (One) Any method requiring an SSH connection will are
  * not able to execute unless the instance is in a running state and contains
  * an SSH object.
  * <br>
@@ -20,6 +22,12 @@ import com.amazonaws.ec2.model._
  * There are two ways that an instance can get an SSH object. Either the
  * instance is instantiated with an EC2 instance is running, or waitUntilReady
  * method is called.
+ * <br>
+ * <br>
+ * (Two) The state of the instance is frozen from whenever this instance
+ * object was instantiated. ie. The response to a running call will always be the
+ * same unless you update the state. To update the state call refresh on the instance
+ * object.
  */
 class Instance(initialInstance: RunningInstance, keyPath: String) {
   private var instance = initialInstance
@@ -64,11 +72,18 @@ class Instance(initialInstance: RunningInstance, keyPath: String) {
       return Some(new JSONObject(response.getStdout()))
   }
   
+  /**
+   * Shuts down this instance and removes itself from the DataCenter's list of 
+   * instances.
+   */
   def stop = {
     DataCenter.terminateInstances(new InstanceGroup(List(this)))
     refresh
   }
   
+  /**
+   * Makes a service object out of all runit services installed on the instance.
+   */
   @throws(classOf[IllegalStateException])
   def getAllServices: Array[Service] = {
     val response = exec("ls /mnt/services")
@@ -80,25 +95,42 @@ class Instance(initialInstance: RunningInstance, keyPath: String) {
     }
   }
   
+  /**
+   * Removes all runit services.
+   */
   def cleanServices = {
     exec("rm -rf /mnt/services")
   }
   
+  /**
+   * If the service of the corresponding name exists, returns it wrapped in
+   * a Some object otherwise returns None.
+   */
   @throws(classOf[IllegalStateException])
   def getService(id: String): Option[Service] = {
     getAllServices.find(service => service.getId == id)
   }
   
+  /**
+   * Tags the instance with the given tag. This tag persists with the EC2
+   * instance.
+   */
   @throws(classOf[IllegalStateException])
   def tagWith(tag: String) = {
     exec("echo \'" + tag + "\' >> /mnt/tags")
   }
 
+  /**
+   * Checks whether the instance is tagged with the given tag.
+   */
   @throws(classOf[IllegalStateException])  
   def isTaggedWith(tag: String): Boolean = {
     getAllTags.find(possible => possible == tag).isDefined
   }
   
+  /**
+   * Gets all tags the instance has been tagged with.
+   */
   @throws(classOf[IllegalStateException])
   def getAllTags: Array[String] = {
     val response = exec("cat /mnt/tags")
@@ -106,17 +138,29 @@ class Instance(initialInstance: RunningInstance, keyPath: String) {
     else return response.getStdout.split("\n")
   }
   
+  /**
+   * Removes the given tag from the instance.
+   */
   @throws(classOf[IllegalStateException])
   def removeTag(tag: String) ={
     exec("sed \'/" + tag + "/d\' /mnt/tags > /mnt/tmp && mv /mnt/tmp /mnt/tags")
   }
   
+  /**
+   * Executes the given command on the instance.
+   *
+   * @throws IllegalStateException if SSH has not been set up. See the
+   *                               Instance class documentation for a discussion of this.
+   */
   @throws(classOf[IllegalStateException])
   def exec(cmd: String): ExecuteResponse = {
     checkSsh
     ssh.executeCommand(cmd)
   }
   
+  /**
+   * Blocks the current thread until this instance is in running state.
+   */
   def waitUntilReady: Unit = {
     while (refresh && !running) {
       Thread.sleep(5000)
@@ -147,48 +191,61 @@ class Instance(initialInstance: RunningInstance, keyPath: String) {
                                       "Call waitUntilReady method first.")
     }
   }
-  
+  /**
+   * Returns true if the instance is in a running state, false otherwise.
+   */
   def running: Boolean = {
     instanceState == "running"
   }
   
+  /**
+   * Returns true if the instance is in the state of 'shutting-down' or 'terminated'.
+   */
   def terminated: Boolean = {
     instanceState == "shutting-down" || instanceState == "terminated"
   }
   
-  /* Accessors */
+  /** Returns the instance ID. */
   def instanceId: String = {
     instance.getInstanceId()
   }
   
+  /** Returns the image ID. */
   def imageId: String = {
     instance.getImageId()
   }
   
+  /** Returns the isntance state. */
   def instanceState: String = {
     instance.getInstanceState().getName()
   }
   
+  /** Returns the private DNS name. */
   def privateDnsName: String = {
     instance.getPrivateDnsName()
   }
   
+  /** Returns the public DNS name. */
   def publicDnsName: String = {
     instance.getPublicDnsName()
   }
   
+  /** Returns the public key name. */  
   def keyName: String = {
     instance.getKeyName()
   }
   
+  /** Returns the instance type. */
   def instanceType: String = {
     instance.getInstanceType()
   }
   
+  /** Returns the time the instance was launched. */
   def launchTime: String = {
     instance.getLaunchTime()
   }
   
+  /** Returns the availability zone the instance resides in. */
   def availabilityZone: String = {
     instance.getPlacement().getAvailabilityZone()
   }
