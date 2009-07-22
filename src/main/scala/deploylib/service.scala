3243f0dc16d3a6dd8b7631ad5af393c2d168104d
Aaron Beitch <aaron.beitch@gmail.com>
2009-07-22 16:15:03 -0700

    Added documentation to the Service class. Added a tailLog where the number of lines is specified.

--- src/main/scala/deploylib/service.scala
+++ src/main/scala/deploylib/service.scala
@@ -5,13 +5,23 @@ import scala.util.matching.Regex
 class Service(id: String, instance: Instance) {
   checkService
   
+  /**
+   * Returns id/name of service.
+   */
   def getId = id
   
+  /**
+   * Tells runit to start the service.
+   */
   def start {
     instance.exec("sv start /mnt/services/" + id)
   }
   
   /**
+   * Tells runit to start the service and checks the service status the given
+   * number of times with 1 second sleeps in between. Stops when the service
+   * is in the running state.
+   *
    * @param wait  How many times to check for a service's status.
    *              Checks are seperated by one second.
    * @return      Returns true if service started after given number of checks,
@@ -33,18 +43,33 @@ class Service(id: String, instance: Instance) {
     blockingStart(120)
   }
   
+  /**
+   * Tells runit to 'once' the service, which means to start the service once
+   * and don't try to restart it if it dies.
+   */
   def once {
     instance.exec("sv once /mnt/services/" + id)
   }
   
+  /**
+   * Tells runit to stop the service.
+   */
   def stop {
     instance.exec("sv stop /mnt/services/" + id)
   }
   
+  /**
+   * Runit tries very hard to stop the service if it won't stop through, the
+   * normal means.
+   */
   def forceStop {
     instance.exec("sv force-stop /mnt/services/" + id)
   }
   
+  /**
+   * Gets the output of running a runit status command and wraps it in a
+   * ServiceStatus object.
+   */
   def status: ServiceStatus = {
     val RunningRegex = new Regex(
           """(\S+): /mnt/services/(\S+): \(pid (\d+)\) (\d+)s.*""")
@@ -63,10 +88,26 @@ class Service(id: String, instance: Instance) {
     }
   }
   
+  /**
+   * Gets the output of a tail on the service's log. Uses the default tail
+   * length: the last 10 lines.
+   */
   def tailLog(): String = {
     instance.exec("tail /mnt/services/" + id + "/log/current").getStdout
   }
   
+  /**
+   * Gets the output of a tail on the service's log.
+   *
+   * @param count The number of lines you want.
+   */
+  def tailLog(count: Int): String = {
+    instance.exec("tail -n " + count + "/mnt/services/" + id + "/log/current").getStdout
+  }
+  
+  /**
+   * Returns true if the status of the service is 'run'.
+   */
   def running: Boolean = {
     status.getStatus() == "run"
   }
