37cb5b77bc37191e1fd4d6ca7ea70054e848dff5
Peter Bodik <bodikp@gmail.com>
2009-07-23 15:57:56 -0700

    creating a new file for the different workload descriptions

---
+++ experiments/src/main/scala/performance/workloadDescriptions.scala
@@ -0,0 +1,31 @@
+package performance
+
+import edu.berkeley.xtrace._
+import edu.berkeley.cs.scads.thrift._
+import edu.berkeley.cs.scads.nodes._
+import edu.berkeley.cs.scads.keys._
+import edu.berkeley.cs.scads.placement._
+import edu.berkeley.cs.scads.client._
+import org.apache.thrift.transport.{TFramedTransport, TSocket}
+import org.apache.thrift.protocol.{TBinaryProtocol,XtBinaryProtocol}
+
+import java.io._
+import java.net._
+
+object WorkloadGenerators {
+	
+	def createSimpleLinearGetPutGetsetWorkload(getProb:Double, getsetProb:Double, getsetLength:Int, namespace:String, totalUsers:Int, userStartDelay:Int, thinkTime:Int):WorkloadDescription = {
+		// create sample workload description
+		val mix = Map("get"->getProb,"getset"->getsetProb,"put"->(1-getProb-getsetProb))
+		val parameters = Map("get"->Map("minKey"->"0","maxKey"->"10000","namespace"->namespace),
+							 "put"->Map("minKey"->"0","maxKey"->"10000","namespace"->namespace),
+							 "getset"->Map("minKey"->"0","maxKey"->"10000","namespace"->namespace,"setLength"->getsetLength.toString)
+							)
+		val reqGenerator = new SimpleSCADSRequestGenerator(mix,parameters)		
+		var intervals = new scala.collection.mutable.ListBuffer[WorkloadIntervalDescription]
+		
+		for (nusers <- 1 to totalUsers) intervals += new WorkloadIntervalDescription(nusers, userStartDelay, reqGenerator)
+		new WorkloadDescription(thinkTime, intervals.toList)
+	}
+
+}
