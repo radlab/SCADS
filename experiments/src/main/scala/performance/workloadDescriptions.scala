6d65590283d4c251483a8ae4985fc844b6515779
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-07-23 19:16:20 -0700

    workload description definition for the flat workload

--- experiments/src/main/scala/performance/workloadDescriptions.scala
+++ experiments/src/main/scala/performance/workloadDescriptions.scala
@@ -27,5 +27,19 @@ object WorkloadGenerators {
 		for (nusers <- 1 to totalUsers) intervals += new WorkloadIntervalDescription(nusers, userStartDelay, reqGenerator)
 		new WorkloadDescription(thinkTime, intervals.toList)
 	}
+	def flatWorkload(getProb:Double, getsetProb:Double, getsetLength:Int, namespace:String, totalUsers:Int, num_minutes:Int, thinkTime: Int) = {
+		// how many minutes to run test flat workload: all users start making requests at the same time
+		val mix = Map("get"->getProb,"getset"->getsetProb,"put"->(1-getProb-getsetProb))
+		val parameters = Map("get"->Map("minKey"->"0","maxKey"->"10000","namespace"->namespace),
+							 "put"->Map("minKey"->"0","maxKey"->"10000","namespace"->namespace),
+							 "getset"->Map("minKey"->"0","maxKey"->"10000","namespace"->namespace,"setLength"->getsetLength.toString)
+							)
+		val reqGenerator = new SimpleSCADSRequestGenerator(mix,parameters)
+		var intervals = new scala.collection.mutable.ListBuffer[WorkloadIntervalDescription]
+		val interval = new WorkloadIntervalDescription(totalUsers, num_minutes*60000, reqGenerator)
+		intervals += interval
+
+		new WorkloadDescription(thinkTime, intervals.toList)
+	}
 
 }
