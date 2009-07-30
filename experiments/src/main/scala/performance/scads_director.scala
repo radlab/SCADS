b03ea313fceb83171062de949cef34479c4c6d19
Peter Bodik <bodikp@gmail.com>
2009-07-30 11:07:05 -0700

    adding scads director startup script

---
+++ experiments/src/main/scala/performance/scads_director.scala
@@ -0,0 +1,29 @@
+/**
+ * scripts for deploying the director
+ */
+ 
+package performance
+ 
+import deploylib._ /* Imports all files in the deployment library */
+import org.json.JSONObject
+import org.json.JSONArray
+import com.twitter.commons._
+import scala.collection.jcl.Conversions._
+
+object ScadsDirector {
+  
+  	var directorVM: Instance = null
+	var config: JsonQuoted = null
+
+  	def startDirectorVM {
+    	directorVM = DataCenter.runInstances(1, "m1.small").getFirst()
+	    directorVM.waitUntilReady
+
+		config = Json.build( Map("recipes"->Array("director::scads_director"),
+									 "director"->Map("basedir"->"/mnt/director",
+													 "metricService"->Map("port"->6001,"dbhost"->"localhost","dbuser"->"root","dbpassword"->"","dbname"->"metrics"))))
+	    println( config.toString() )
+	    directorVM.deploy(config)
+  	}
+   
+}
