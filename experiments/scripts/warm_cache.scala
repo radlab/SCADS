4c9e9d5035b2a753e9cc94fce4c3b7fbeffaf819
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-07-30 11:07:55 -0700

    cache warming script also takes xtrace_on command arg

--- experiments/scripts/warm_cache.scala
+++ experiments/scripts/warm_cache.scala
@@ -1,5 +1,6 @@
 // note:  this assumes each key belongs to exactly one storage node
 import edu.berkeley.cs.scads.client._
+import edu.berkeley.cs.scads.thrift.Record
 import performance._
 
 val port = 8000
@@ -7,23 +8,29 @@ val host = args(0)
 val namespace = args(1)
 val minKey = args(2).toDouble // doubles so can divide later
 val maxKey = args(3).toDouble 
-val xtrace_on:Boolean = false
+val xtrace_on:Boolean = args(4).toBoolean
+
+if (xtrace_on) System.setProperty("xtrace","")
 
 case class CacheWarmer(startk:Int, endk:Int) extends Runnable {
 	override def run() = {
 		val client = new SCADSClient(host,port)
 		(startk to endk).toList.foreach((key) => {
 			client.get(namespace,getKey(key))
+			//client.put(namespace,new Record(getKey(key),getValue()))
 		})
 	}
 	def getKey(key: Int):String = {
 		val keyFormat = new java.text.DecimalFormat("000000000000000")
 		keyFormat.format(key)
 	}
+	def getValue() = {
+		"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	}
 }
 // set up the warming threads
-val slice = (Math.ceil( (maxKey-minKey)/100 )).toInt // divide the work amongst 100 threads
-val threads = (0 to 100).toList.map((id)=>{
+val slice = (Math.ceil( (maxKey-minKey)/128 )).toInt // divide the work amongst 128 threads
+val threads = (0 to 127).toList.map((id)=>{
 	new Thread(new CacheWarmer( id*slice,id*slice+(slice-1) ))
 })
 
