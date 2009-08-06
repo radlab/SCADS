2254b2c3afa8b75192514660d9585073b30d6926
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-08-06 11:12:03 -0700

    manager class for adding and removing scads storage nodes, useful for having standby nodes when running director tests. note: does not currently restart storage engines that are released back to pool

---
+++ experiments/src/main/scala/performance/ServerManager.scala
@@ -0,0 +1,34 @@
+package performance
+
+import deploylib._
+
+class ScadsServerManager(deploy_name:String, xtrace_on:Boolean, namespace:String) {
+	val myscads = new Scads(deploy_name,xtrace_on,namespace)
+	myscads.loadState
+	var standbys = new scala.collection.mutable.ListBuffer[String]() 
+
+	def bootServers(num:Int): List[String] = {
+		var new_guys = new scala.collection.mutable.ListBuffer[String]()
+		val old_num = myscads.servers.size
+		myscads.addServers(num)
+		(old_num to (myscads.servers.size-1) ).foreach((id) => { 
+			val server = myscads.servers.get(id).privateDnsName
+			new_guys += server
+			standbys += server
+		})
+		new_guys.toList
+	}
+	def killServer(host:String) = myscads.shutdownServer(host) 
+	def getServers(num:Int): List[String] = {
+		assert( standbys.size >= num, "Only have "+standbys.size + " servers left." ) 
+		var ret = new scala.collection.mutable.ListBuffer[String]() 
+		(1 to num).foreach((id)=>{
+			ret += standbys.remove(0)
+		})
+		ret.toList
+	}
+	def releaseServer(host:String):Int = {
+		standbys += host
+		standbys.size
+	}
+}
