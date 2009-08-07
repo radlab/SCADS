335909200759cdaa4c6fb19d4997ede6be8e6739
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-08-07 13:59:32 -0700

    test case for placement server concurrency

--- placement/src/test/scala/scads/test/placementsuite.scala
+++ placement/src/test/scala/scads/test/placementsuite.scala
@@ -4,11 +4,13 @@ import org.scalatest.Suite
 
 import edu.berkeley.cs.scads.keys._
 import edu.berkeley.cs.scads.nodes.{TestableSimpleStorageNode,TestableBdbStorageNode,StorageNode}
-import edu.berkeley.cs.scads.placement.{RunnableDataPlacementServer,LocalDataPlacementProvider,RemoteDataPlacementProvider}
-import edu.berkeley.cs.scads.thrift.{Record,KnobbedDataPlacementServer,DataPlacement,RangeConversion,NotResponsible}
+import edu.berkeley.cs.scads.placement.{SimpleKnobbedDataPlacementServer,RunnableDataPlacementServer,LocalDataPlacementProvider,RemoteDataPlacementProvider}
+import edu.berkeley.cs.scads.thrift.{Record,RecordSet,KnobbedDataPlacementServer,DataPlacement,RangeConversion,NotResponsible}
 import edu.berkeley.cs.scads.client._
 import org.apache.thrift.transport.{TFramedTransport, TSocket}
 import org.apache.thrift.protocol.{TBinaryProtocol, XtBinaryProtocol}
+import org.apache.thrift.server.{THsHaServer,TNonblockingServer}
+import org.apache.thrift.transport.TNonblockingServerSocket
 import org.apache.log4j.Logger
 
 class PlacementTest extends Suite with AutoKey {
@@ -38,7 +40,7 @@ class DataPlacementServerTest extends Suite with AutoKey with RangeConversion {
 	val h = "localhost"
 	System.setProperty("placementport",p.toString)
 
-	val dp = new RunnableDataPlacementServer // start up dp server on port p
+	val dp = new RunnableDataPlacementServer(p) // start up dp server on port p
 	Thread.sleep(1000)
 	val transport = new TFramedTransport(new TSocket(h, p))
 	val protocol = new TBinaryProtocol(transport)
@@ -47,6 +49,66 @@ class DataPlacementServerTest extends Suite with AutoKey with RangeConversion {
 
 	class LocalProviderTest extends LocalROWAClientLibrary
 	class RemoteProviderTest extends SCADSClient(h,p)
+
+	case class TestDataPlacementServer(port:Int) extends Runnable {
+		val serverthread = new Thread(this, "TestDataPlacementServer-" + port)
+		serverthread.start
+
+		class DPThinker extends SimpleKnobbedDataPlacementServer {
+			override def move(ns: String, rset: RecordSet, src_host: String, src_thrift: Int, src_sync: Int, dest_host: String, dest_thrift: Int, dest_sync: Int) {
+				Thread.sleep(10*1000)
+			}
+		}
+
+		def run() {
+			val logger = Logger.getLogger("placement.dataplacementserver")
+			try {
+				val serverTransport = new TNonblockingServerSocket(port)
+		    	val processor = new KnobbedDataPlacementServer.Processor(new DPThinker)
+				val protFactory = new TBinaryProtocol.Factory(true, true)
+		    	val options = new THsHaServer.Options
+				options.maxWorkerThreads=1
+				val server = new THsHaServer(processor, serverTransport,protFactory,options)
+				logger.info("Starting test data placement server on "+port)
+		    	server.serve()
+		  	} catch {
+		    	case x: Exception => x.printStackTrace()
+		  	}
+		}
+	}
+	class ConcurrentAccess(namespace: String, remove:Boolean) extends Runnable {
+		def run() = {
+			val transport = new TFramedTransport(new TSocket("localhost", 8001))
+			val protocol = new TBinaryProtocol(transport)
+			val dpclient = new KnobbedDataPlacementServer.Client(protocol)
+			transport.open()
+
+			if (remove) {
+				Thread.sleep(1*1000)
+				val startt = System.currentTimeMillis
+				val size = dpclient.lookup_namespace(namespace).size
+				val node = new TestableSimpleStorageNode
+				val list = new java.util.ArrayList[DataPlacement]()
+				list.add(new DataPlacement(node.host,node.thriftPort,node.syncPort,KeyRange("0","9")))
+				dpclient.add(namespace,list)
+				val endt = System.currentTimeMillis
+				assert ( dpclient.lookup_namespace(namespace).size==(size+1) )
+			}
+			else {
+				val size = dpclient.lookup_namespace(namespace).size
+				dpclient.move(namespace, null, "fake",9000, 9091, "fake", 9000, 9091)
+				assert ( dpclient.lookup_namespace(namespace).size==(size+1) )
+			}
+		}
+	}
+
+	def testConcurrentAccess {
+		val dp = new TestDataPlacementServer(8001)
+		val namespace = "concurrent"
+		val threads = List[Thread](new Thread(new ConcurrentAccess(namespace,false)),new Thread(new ConcurrentAccess(namespace,true)))
+		for(thread <- threads) thread.start
+		for(thread <- threads) thread.join
+	}
 
 	def testSetAndLookup {
 		val namespace = "setandlookup"
@@ -90,4 +152,5 @@ class DataPlacementServerTest extends Suite with AutoKey with RangeConversion {
 		assert(ks.lookup(namespace, new StringKey("0")).length==1)
 		assert(ks.lookup(namespace, new StringKey("0"))(0)==node)
 	}
+
 }
