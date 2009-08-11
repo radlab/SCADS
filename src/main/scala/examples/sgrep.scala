fcbafadff1861726406d37f02ce8e19a664e21ed
Paul Phillips <paulp@improving.org>
2009-08-11 05:02:00 -0700

    Added -i option to sgrep; now sgrep -vi works as expected.

--- src/main/scala/examples/sgrep.scala
+++ src/main/scala/examples/sgrep.scala
@@ -7,11 +7,18 @@ object sgrep extends optional.Application
 {
   def mkRegexp(s: String): Regex = s.r
   
-  register(ArgInfo('v', "invert-match", true, "Invert the sense of matching, to select non-matching lines."))
+  register(
+    ArgInfo('v', "invert-match", true, "Invert the sense of matching, to select non-matching lines."),
+    ArgInfo('i', "ignore-case", true, "Ignore case distinctions in both the PATTERN and the input files.")
+  )
   
-  def main(v: Boolean, arg1: Regex, arg2: File) {
+  def main(v: Boolean, i: Boolean, arg1: Regex, arg2: File) {
+    // reverse condition if -v is given
     def cond(x: Option[_]) = if (v) x.isEmpty else x.isDefined
-    for (line <- io.Source.fromFile(arg2).getLines ; if cond(arg1.findFirstIn(line)))
-      println(line)
+    // case insensitive if -i is given
+    val regex = if (i) ("""(?i)""" + arg1.toString).r else arg1
+
+    for (line <- io.Source.fromFile(arg2).getLines ; if cond(regex findFirstIn line))
+      print(line)
   }
 }
