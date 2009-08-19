44295e88f8345605f5f0f8b71e4cddf302cf0882
Peter Bodik <bodikp@gmail.com>
2009-08-19 13:38:34 -0700

    updating scadsstate: generation of random server names for action previews, adding workload histogram to the state

--- experiments/src/main/scala/performance/director/scadsState.scala
+++ experiments/src/main/scala/performance/director/scadsState.scala
@@ -1,5 +1,6 @@
 package scads.director
 
+import performance._
 import java.util.Date
 
 import java.sql.Connection
@@ -9,11 +10,16 @@ import java.sql.SQLException
 import java.sql.Statement
 
 // ??
-class DirectorKeyRange(
+case class DirectorKeyRange(
 	val minKey: Int,
 	val maxKey: Int
 ) {
 	override def toString():String = "["+minKey+","+maxKey+")"
+	
+	def overlaps(that:DirectorKeyRange):Boolean = {
+		if (that.maxKey<=this.minKey || this.maxKey<=that.minKey) false
+		else true
+	}
 }
 
 
@@ -34,6 +40,19 @@ case class SCADSconfig(
 	val storageNodes: Map[String,DirectorKeyRange]
 ) {
 	def getNodes:List[String] = storageNodes.keySet.toList
+	override def toString():String = storageNodes.toList.sort(_._2.minKey<_._2.minKey).map( x=>"%-15s".format(x._2)+"  "+x._1 ).mkString("\n")
+}
+
+object SCADSconfig {
+	def getInitialConfig(range:DirectorKeyRange):SCADSconfig = SCADSconfig( Map(getRandomServerNames(null,1).first->range) )
+
+	def getRandomServerNames(cfg:SCADSconfig,n:Int):List[String] = {
+		val rnd = new java.util.Random()
+		val newNames = scala.collection.mutable.HashSet[String]()
+		var name = ""
+		for (i <- 1 to n) { do { name = "s"+"%05d".format(rnd.nextInt(10000)) } while ( (cfg!=null&&cfg.storageNodes.contains(name))||newNames.contains(name) ); newNames+=name }
+		newNames.toList
+	}
 }
 
 object SCADSState {
@@ -74,7 +93,7 @@ object SCADSState {
 		
 		val metrics = PerformanceMetrics.load(metricReader,"ALL","ALL")
 		val metricsByType = reqTypes.map( (t) => t -> PerformanceMetrics.load(metricReader,"ALL",t)).foldLeft(Map[String,PerformanceMetrics]())((x,y)=>x+y)
-		new SCADSState(new Date(), SCADSconfig(nodeConfig), nodes.toList, metrics, metricsByType)
+		new SCADSState(new Date(), SCADSconfig(nodeConfig), nodes.toList, metrics, metricsByType, null)
 	}
 }
 
@@ -83,20 +102,99 @@ class SCADSState(
 	val config: SCADSconfig,
 	val storageNodes: List[StorageNodeState],
 	val metrics: PerformanceMetrics,
-	val metricsByType: Map[String,PerformanceMetrics]
-	/* TODO: add range stats */
+	val metricsByType: Map[String,PerformanceMetrics],
+	val workloadHistogram: WorkloadHistogram
 ) {	
 	override def toString():String = {
-		"STATE@"+time+"  metrics["+metrics.toString+"]\n"+
-		metricsByType.map("   ["+_.toString+"]").mkString("","\n","")+
-		"\nindividual servers:\n"+storageNodes.map(_.toString).mkString("","\n","")
+		"STATE@"+time+"  metrics["+
+		(if(metrics!=null)metrics.toString)+"]\n"+
+		(if(metricsByType!=null)metricsByType.map("   ["+_.toString+"]").mkString("","\n",""))+
+		"\nindividual servers:\n"+
+		(if(storageNodes!=null)storageNodes.map(_.toString).mkString("","\n",""))
 	}
 	def toShortString():String = {
-		"STATE@"+time+"  W="+metrics.workload.toInt+"/"+metricsByType("get").workload.toInt+"/"+metricsByType("put").workload.toInt 	+ 
-		"  getL="+metricsByType("get").toShortLatencyString()+"  putL="+metricsByType("put").toShortLatencyString() +
-		storageNodes.map(_.toShortString()).mkString("\n  ","\n  ","")
+		"STATE@"+time+"  W="+
+		(if(metrics!=null)metrics.workload.toInt)+"/"+
+		(if(metricsByType!=null)(metricsByType("get").workload.toInt+"/"+metricsByType("put").workload.toInt 	+ 
+		"  getL="+metricsByType("get").toShortLatencyString()+"  putL="+metricsByType("put").toShortLatencyString())) +
+		(if(storageNodes!=null)storageNodes.map(_.toShortString()).mkString("\n  ","\n  ",""))
 	}
-	def changeConfig(new_config:SCADSconfig):SCADSState = new SCADSState(time,new_config,storageNodes,metrics,metricsByType)
+	def changeConfig(new_config:SCADSconfig):SCADSState = new SCADSState(time,new_config,storageNodes,metrics,metricsByType,null)
+	
+	def preview(action:Action,performanceModel:PerformanceModel,updatePerformance:Boolean):SCADSState = {
+		val newConfig = action.preview(config)
+		new SCADSState(time,newConfig,null,null,null,null)
+	}
 }
 
+
+case class WorkloadFeatures(
+	getRate: Double,
+	putRate: Double,
+	getsetRate: Double
+) {
+	def add(that:WorkloadFeatures):WorkloadFeatures = {
+		WorkloadFeatures(this.getRate+that.getRate,this.putRate+that.putRate,this.getsetRate+that.getsetRate)
+	}
+	
+	// if this workload is running against n machines, each machine get 1/n of the gets
+	def splitGets(n:Int):WorkloadFeatures = {
+		WorkloadFeatures(this.getRate/n,this.putRate,this.getsetRate)
+	}
+}
+
+/**
+* Represents the histogram of keys in workload
+*/
+class WorkloadHistogram(
+	val rangeStats: Map[DirectorKeyRange,WorkloadFeatures]
+)
+
+object WorkloadHistogram {
+	def create(interval:WorkloadIntervalDescription, rate:Double, nBins:Int): WorkloadHistogram = {
+		val nRequests = nBins*100
+		val rnd = new java.util.Random()
+		val requests = (for (r <- 1 to nRequests) yield { interval.requestGenerator.generateRequest(null,0) }).toList
+		val allkeys = for (r <- requests) yield { r match{ 
+										case g:SCADSGetRequest => {g.key.toInt} 
+										case p:SCADSPutRequest => {p.key.toInt}
+										case _ => {-1} } }
+		val keys = allkeys.filter(_!= -1).toList.sort(_<_).toList
+		val boundaries = (List(keys.first)++(for (i <- 1 to nBins-1) yield { keys(rnd.nextInt(keys.size)) })++List(keys.last)).sort(_<_)
+		val ranges = boundaries.take(nBins).zip(boundaries.tail).map(b=>new DirectorKeyRange(b._1,b._2))
+		
+		// have the ranges, now count the keys in each range
+		val getCounts = scala.collection.mutable.Map[DirectorKeyRange,Int]()
+		val putCounts = scala.collection.mutable.Map[DirectorKeyRange,Int]()
+		var ri = 0
+		var range = ranges(ri)
+
+		val typesAndKeys = (for (r <- requests) yield { 
+			r match {
+				case g:SCADSGetRequest => { ("get",g.key.toInt) }
+				case p:SCADSPutRequest => { ("put",p.key.toInt) }
+				case _ => { ("na",-1) } 
+			}			
+		}).filter(_._2!= -1).toList.sort(_._2<_._2).toList
+		
+		for (tk <- typesAndKeys) { 
+			val rtype = tk._1
+			val key = tk._2
+			while (key>range.maxKey) { ri = ri+1; /*println("next range: "+ri+" key="+key+"  max="+range.maxKey);*/ range = ranges(ri) }
+			rtype match {
+				case "get" => getCounts(range) = getCounts.getOrElse(range,0)+1
+				case "put" => putCounts(range) = putCounts.getOrElse(range,0)+1
+				case _ =>
+			}
+		}
+		
+		val rangeStats = scala.collection.mutable.Map[DirectorKeyRange,WorkloadFeatures]()
+		for (range <- ranges) {
+			val getRate = getCounts.getOrElse(range,0).toDouble / nRequests * rate //(interval.duration/1000)
+			val putRate = putCounts.getOrElse(range,0).toDouble / nRequests * rate //(interval.duration/1000)
+			rangeStats += range -> WorkloadFeatures(getRate,putRate,Double.NaN)
+		}		
+		new WorkloadHistogram(Map[DirectorKeyRange,WorkloadFeatures]()++rangeStats)
+	}
+}
 
