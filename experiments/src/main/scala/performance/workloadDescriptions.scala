bbfb5404e6510f1aff6c500a2d775e22cb6b061a
Peter Bodik <bodikp@gmail.com>
2009-09-14 09:00:55 -0700

    added a bunch of workloads that we'll use for the final experiments ...

--- experiments/src/main/scala/performance/workloadDescriptions.scala
+++ experiments/src/main/scala/performance/workloadDescriptions.scala
@@ -16,6 +16,65 @@ import scala.collection.mutable.ListBuffer
 
 object WorkloadGenerators {
 	
+	val namespace = "perfTest256"
+	val thinkTime = 10
+	val mix100 = new MixVector(Map("get"->1.0,"getset"->0.0,"put"->0.0))
+	val mix99 = new MixVector(Map("get"->0.99,"getset"->0.0,"put"->0.01))
+	val mix98 = new MixVector(Map("get"->0.98,"getset"->0.0,"put"->0.02))
+	val mix97 = new MixVector(Map("get"->0.97,"getset"->0.0,"put"->0.03))
+	val mix96 = new MixVector(Map("get"->0.96,"getset"->0.0,"put"->0.04))
+	
+	def stdWorkload3Flat(mix:MixVector, maxUsers:Int):WorkloadDescription = {
+		val n = 600
+		val wProf = WorkloadProfile.getFlat(n,maxUsers).addSpike(n/3,n/3,n*2/3,n*2/3,3.0).addSpike(n*2/3,n*2/3,n,n,1.5)
+		val keyGenerator = new ZipfKeyGenerator(1.001,1,10000)
+		val reqGenerators = List.make( n, new FixedSCADSRequestGenerator(mix,keyGenerator,namespace,1) )
+		val durations = List.make( n, 60*1000 )
+		WorkloadDescription.create(wProf,durations,reqGenerators,thinkTime)
+	}
+	
+	def stdWorkloadEbatesWSpike(mix:MixVector, maxUsers:Int):WorkloadDescription = {
+		val nVirtualDays = 1.5
+		val nMinutes = 60
+		
+		val wProf = WorkloadProfile.getEbatesProfile(6*nMinutes,4*24*60,Math.floor(nVirtualDays*24*60/(6*nMinutes)).toInt, maxUsers).addSpike(180,186,246,300,3.0)
+		val keyGenerator = new ZipfKeyGenerator(1.001,1,10000)
+		val reqGenerators = List.make( 6*nMinutes, new FixedSCADSRequestGenerator(mix,keyGenerator,namespace,1) )
+		val durations = List.make(6*nMinutes,10*1000)
+		WorkloadDescription.create(wProf,durations,reqGenerators,thinkTime)
+	}
+	
+	def stdWorkloadEbatesWMixChange(mix0:MixVector, mix1:MixVector, maxUsers:Int):WorkloadDescription = {
+		val nVirtualDays = 1.5
+		val nMinutes = 60
+		val nInt = 6*nMinutes
+
+		val mixProfile = WorkloadMixProfile.getStaticMix(nInt,mix0)
+				.transition( WorkloadMixProfile.getStaticMix(nInt,mix1), (0.4*nInt).toInt, (0.45*nInt).toInt )
+				.transition( WorkloadMixProfile.getStaticMix(nInt,mix0), (0.65*nInt).toInt, (0.75*nInt).toInt )
+		
+		val wProf = WorkloadProfile.getEbatesProfile(nInt,4*24*60,Math.floor(nVirtualDays*24*60/(nInt)).toInt, maxUsers)
+		val keyGenerator = new ZipfKeyGenerator(1.001,1,10000)
+		val reqGenerators = mixProfile.profile.map( new FixedSCADSRequestGenerator(_,keyGenerator,namespace,1) ).toList
+		val durations = List.make(nInt,10*1000)
+		WorkloadDescription.create(wProf,durations,reqGenerators,thinkTime)		
+	}
+	
+	def stdWorkloadFlatWithMixChange(mixes:List[MixVector], maxUsers:Int):WorkloadDescription = {
+		val n = 100
+		val wProf = WorkloadProfile.getFlat(n,maxUsers)
+		
+		val mixProfile = WorkloadMixProfile.getStaticMix(n,mixes(0))
+				.transition( WorkloadMixProfile.getStaticMix(n,mixes(1)), (1/5.0*n).toInt, (2/5.0*n).toInt )
+				.transition( WorkloadMixProfile.getStaticMix(n,mixes(2)), (3/5.0*n).toInt, (4/5.0*n).toInt )
+		
+		val keyGenerator = new ZipfKeyGenerator(1.001,1,10000)
+		val reqGenerators = mixProfile.profile.map( new FixedSCADSRequestGenerator(_,keyGenerator,namespace,1) ).toList
+		
+		val durations = List.make( n, 60*1000 )
+		WorkloadDescription.create(wProf,durations,reqGenerators,thinkTime)		
+	}
+	
 	def flatWorkload(getProb:Double, getsetProb:Double, getsetLength:Int, maxKey:String, namespace:String, totalUsers:Int, num_minutes:Int, thinkTime: Int):WorkloadDescription = {
 		val wProf = WorkloadProfile.getFlat(1,totalUsers)
 		val mix = new MixVector(Map("get"->getProb,"getset"->getsetProb,"put"->(1-getProb-getsetProb)))
