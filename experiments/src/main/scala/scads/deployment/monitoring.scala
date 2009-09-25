dee6d07ea0dac79b23e40a65d8da4d8b9b251c78
Peter Bodik <bodikp@gmail.com>
2009-09-25 16:07:53 -0700

    fixing monitoring deployment

--- experiments/src/main/scala/scads/deployment/monitoring.scala
+++ experiments/src/main/scala/scads/deployment/monitoring.scala
@@ -4,9 +4,7 @@ package scads.deployment
 * This will deploy a machine with MySQL, chukwa collector, xtrace data parser, and metric service that generates histograms and workload and performance metrics.
 * This should also do basic plotting ...
 */
-case class SCADSMonitoringDeployment(
-	scads:Scads
-) extends Component {
+case class SCADSMonitoringDeployment() extends Component {
 	var monitoringVMInstanceType = "c1.small"
 	var monitoringVM:Instance = null
 	
@@ -38,18 +36,21 @@ case class SCADSMonitoringDeployment(
 	
 	case class Deployer extends Runnable {
 		def run = {
-			monitoringVM.exec("/etc/init.d/apache2 start") // start apache for viewing graphs
 			
-			// start xtrace parser
-			monitoringVM.exec("java -DMSHost=localhost -DMSPort=6001 -DchukwaHost=localhost -DchukwaPort=9094 -DnBins="+nBins+" -DminKey="+minKey+" -DmaxKey="+maxKey+" -DhistogramDBHost=localhost -DaggregationInterval="+aggregationInterval+" -cp target/experiments-1.0-jar-with-dependencies.jar scads.director.ParseXtraceReports")
-			deployed = true
+			// deploy monitoring
+			cal monitoringCfg = Json.build( Map("recipes"->Array("scads::monitoring"),
+										 	"monitoring"->Map(	"basedir"->"/mnt/monitoring",
+														 		"metricService"->Map("port"->6001,"dbhost"->"localhost","dbuser"->"root","dbpassword"->"","dbname"->"metrics"),
+																"xtrace_parser"->Map("nBins"->nBins,"minKey"->minKey,"maxKey"->maxKey,"aggregationInterval"->aggregationInterval)
+														)))
+		    //monitoringVM.deploy(monitoringCfg)
 		}
 	}
 
 	override def waitUntilDeployed = while (!deployed) Thread.sleep(100)
 }
 
-object SCADSMonitoringDeployment {
+/*object SCADSMonitoringDeployment {
 	def loadState(deploymentName,scads):SCADSMonitoringDeployment = {
 		val monitoringVM = try { DataCenter.getInstanceGroupByTag( DataCenter.keyName+"--SCADS--"+scads.scadsName+"--monitoring", true ).getFirst } catch { case _: null }
 		if (monitoringVM!=null) {
@@ -60,4 +61,4 @@ object SCADSMonitoringDeployment {
 		} else 
 			null
 	}
-}
+}*/
