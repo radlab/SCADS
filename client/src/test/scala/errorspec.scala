9a916c3a6c5512f6da90bf8ff0a158578474af4d
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-10-06 01:34:50 -0700

    more binding error tests.

--- client/src/test/scala/errorspec.scala
+++ client/src/test/scala/errorspec.scala
@@ -3,23 +3,27 @@ package edu.berkeley.cs.scads.test
 import org.specs._
 import org.specs.runner.JUnit4
 
+import edu.berkeley.cs.scads.Compiler
 import edu.berkeley.cs.scads.model.parser._
 
 object ErrorSpec extends SpecificationWithJUnit("Scads Compiler Error Specification"){
-	val parser = new ScadsLanguage
-
-	def bind(code: String) =
- 		Binder.bind(parser.parse(code).get)	
-	
 	"The SCADS binder" should {
 		"detect duplicate entities" in {
-					bind("ENTITY User {string name PRIMARY(name)}\n ENTITY User {string name PRIMARY(name)}") must
-						throwA[DuplicateEntityException]
-				}	
-
-		"detect duplicate attributes" in {skip("not written")}
-		"detect duplicate relationships" in {skip("not written")}
-		"detect unknown entities" in {skip("not written")}
+			Compiler.codeGenFromSource("ENTITY User {string name, int nam PRIMARY(name)}\n ENTITY User {string name, int nam PRIMARY(name)}") must
+				throwA[DuplicateEntityException]
+		}
+		"detect duplicate attributes" in {
+			Compiler.codeGenFromSource("ENTITY User {string name, int name PRIMARY(name)}") must
+				throwA[DuplicateAttributeException]
+		}
+		"detect duplicate relationships" in {
+			Compiler.codeGenFromSource("ENTITY User {string name, int nam PRIMARY(name)}\nRELATIONSHIP x FROM User TO ONE User\n RELATIONSHIP x FROM User TO ONE User") must
+				throwA[DuplicateRelationException]
+		}
+		"detect unknown entities" in {
+			Compiler.codeGenFromSource("RELATIONSHIP x FROM unknown TO ONE unknown") must
+				throwA[DuplicateEntityException]
+		}
 		"detect duplicate queries" in {skip("not written")}
 		"detect duplicate parameters" in {skip("not written")}
 		"detect bad ordinals" in {skip("not written")}
@@ -30,6 +34,7 @@ object ErrorSpec extends SpecificationWithJUnit("Scads Compiler Error Specificat
 		"detect unknown attributes" in {skip("not written")}
 		"detect unknown fetch alias" in {skip("not written")}
 		"detect inconsistent parameter typing" in {skip("not written")}
+		"allow entities with only one attribute" in {skip("not written")}
 	}
 }
 
