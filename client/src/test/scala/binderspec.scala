a6bccc194f59d3234bd5c74fcc9c354467bf019d
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-10-07 13:28:00 -0700

    Clean up tests that are redundant w/ stephen's

---
+++ client/src/test/scala/binderspec.scala
@@ -0,0 +1,47 @@
+package edu.berkeley.cs.scads.test
+
+import org.specs._
+import org.specs.runner.JUnit4
+
+import edu.berkeley.cs.scads.Compiler
+import edu.berkeley.cs.scads.model.parser._
+
+object BinderSpec extends SpecificationWithJUnit("Scads Compiler Error Specification"){
+	"The SCADS binder" should {
+		"throw an error for" >> {
+			"duplicate entities" in {
+				Compiler.codeGenFromSource("ENTITY User {string name, int nam PRIMARY(name)}\n ENTITY User {string name, int nam PRIMARY(name)}") must
+					throwA[DuplicateEntityException]
+			}
+			"duplicate attributes" in {
+				Compiler.codeGenFromSource("ENTITY User {string name, int name PRIMARY(name)}") must
+					throwA[DuplicateAttributeException]
+			}
+			"duplicate relationships" in {
+				Compiler.codeGenFromSource("ENTITY User {string name, int nam PRIMARY(name)}\nRELATIONSHIP x FROM User TO ONE User\n RELATIONSHIP x FROM User TO ONE User") must
+					throwA[DuplicateRelationException]
+			}
+			"unknown entities" in {
+				Compiler.codeGenFromSource("RELATIONSHIP x FROM unknown TO ONE unknown") must
+					throwA[DuplicateEntityException]
+			}
+			"duplicate queries" in {skip("not written")}
+			"duplicate parameters" in {skip("not written")}
+			"bad ordinals" in {skip("not written")}
+			"ambiguious this params" in {skip("not written")}
+			"unknown relationships" in {skip("not written")}
+			"ambiguious joins" in {skip("not written")}
+			"ambiguious attributes" in {skip("not written")}
+			"unknown attributes" in {skip("not written")}
+			"unknown fetch alias" in {skip("not written")}
+			"inconsistent parameter typing" in {skip("not written")}
+		}
+		"allow" >> {
+			"entities with only one attribute" in {
+				Compiler.codeGenFromSource("ENTITY User {string name PRIMARY(name)}") mustMatch("User")
+			}
+		}
+	}
+}
+
+class BinderTest extends JUnit4(BinderSpec)
