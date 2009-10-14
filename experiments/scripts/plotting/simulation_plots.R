4f27766199280cc07cb7e90919274db174ba11a3
Peter Bodik <bodikp@gmail.com>
2009-10-14 01:52:14 -0700

    updating plots

--- experiments/scripts/plotting/simulation_plots.R
+++ experiments/scripts/plotting/simulation_plots.R
@@ -3,6 +3,58 @@
 SERVER.COST = 1
 VIOLATION.COST = 100
 
+history = function() {
+	d = read.csv("~/Downloads/scads/simulation/simulation_search_down0.csv")
+	plot.tradeoff(d,"p.alpha_down",logx=T)
+	d = read.csv("~/Downloads/scads/simulation/simulation_search_safety0.csv")
+	plot.tradeoff(d,"p.safety",logx=F)
+	d = read.csv("~/Downloads/scads/simulation/simulation_search_up0.csv")
+	plot.tradeoff(d,"p.alpha_up",logx=F)
+	head(d)
+	dim(d)
+	d
+}
+
+superpose.eb <- function (x, y, ebl, ebu = ebl, length = 0.08, ...) {
+	arrows(x, y + ebu, x, y - ebl, angle = 90, code = 3, length = length, ...)
+}
+
+plot.tradeoff = function(data, x, y1="nServerUnits", y2="percentageSlow", logx=FALSE) {
+	params = grep( "p\\.", colnames(data) )
+	x.param = grep( x, colnames(data))
+
+	d.mean = aggregate( d, by=list(x=d[,x]), FUN=mean)
+	d.mean = d.mean[ order(d.mean$x), ]
+	d.sd = aggregate( d, by=list(x=d[,x]), FUN=sd)
+	d.sd = d.sd[ order(d.sd$x), ]
+	
+	xd = sort( unique( d[,x] ) )
+	
+	logxs = ""
+	if (logx) logxs = "x"
+	
+	y1lim = c(0,max(data[,y1]))
+	if (y1=="percentageSlow") y1lim = c(0,1)
+	y2lim = c(0,max(data[,y2]))
+	if (y2=="percentageSlow") y2lim = c(0,1)
+	
+	print( d.mean[,c(x,y1,y2)])
+	
+	par(mar=c(4, 4, 2, 4) + 0.1)
+	par(mgp=c(2,0.8,0))
+	plot( xd, d.mean[,y1], type="b", col="blue", ylim=y1lim, xlab=x, ylab="", bty="n", yaxt="n", main=paste(y1," and ",y2,sep=""), log=logxs )
+	superpose.eb( xd, d.mean[,y1], d.sd[,y1], length=0.05, col="blue" )
+	axis(2, col.axis="blue", las=2)
+	mtext(y1, side=2, line=2.3, cex.lab=1, las=3, col="blue")
+
+	par(new=T)
+	plot( xd, d.mean[,y2], axes=F, type="b", col="red", ylim=y2lim, xlab="", ylab="", bty="n", log=logxs)
+	axis(4, col.axis="red", las=2)
+	superpose.eb( xd, d.mean[,y2], d.sd[,y2], length=0.05, col="red" )
+	mtext(y2, side=4, line=2, cex.lab=1, las=3, col="red")
+
+}
+
 
 plot.slice = function(data, x) {
 	params = grep( "p\\.", colnames(data) )
--- experiments/scripts/wikipedia/wikipediaPlots.R
+++ experiments/scripts/wikipedia/wikipediaPlots.R
@@ -1,13 +1,22 @@
 
 
+history = function() {
+	wiki10 = read.csv("~/workspace/data/wikipedia/wiki_10buckets.csv")
+	plot.wikipedia.buckets(wiki10)
+
+	wiki1 = data.frame( hour=wiki10$hour, all=apply( wiki10[,2:10], 1, sum ) )
+	plot.wikipedia.buckets(wiki1)
+}
+
 plot.wikipedia.buckets = function(data) {
 	n = ncol(data)-1
 	
 	colors = rainbow(n)
 	
-	plot(data[,2],type="l",bty="n",ylim=c(0,max(data[,2:ncol(data)])),col=colors[1],
-		xlab="hours", ylab="hits/hour")
-	for (i in 3:(n+1)) lines(data[,i],col=colors[i-1])
+	par(cex=1.4)
+	plot(data[,2]/1000,type="l",bty="n",ylim=c(0,max(data[,2:ncol(data)]/1000)),col=colors[n],
+		xlab="hours", ylab="hits/hour [x 1000]", lwd=3, cex=2)
+	for (i in 3:(n+1)) lines(data[,i]/1000,col=colors[n-i+1], lwd=3)
 }
 
 seq.of.histograms = function(samples, nbuckets) {
