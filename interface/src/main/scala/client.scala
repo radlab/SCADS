c1cf7561bf5d2dea198a9b30cc3e0104ad07a26e
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-10-21 14:39:21 -0700

    Switch to generic ThriftClient

---
+++ interface/src/main/scala/client.scala
@@ -0,0 +1,52 @@
+package edu.berkeley.cs.scads.thrift
+
+import java.util.concurrent.ConcurrentLinkedQueue
+import org.apache.log4j.Logger
+import org.apache.thrift.transport.{TFramedTransport, TSocket}
+
+import org.apache.thrift.protocol.TBinaryProtocol
+import org.apache.thrift.protocol.XtBinaryProtocol
+
+
+
+class ConnectionPoolManager[ClientType](implicit manifest : scala.reflect.Manifest[ClientType]) {
+	val logger = Logger.getLogger("scads.connectionpool")
+	val clientConstructor = manifest.erasure.getConstructors()(0)
+	val pools = new scala.collection.mutable.HashMap[(String, Int), ConcurrentLinkedQueue[ClientType]]
+
+	def getPool(host: String, port:Int): ConcurrentLinkedQueue[ClientType] = {
+		synchronized {
+			pools.get((host, port)) match {
+				case Some(pool) => pool
+				case None => {
+					val newPool = new ConcurrentLinkedQueue[ClientType]
+					pools.put((host, port), newPool)
+					newPool
+				}
+			}
+		}
+	}
+
+	def newClient(host: String, port: Int): ClientType = {
+	  val transport = new TFramedTransport(new TSocket(host, port))
+    val protocol = if (System.getProperty("xtrace")!=null) {new XtBinaryProtocol(transport)} else {new TBinaryProtocol(transport)}
+ 		clientConstructor.newInstance(protocol).asInstanceOf[ClientType]
+	}
+}
+
+class ThriftClient[ClientType](host: String, port: Int, cpm: ConnectionPoolManager[ClientType]) {
+	val pool = cpm.getPool(host, port)
+
+	def useConnection[ReturnType](func: ClientType => ReturnType): ReturnType = {
+		val conn = pool.poll() match {
+			case null => cpm.newClient(host, port)
+			case c => c
+		}
+		val result = func(conn)
+		pool.add(conn)
+		return result
+	}
+}
+
+object StorageNodePool extends ConnectionPoolManager[StorageEngine.Client]
+class StorageNode(host: String, port: Int) extends ThriftClient(host, port, StorageNodePool)
