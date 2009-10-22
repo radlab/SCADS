baa131233422a81346114e2aa522417e396f5302
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-10-21 19:08:04 -0700

    Drop the plan classes since we are going to try just using ExecutionNodes

--- client/src/main/scala/model/plan.scala
+++
@@ -1,8 +0,0 @@
-package edu.berkeley.cs.scads.model.parser
-
-abstract sealed class Plan
-abstract class EntityProvider extends Plan
-case class Materialize(entityType: String, child: TupleProvider) extends EntityProvider
-
-abstract class TupleProvider extends Plan
-case class AttributeKeyedIndexGet(index: AttributeKeyedIndex, values: List[BoundValue]) extends TupleProvider
