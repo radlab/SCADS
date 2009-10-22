9b3f0101527328e5cc58f80d81a7deed4be65c18
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-10-21 18:31:38 -0700

    Log4j setup and easy level change for console.

--- experiments/client/scadrclient/setup.scala
+++ experiments/client/scadrclient/setup.scala
@@ -1,6 +1,9 @@
 import edu.berkeley.cs.scads.model.Environment
 import edu.berkeley.cs.scads.model.{TrivialExecutor,TrivialSession}
 import edu.berkeley.cs.scads.TestCluster
+import org.apache.log4j._
+import org.apache.log4j.Level._
+
 
 implicit val env = new Environment
 env.placement = new TestCluster
--- experiments/client/scadrclient/src/main/scala/console.scala
+++ experiments/client/scadrclient/src/main/scala/console.scala
@@ -3,7 +3,7 @@ import org.apache.log4j.PropertyConfigurator
 
 object ScadrConsole {
    def main(args : Array[String]) {
-     //PropertyConfigurator.configure("src/main/resources/log4j.console.properties")
+     PropertyConfigurator.configure("src/main/resources/log4j.console.properties")
      MainGenericRunner.main(Array("-i", "setup.scala") ++ args)
      // After the repl exits, then exit the scala script
      exit(0)
