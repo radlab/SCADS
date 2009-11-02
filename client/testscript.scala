1d15c3d77112996069f40d738e58101a004ffbac
Stephen Tu <stephentu@it-programmers.(none)>
2009-11-02 10:35:17 -0800

    fixed dependencies in testscript

--- client/testscript.scala
+++ client/testscript.scala
@@ -1,19 +1,23 @@
 import edu.berkeley.cs.scads.model.TrivialExecutor
 import edu.berkeley.cs.scads.model.TrivialSession
+import edu.berkeley.cs.scads.model.Environment
 import edu.berkeley.cs.scads.TestCluster
 import org.apache.log4j.BasicConfigurator
 
 BasicConfigurator.configure()
 
-implicit val env = ScadsEnv
-
-ScadsEnv.placement = new TestCluster
-ScadsEnv.session = new TrivialSession
-ScadsEnv.executor = new TrivialExecutor()(ScadsEnv)
+implicit val env = new Environment
+env.placement = new TestCluster
+env.session = new TrivialSession
+env.executor = new TrivialExecutor
 
 val u = new user
 u.name("marmbrus")
 u.email("marmbrus@cs.berkeley.edu")
 u.save
+
+val t = new thought
+t.timestamp(1)
+t.save
 
 println(Queries.userByName("marmbrus").apply(0).email)
