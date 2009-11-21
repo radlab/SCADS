70ee8a557a80b25bc1a325049937aa0d41f54113
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-11-21 12:15:48 -0800

    Add test for prefix join sorting.

--- client/src/test/scala/optimizerspec.scala
+++ client/src/test/scala/optimizerspec.scala
@@ -7,8 +7,10 @@ import edu.berkeley.cs.scads.Compiler
 import edu.berkeley.cs.scads.model.parser.ScalaCompiler
 import edu.berkeley.cs.scads.model._
 
+import org.apache.log4j.Logger
 
 object QueryExecSpec extends SpecificationWithJUnit("PIQL Query Execution Specification"){
+	val log = Logger.getLogger("scads.test.queryexec")
 	val rand = new scala.util.Random
 
 	"The SCADS Optimizer" should {
@@ -29,7 +31,7 @@ object QueryExecSpec extends SpecificationWithJUnit("PIQL Query Execution Specif
 				})
 			}
 
-			"ascending sorted integers whith no join" in {
+			"ascending sorted integers with no join" in {
 				implicit val loader = createLoader("ENTITY e1 {int a1, int a2 PRIMARY(a1)}\n QUERY q1 FETCH e1 ORDER BY a2 ASC LIMIT 10 MAX 10")
 				val entities = (1 to 100).toList.map(i => {
 					createEntity("e1", Map("a1" -> rand.nextInt, "a2" -> i))
@@ -37,11 +39,31 @@ object QueryExecSpec extends SpecificationWithJUnit("PIQL Query Execution Specif
 
 				execQuery("q1") must containInOrder(entities.slice(0,10))
 			}
+
+			"ascending sorted integers with prefix join" in {
+				implicit val loader = createLoader("""
+					ENTITY e1 {int a1 PRIMARY(a1)}
+					ENTITY e2 {int a1, int a2 PRIMARY(r,a1)}
+					RELATIONSHIP r FROM e1 TO MANY e2
+					QUERY q1 FETCH e2 OF e1 BY r WHERE e1 = [this] ORDER BY a2 ASC LIMIT 10 MAX 10""")
+
+				val e1 = createEntity("e1", Map("a1" -> 1))
+
+				val entities = (1 to 100).toList.map(i => {
+					createEntity("e2", Map("a1" -> rand.nextInt, "a2" -> i, "r" -> e1))
+				})
+
+				execQuery(e1, "q1") must containInOrder(entities.slice(0,10))
+			}
 		}
 	}
 
 	def execQuery(name: String, args: Any*)(implicit loader: ClassLoader, env: Environment): Seq[Entity] = {
 		val queryObject = loader.loadClass("Queries")
+		execQuery(queryObject, name, args:_*)
+	}
+
+	def execQuery(obj: AnyRef, name: String, args: Any*)(implicit loader: ClassLoader, env: Environment): Seq[Entity] = {
 		val allArgs = args ++ Array(env)
 		val argTypes = allArgs.map(_ match {
 			case _: Int => classOf[Int]
@@ -51,9 +73,12 @@ object QueryExecSpec extends SpecificationWithJUnit("PIQL Query Execution Specif
 			case i: Int => new java.lang.Integer(i)
 			case r: AnyRef => r
 		})
-		val queryMethod = queryObject.getMethod("q1", argTypes: _*)
+		val queryMethod = obj match {
+			case c: Class[_] => c.getMethod("q1", argTypes: _*)
+			case a: AnyRef => a.getClass.getMethod("q1", argTypes: _*)
+		}
 
-		queryMethod.invoke(queryObject, boxedArgs: _*).asInstanceOf[Seq[Entity]]
+		queryMethod.invoke(obj, boxedArgs: _*).asInstanceOf[Seq[Entity]]
 	}
 
 	def createEntity(name: String, fields: Map[String, Any])(implicit loader: ClassLoader, env: Environment): Entity = {
@@ -62,6 +87,7 @@ object QueryExecSpec extends SpecificationWithJUnit("PIQL Query Execution Specif
 			a._2 match {
 				case i: Int => e.attributes(a._1).asInstanceOf[IntegerField].apply(i)
 				case s: String => e.attributes(a._1).asInstanceOf[StringField].apply(s)
+				case ent: Entity => e.attributes(a._1).apply(ent)
 			}
 		})
 		e.save
