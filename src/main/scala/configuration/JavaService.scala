5de19dd8815b3574db10cb69b9cbc9988d089ed0
Stephen Tu <stephen_tu@berkeley.edu>
2009-11-22 15:36:25 -0800

    added JVM args for JavaService

--- src/main/scala/configuration/JavaService.scala
+++ src/main/scala/configuration/JavaService.scala
@@ -2,9 +2,17 @@ package deploylib.configuration
 
 import deploylib.configuration.ValueConverstion._
 
-class JavaService(localJar: String, className: String, args: String) extends CompositeConfiguration {
+class JavaService(localJar: String, className: String, args: String, jvmArgs:String) extends CompositeConfiguration {
+
+    def this(localJar: String, className: String, args: String) = 
+        this(localJar,className,args,null)
+
 	val remoteJar = new FileUpload(localJar, MachineRoot)
-	val runCmd = new LateStringBuilder(RunitService.header, "/usr/lib/jvm/java-6-sun/bin/java -cp .:", new LateBoundValue("JarLocation" ,remoteJar.getValue(_)), " ", className, " ", args)
+	val runCmd = jvmArgs match {
+        case null => new LateStringBuilder(RunitService.header, "/usr/lib/jvm/java-6-sun/bin/java -cp .:", new LateBoundValue("JarLocation" ,remoteJar.getValue(_)), " ", className, " ", args)
+        case args => new LateStringBuilder(RunitService.header, "/usr/lib/jvm/java-6-sun/bin/java ", args, " -cp .:", new LateBoundValue("JarLocation" ,remoteJar.getValue(_)), " ", className, " ", args) 
+    }
+
 	val service = new DefaultLoggedRunitService(className, runCmd)
 	val log4jProperties = new RemoteFile(service.baseDirectory,
 																			 "log4j.properties",
