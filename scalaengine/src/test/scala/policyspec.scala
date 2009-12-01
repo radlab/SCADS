3fdbc555d14dc3b2a461fd115df24766805cd4de
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-12-01 15:21:55 -0800

    Add a placeholder for responsibiliy policy testing.

---
+++ scalaengine/src/test/scala/policyspec.scala
@@ -0,0 +1,11 @@
+package edu.berkeley.cs.scads.storage
+
+import org.specs._
+import org.specs.runner.JUnit4
+
+object ResponsibilityPolicySpec extends SpecificationWithJUnit("KeyStore Specification") {
+
+
+}
+
+class ResponsibilityPolicyTest extends JUnit4(ResponsibilityPolicySpec)
