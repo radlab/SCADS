a3213f913d446b272d7b0e5e4ac2792b77e065c2
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-12-03 16:52:12 -0800

    whitespace.

--- src/main/scala/configuration/JavaService.scala
+++ src/main/scala/configuration/JavaService.scala
@@ -5,13 +5,13 @@ import deploylib.configuration.ValueConverstion._
 @deprecated
 class JavaService(localJar: String, className: String, args: String, jvmArgs:String) extends CompositeConfiguration {
 
-	def this(localJar: String, className: String, args: String) = 
+	def this(localJar: String, className: String, args: String) =
 		this(localJar,className,args,null)
 
 	val remoteJar = new FileUpload(localJar, MachineRoot)
 	val runCmd = jvmArgs match {
         case null => new LateStringBuilder(RunitService.header, "/usr/lib/jvm/java-6-sun/bin/java -cp .:", new LateBoundValue("JarLocation" ,remoteJar.getValue(_)), " ", className, " ", args)
-        case args => new LateStringBuilder(RunitService.header, "/usr/lib/jvm/java-6-sun/bin/java ", args, " -cp .:", new LateBoundValue("JarLocation" ,remoteJar.getValue(_)), " ", className, " ", args) 
+        case args => new LateStringBuilder(RunitService.header, "/usr/lib/jvm/java-6-sun/bin/java ", args, " -cp .:", new LateBoundValue("JarLocation" ,remoteJar.getValue(_)), " ", className, " ", args)
     }
 
 	val service = new DefaultLoggedRunitService(className, runCmd)
