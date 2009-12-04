82b9d71387980c18d63e552ed3c516732594d7a3
rean <rean@hydro.(none)>
2009-12-04 15:04:08 -0800

    Added classes to work with loCal ATOMs (physical nodes rather than EC2 virtual machines

---
+++ src/main/scala/physical/Tag.scala
@@ -0,0 +1,11 @@
+package deploylib.physical
+/*
+ * Tag.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+class Tag( val name: String, val persistent: Boolean ) {
+
+}
