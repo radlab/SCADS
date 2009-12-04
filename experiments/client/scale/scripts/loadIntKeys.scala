abbf76aafac98ec99819307950eb1c4f3158bd05
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-12-03 20:19:35 -0800

    working deploy script.

--- experiments/client/scale/scripts/loadIntKeys.scala
+++ experiments/client/scale/scripts/loadIntKeys.scala
@@ -1,34 +1,47 @@
 import scaletest._
 import deploylib.rcluster._
 import edu.berkeley.cs.scads.thrift._
+import org.apache.log4j.Logger
+import deploylib.Util
+
+settings.maxPrintString = 1000000
 
 object Exp {
-	val nodes = List(r6,r8,r10,r11,r12)
-	val testSize = 1000
-	val partitions = IntTestDeployment.createPartitions(testSize, nodes)
+	val logger = Logger.getLogger("script")
 
+	val nodes = List(r26, r16, r31, r30)
+	val testSize = 100000
+	val partitions = IntTestDeployment.createPartitions(testSize, nodes.size)
+
+	logger.info("Cleaning up")
 	nodes.foreach(_.clearAll)
 	nodes.foreach(_.executeCommand("killall java"))
 	nodes.foreach(_.setupRunit)
 
+	logger.info("Configuring zookeeper")
 	val zooNode = nodes(0)
 	val zooService = ScadsDeployment.deployZooKeeperServer(zooNode)
-	zooService.watchLog
+	zooService.watchFailures
 	zooService.start
 	zooService.blockTillUpFor(5)
+	zooNode.blockTillPortOpen(ScadsDeployment.zookeeperPort)
 
+
+	logger.info("Configuring storage engines")
 	val storageServices = nodes.map(ScadsDeployment.deployStorageEngine(_, zooNode))
-	storageServices.foreach(_.watchLog)
+	storageServices.foreach(_.watchFailures)
 	storageServices.foreach(_.start)
 	storageServices.foreach(_.blockTillUpFor(5))
+	nodes.foreach(_.blockTillPortOpen(ScadsDeployment.storageEnginePort))
 
-	val loadServices = partitions.map(p => {
-		val n = StorageNode(p.node.hostname, ScadsDeployment.storageEnginePort)
-		n.useConnection(_.set_responsibility_policy("intKeys", RangedPolicy.convert((p.start, p.end))))
-
-		IntTestDeployment.deployIntKeyLoader(p.node, p.start, p.end, zooNode.hostname + ":" + ScadsDeployment.zookeeperPort)
+	val loadServices = partitions.zip(nodes).map(p => {
+		logger.info("Setting range policy for: " + p)
+		val n = StorageNode(p._2.hostname, ScadsDeployment.storageEnginePort)
+		Util.retry(5)(() => {
+			n.useConnection(_.set_responsibility_policy("intKeys", RangedPolicy.convert((p._1.start, p._1.end))))
+		})
+		IntTestDeployment.deployIntKeyLoader(p._2, "SingleConnectionPoolLoader", p._1.start, p._1.end, zooNode.hostname + ":" + ScadsDeployment.zookeeperPort)
 	})
-
+	loadServices.foreach(_.watchFailures)
 	loadServices.foreach(_.once)
-
 }
