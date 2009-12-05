c246bda0e33662b58c224ef94aac2e8f35cfff18
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-12-04 20:45:28 -0800

    Add memory to java services.

--- experiments/client/scale/src/main/scala/configuration.scala
+++ experiments/client/scale/src/main/scala/configuration.scala
@@ -10,16 +10,19 @@ object ScadsDeployment extends ConfigurationActions {
 	val storageEnginePort = 9090
 	val zookeeperPort = 2181
 
-	def deployStorageEngine(target: RunitManager, zooServer: RemoteMachine): RunitService = {
+	def deployStorageEngine(target: RunitManager, zooServer: RemoteMachine, bulkLoad: Boolean): RunitService = {
+    val bulkLoadFlag = if(bulkLoad) " -b " else ""
 		createJavaService(target, new File("target/scale-1.0-SNAPSHOT-jar-with-dependencies.jar"),
 			"edu.berkeley.cs.scads.storage.JavaEngine",
-			"-p " + storageEnginePort + " -z " + zooServer.hostname + ":" + zookeeperPort)
+      1024,
+			"-p " + storageEnginePort + " -z " + zooServer.hostname + ":" + zookeeperPort + bulkLoadFlag)
 	}
 
 	def deployZooKeeperServer(target: RunitManager): RunitService = {
 		val zooStorageDir = createDirectory(target, new File(target.rootDirectory, "zookeeperdata"))
 		val zooService = createJavaService(target, new File("target/scale-1.0-SNAPSHOT-jar-with-dependencies.jar"),
 			"org.apache.zookeeper.server.quorum.QuorumPeerMain",
+      1024,
 			"zoo.cnf")
 		val zooConfigData = Array("tickTime=2000", "initLimit=10", "syncLimit=5", "clientPort=" + zookeeperPort, "dataDir=" + zooStorageDir).mkString("", "\n", "\n")
 		val zooConfigFile = createFile(target, new File(zooService.serviceDir, "zoo.cnf"), zooConfigData, "644")
