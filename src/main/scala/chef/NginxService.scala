0b0e5f50f452e7db00f41f7af3301a0edacfe672
Aaron Beitch <aaron.beitch@gmail.com>
2009-12-06 16:42:53 -0800

    Added JSON import statements to all ChefServices.

--- src/main/scala/chef/NginxService.scala
+++ src/main/scala/chef/NginxService.scala
@@ -1,5 +1,7 @@
 package deploylib.chef
 
+import org.json.JSONObject
+import org.json.JSONArray
 import deploylib._
 
 /*************************
@@ -51,6 +53,30 @@ case class NginxService(remoteMachine: RemoteMachine,
       // Use haproxy service.
     }
     // TODO: Execute command to run recipe
+  }
+  
+  override def getJSONConfig: String = {
+    val nginxConfig = new JSONObject()
+    val nginxRecipes = new JSONArray()
+    nginxRecipes.put("cloudstone::nginx")
+    nginxRecipes.put("cloudstone::faban-agent")
+    nginxConfig.put("recipes", nginxRecipes)
+    val nginxNginx = new JSONObject()
+    val nginxNginxServers = new JSONObject()
+    
+    haproxy.foreach(instance => {
+      val server = new JSONObject()
+      server.put("start", 4000)
+      server.put("count", 1)
+      nginxNginxServers.put(instance.privateDnsName, server)
+    })
+    nginxNginx.put("servers", nginxNginxServers)
+    nginxConfig.put("nginx", nginxNginx)
+    
+    val nginxFaban = new JSONObject()
+    nginxFaban.put("mysql", false)
+    nginxFaban.put("postgresql", false)
+    nginxConfig.put("faban", nginxFaban)
   }
   
 }
