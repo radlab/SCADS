b5047a852311ff2121679c080ff27503366c1459
Timothy Yung <yungsters@gmail.com>
2009-12-06 16:23:48 -0800

    Added comments for all ChefService subclasses.

--- src/main/scala/chef/FabanAgentService.scala
+++ src/main/scala/chef/FabanAgentService.scala
@@ -2,6 +2,15 @@ package deploylib.chef
 
 import deploylib._
 
+/*************************
+{
+    "faban": {
+        "jdbc": "mysql"
+    },
+    "recipes": ["cloudstone::faban-agent"]
+}
+*************************/
+
 case class FabanAgentService(remoteMachine: RemoteMachine,
                              config: Map[String,Any]) extends ChefService(remoteMachine, config) {
   val cookbookName = "cloudstone"
--- src/main/scala/chef/HAProxyService.scala
+++ src/main/scala/chef/HAProxyService.scala
@@ -2,6 +2,24 @@ package deploylib.chef
 
 import deploylib._
 
+/*************************
+{
+    "recipes": ["cloudstone::haproxy"],
+    "haproxy": {
+        "servers": {
+            "localhost": {
+                "start": 3000,
+                "count": 2
+            }
+        },
+        "metric_service": {
+            "host": null,
+            "port": null
+        }
+    }
+}
+*************************/
+
 case class HAProxyService(remoteMachine: RemoteMachine,
                           config: Map[String,Any]) extends ChefService(remoteMachine, config) {
   val cookbookName = "cloudstone"
