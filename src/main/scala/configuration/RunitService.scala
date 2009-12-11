918260fbc9e344fea555d8151c118c52d2df26f6
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-12-10 19:38:22 -0800

    Deprecate more things that rely on the old configuration framework.

--- src/main/scala/configuration/RunitService.scala
+++ src/main/scala/configuration/RunitService.scala
@@ -2,14 +2,17 @@ package deploylib.configuration
 
 import deploylib.configuration.ValueConverstion._
 
+@deprecated()
 object ServiceRoot extends RemoteDirectory(MachineRoot, "services")
 
+@deprecated()
 object RunitService {
 	val shell = "#!/bin/sh\n"
 	val redirect = "exec 2>&1\n"
 	val header = shell + redirect + "exec "
 }
 
+@deprecated()
 class RunitService(name: String, runCommand: Value[String], logCommand: Value[String]) extends CompositeConfiguration {
 	val baseDirectory =  new RemoteDirectory(ServiceRoot, name)
 	val logDirectory = new RemoteDirectory(baseDirectory, "log")
@@ -21,5 +24,6 @@ class RunitService(name: String, runCommand: Value[String], logCommand: Value[St
 	def description: String = "Create RunitService " + name + " w/ cmd " + runCommand + " and logCmd " + logCommand
 }
 
+@deprecated()
 class DefaultLoggedRunitService(name: String, runCommand: Value[String]) extends
 	RunitService(name, runCommand, new LateBoundValue("logger to <bindir>/svlogdCmd", (target: RemoteMachine) => "#!/bin/sh\nexec " + target.svlogdCmd + " -tt ./"))
