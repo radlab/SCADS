cae6ed3b783bc9217d156d4ca32fb00bab3c477e
Michael Armbrust <marmbrus@cs.berkeley.edu>
2009-12-22 13:46:34 -0500

    Add threading to scadr loader.

--- experiments/client/scale/src/main/scala/Scadr.scala
+++ experiments/client/scale/src/main/scala/Scadr.scala
@@ -22,7 +22,7 @@ object UserCountTest {
 
 	def run() = test = Future {
 		List(10, 100, 1000, 10000, 50000, 100000, 500000).foreach(users => {
-			le = new ScadrLoadExp(nodes, users, 20, 10)
+			le = new ScadrLoadExp(nodes, 1, users, 20, 10)
 			le.postTestCollection()
 			re = new ScadrReadExp(nodes, users, 5)
 			re.postTestCollection()
@@ -30,7 +30,20 @@ object UserCountTest {
 	}
 }
 
-class ScadrLoadExp(nodes: List[RunitManager], users: Int, thoughts: Int, following: Int) {
+object ThreadedUserCreateTest {
+	val nodes = EC2Instance.myInstances
+	var le: ScadrLoadExp = null
+	var test: Future[Unit] = null
+
+	def run = test = Future {
+		List(5,10,15,20,25,30,35,40).foreach(threads => {
+			le = new ScadrLoadExp(nodes, threads, 1000, 20, 10)
+			le.postTestCollection()
+		})
+	}
+}
+
+class ScadrLoadExp(nodes: List[RunitManager], threads: Int, users: Int, thoughts: Int, following: Int) {
 	val logger = Logger.getLogger("script")
 	val partitions = IntTestDeployment.createPartitions(users, nodes.size)
 
@@ -43,7 +56,7 @@ class ScadrLoadExp(nodes: List[RunitManager], users: Int, thoughts: Int, followi
 //		Util.retry(5) {n.useConnection(_.set_responsibility_policy("intKeys", RangedPolicy.convert((p._1.start, p._1.end))))}
 		Util.retry(5) {Queries.configureStorageEngine(n)}
 
-		ScadsDeployment.deployLoadClient(p._2, "CreateScadrUsers", Map("zookeeper" -> cluster.zooUri, "startuser" -> p._1.start, "enduser" -> p._1.end, "thoughts" -> thoughts.toString, "following" -> following.toString, "maxuser" -> users.toString))
+		ScadsDeployment.deployLoadClient(p._2, "CreateScadrUsers", Map("zookeeper" -> cluster.zooUri, "startuser" -> p._1.start, "enduser" -> p._1.end, "thoughts" -> thoughts.toString, "following" -> following.toString, "maxuser" -> users.toString, "threads" -> threads.toString))
 	})
 
 	loadServices.foreach(_.watchFailures)
@@ -103,6 +116,7 @@ abstract class ScadrTest extends ScadsTest {
 
 
 object CreateScadrUsers extends ScadrTest {
+	options.addOption("r", "threads", true, "the number of threads to create")
 	options.addOption("s", "startuser", true, "the userId to start with")
 	options.addOption("e", "enduser", true, "the userId to end with")
 	options.addOption("t", "thoughts", true, "the number of thoughts per user")
@@ -114,39 +128,42 @@ object CreateScadrUsers extends ScadrTest {
 		val thoughts = getIntOption("thoughts")
 		val following = getIntOption("following")
 
-		XResult.recordResult(
-			XResult.benchmark {
-				(startuser to (enduser - 1)).foreach(id => {
-					val u = new user
-					u.name(makeUsername(id))
-					u.password("pass" + u)
-					Util.retry(5) {XResult.recordException{u.save}}
+		val results = IntTestDeployment.createPartitions(startuser, enduser - 1, getIntOption("threads")).map(p =>
+			Future {
+				XResult.benchmark {
+					(p.start.toInt to (p.end.toInt - 1)).foreach(id => {
+						val u = new user
+						u.name(makeUsername(id))
+						u.password("pass" + u)
+						Util.retry(5) {XResult.recordException{u.save}}
 
-					(1 to thoughts).foreach(tId => {
-						val t = new thought
-						t.owner(u)
-						t.timestamp(tId)
-						t.thought("User " + id + "is thinking:" + tId)
-						Util.retry(5) {XResult.recordException{t.save}}
-					})
+						(1 to thoughts).foreach(tId => {
+							val t = new thought
+							t.owner(u)
+							t.timestamp(tId)
+							t.thought("User " + id + "is thinking:" + tId)
+							Util.retry(5) {XResult.recordException{t.save}}
+						})
 
-					val followedUsers = new HashSet[String]()
-					while(followedUsers.size < following) {
-						val friend = randomUsername()
-						if(!followedUsers.contains(friend)) {
-							followedUsers += friend
-							val s = new subscription
-							s.owner(u)
-							s.target(friend)
-							s.approved(true)
-							Util.retry(5) {XResult.recordException{s.save}}
+						val followedUsers = new HashSet[String]()
+						while(followedUsers.size < following) {
+							val friend = randomUsername()
+							if(!followedUsers.contains(friend)) {
+								followedUsers += friend
+								val s = new subscription
+								s.owner(u)
+								s.target(friend)
+								s.approved(true)
+								Util.retry(5) {XResult.recordException{s.save}}
+							}
 						}
-					}
 
-				})
-				<sequentialUserCreate startUser={getIntOption("startuser").toString} endUser={getIntOption("enduser").toString}/>
+					})
+					<sequentialUserCreate startUser={getIntOption("startuser").toString} endUser={getIntOption("enduser").toString} thoughts={thoughts.toString} following={following.toString}/>
+				}
 			}
 		)
+		XResult.recordResult(results.map(r => <thread>{r()}</thread>))
 	}
 }
 
