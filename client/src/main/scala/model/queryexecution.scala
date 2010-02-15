9eb4215ea0aa0297b8f85f7190456b3b8dcd9cfe
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-02-14 20:46:03 -0800

    Optimizer able to optimize SCADr.

--- client/src/main/scala/model/queryexecution.scala
+++ client/src/main/scala/model/queryexecution.scala
@@ -2,18 +2,24 @@ package edu.berkeley.cs.scads.piql
 
 import scala.collection.mutable.HashMap
 import org.apache.log4j.Logger
+import edu.berkeley.cs.scads.piql.parser.BoundValue
+import org.apache.avro.generic.IndexedRecord
+
+abstract sealed class JoinCondition
+case class AttributeCondition(attrName: String) extends JoinCondition
+case class FieldLiteralCondition(fieldValue: BoundValue) extends JoinCondition
 
 /* Query Plan Nodes */
 abstract sealed class QueryPlan
-/*abstract class TupleProvider extends QueryPlan
+abstract class TupleProvider extends QueryPlan
 abstract class EntityProvider extends QueryPlan
-case class SingleGet(namespace: String, key: Field, policy: ReadPolicy) extends TupleProvider
-case class PrefixGet(namespace: String, prefix: Field, limit: Field, ascending: Boolean, policy: ReadPolicy) extends TupleProvider
-case class SequentialDereferenceIndex(targetNamespace: String, policy: ReadPolicy, child: TupleProvider) extends TupleProvider
-case class PrefixJoin(namespace: String, conditions: List[JoinCondition], limit: Field, ascending: Boolean, policy: ReadPolicy, child: EntityProvider) extends TupleProvider
-case class PointerJoin(namespace: String, conditions: List[JoinCondition], policy: ReadPolicy, child: EntityProvider) extends TupleProvider
-case class Materialize(entityClass: Class[Entity], child: TupleProvider) extends EntityProvider
-case class Selection(equalityMap: HashMap[String, Field], child: EntityProvider) extends EntityProvider
+case class SingleGet(namespace: String, key: List[BoundValue]) extends TupleProvider
+case class PrefixGet(namespace: String, prefix: List[BoundValue], limit: BoundValue, ascending: Boolean) extends TupleProvider
+case class SequentialDereferenceIndex(targetNamespace: String, child: TupleProvider) extends TupleProvider
+case class PrefixJoin(namespace: String, conditions: Seq[JoinCondition], limit: BoundValue, ascending: Boolean, child: EntityProvider) extends TupleProvider
+case class PointerJoin(namespace: String, conditions: Seq[JoinCondition], child: EntityProvider) extends TupleProvider
+case class Materialize(entityType: String, child: TupleProvider) extends EntityProvider
+case class Selection(equalityMap: HashMap[String, BoundValue], child: EntityProvider) extends EntityProvider
 case class Sort(fields: List[String], ascending: Boolean, child: EntityProvider) extends EntityProvider
-case class TopK(k: Field, child: EntityProvider) extends EntityProvider
-*/
+case class TopK(k: BoundValue, child: EntityProvider) extends EntityProvider
+
