ff9708c0512a661a75ee323aea633b8afcd323c0
Beth Trushkowsky <trush@eecs.berkeley.edu>
2010-02-22 13:09:37 -0800

    update to not use storage actor proxy and whatnot

--- scalaengine/src/main/scala/test_generator.scala
+++ scalaengine/src/main/scala/test_generator.scala
@@ -4,6 +4,7 @@ import edu.berkeley.cs.scads.test._
 import edu.berkeley.cs.scads.comm.Conversions._
 import scala.actors._
 import scala.actors.Actor._
+import org.apache.avro.util.Utf8
 import org.apache.log4j.Logger
 
 object ZooKeeperServer extends optional.Application {
@@ -14,9 +15,20 @@ object ZooKeeperServer extends optional.Application {
 	}
 }
 
+//ScadsDeployment.createJavaService(machine, new java.io.File(jarpath), "SetupNodes", 1024, host + " perfTest256")
+object SetupNodes{
+	def main(args: Array[String]): Unit = {
+		val host = args(0)
+
+		val cr = new ConfigureRequest
+		cr.namespace = args(1)
+		cr.partition = "1"
+		Sync.makeRequest(RemoteNode(host,9991), new Utf8("Storage"),cr)
+	}
+}
+
 object CreateDirectorData {
 	val logger = Logger.getLogger("scads.datagen")
-	implicit val proxy = new StorageActorProxy
 	val namespace = "perfTest256"
 	val key = new IntRec
 	var request_count = 0
@@ -41,7 +53,7 @@ object CreateDirectorData {
 		logger.info("Done creating requests")
 		while ((request_count+exception_count) < (maxKey-minKey)) { logger.info("Still waiting for responses"); Thread.sleep(500) }
 		if (exception_count == 0) logger.info("Done warming with no exceptions")
-		else logger.warn("Warming had exceptions!")
+		else logger.warn("Warming had "+exception_count+" exceptions and "+request_count+ " successful requests")
 	}
 	class ScadsWarmerActor(dest:RemoteNode, scads_req:Object) extends Actor {
 		var starttime:Long = -1L; var startNano:Long = -1L
@@ -51,17 +63,19 @@ object CreateDirectorData {
 			startNano = System.nanoTime
 			val req = new Message
 			req.body = scads_req
-			req.src = new java.lang.Long(MessageHandler.registerActor(this))
+			req.dest = new Utf8("Storage")
+			req.src = new java.lang.Long(MessageHandler.registerActor(self))
+			//logger.info("Message sent as "+req.src)
 			makeRequest(req)
 		}
-		def makeRequest(req:Message)(implicit mgr: StorageActorProxy):Object = {
+		def makeRequest(req:Message):Object = {
 			// send the request
-			mgr.sendMessage(dest, req) // go to only first node
+			MessageHandler.sendMessage(dest, req)
 
 			// wait for response
 			reactWithin(10000) {
 				case (RemoteNode(hostname, port), msg: Message) => msg.body match {
-					case exp: ProcessingException => exception_count +=1
+					case exp: ProcessingException => { logger.warn("Exception making request: "+exp); exception_count +=1 }
 					case obj => {
 						endtime = System.currentTimeMillis
 						latency = (System.nanoTime-startNano)/1000000.0
@@ -70,7 +84,7 @@ object CreateDirectorData {
 						//request_info.put(starttime+","+endtime+","+latency+","+hostname+"\n")
 					}
 				}
-				case TIMEOUT => exception_count +=1
+				case TIMEOUT => { logger.warn("Request timeout"); exception_count +=1 }
 				case msg => logger.warn("Unexpected message: " + msg)
 			}
 		}
@@ -121,39 +135,59 @@ class RequestLogger(queue:java.util.concurrent.BlockingQueue[String]) extends Ru
 }
 
 object RequestRunner {
+	val logger = Logger.getLogger("scads.requestgen")
+	val commapattern = java.util.regex.Pattern.compile(",")
+	var wait_time = 5 // ms before sending another request
+	
 	def main(args: Array[String]): Unit = {
+		var num_requests:Long = 0L
 		val numthreads = args(0).toInt
-		val numSeconds = args(1).toInt
+		val numMillis = args(1).toInt
 
 		val mapping = fileToMapping(args(2))
 		val queue = new java.util.concurrent.ArrayBlockingQueue[String](100)
+		val request_queue = new java.util.concurrent.ArrayBlockingQueue[Long](100)
 		val requestlogger = new RequestLogger(queue)
 
-		val gens = (0 until numthreads).map(t=>new RequestGenerator(mapping,queue))
+		val gens = (0 until numthreads).map(t=>new RequestGenerator(mapping,queue,request_queue))
 		val threads = gens.map(g => new Thread(g))
 		val startTime = System.currentTimeMillis
+		var running = true
 
 		// start!
 		(new Thread(requestlogger)).start
 		threads.foreach(t=> t.start)
 
-		while (true) {
-			if (System.currentTimeMillis >= startTime+(numSeconds*1000)) {
+		while (running && !mapping.isEmpty) {
+			if (System.currentTimeMillis >= (startTime+numMillis)) {
+				logger.info("Stopping generators")
 				requestlogger.stop
-				gens.foreach(g=> {g.stop; println(g.total_request_gen_time+","+g.request_count+","+g.exception_count)})
+				gens.foreach(g=> {g.stop; logger.info(g.total_request_gen_time+","+g.request_count+","+g.exception_count)})
+				running = false
 			}
-			Thread.sleep(1*1000)
+			else {
+				request_queue.put(num_requests)
+				num_requests+=1
+				Thread.sleep(wait_time)
+			}
+			//Thread.sleep(100)
 		}
 	}
 	def fileToMapping(filename:String):Map[PolicyRange,RemoteNode] = {
-		null //TODO
+		
+		val f = scala.io.Source.fromFile(filename)
+		val lines = f.getLines
+		Map[PolicyRange,RemoteNode]( lines.map(line=>{
+			val tokens = commapattern.split(line)
+			/*if (tokens.size == 3 )*/ (PolicyRange(tokens(1).toInt,tokens(2).toInt) -> RemoteNode(tokens(0),9991))
+			//else println("Wrong number of tokens in file!")
+		}).toList:_*)
 	}
 }
 
-class RequestGenerator(mapping: Map[PolicyRange,RemoteNode], request_info:java.util.concurrent.BlockingQueue[String]) extends Runnable {
+class RequestGenerator(mapping: Map[PolicyRange,RemoteNode], request_info:java.util.concurrent.BlockingQueue[String], request_queue:java.util.concurrent.BlockingQueue[Long]) extends Runnable {
 	val logger = Logger.getLogger("scads.requestgen")
 	
-	implicit val proxy = new StorageActorProxy
 	var running = true
 	val rnd = new java.util.Random
 	
@@ -171,7 +205,6 @@ class RequestGenerator(mapping: Map[PolicyRange,RemoteNode], request_info:java.u
 	val ranges = Array[PolicyRange](mapping.toList.map(e=>e._1):_*)
 	var minKey = ranges.foldLeft(0)((out,entry)=>{if(entry.minKey<out) entry.minKey else out})
 	var maxKey = ranges.foldLeft(0)((out,entry)=>{if(entry.maxKey>out) entry.maxKey else out})
-	var wait_time = 5 // ms before sending another request
 	
 	class ScadsRequestActor(dest:List[RemoteNode], scads_req:Object,log:Boolean) extends Actor {
 		var starttime:Long = -1L; var startNano:Long = -1L
@@ -181,17 +214,19 @@ class RequestGenerator(mapping: Map[PolicyRange,RemoteNode], request_info:java.u
 			startNano = System.nanoTime
 			val req = new Message
 			req.body = scads_req // get or put request
-			req.src = new java.lang.Long(MessageHandler.registerActor(this))
+			req.dest = new Utf8("Storage")
+			req.src = new java.lang.Long(MessageHandler.registerActor(self))
+			//logger.info("Sending message as "+req.src)
 			makeRequest(req)
 		}
-		def makeRequest(req:Message)(implicit mgr: StorageActorProxy):Object = {
+		def makeRequest(req:Message):Object = {
 			// send the request
-			mgr.sendMessage(dest(0), req) // go to only first node
+			MessageHandler.sendMessage(dest(0), req) // go to only first node
 
 			// wait for response
 			reactWithin(10000) {
 				case (RemoteNode(hostname, port), msg: Message) => msg.body match {
-					case exp: ProcessingException => exception_count +=1
+					case exp: ProcessingException => { logger.warn("Exception making request: "+exp); exception_count +=1}
 					case obj => {
 						endtime = System.currentTimeMillis
 						latency = (System.nanoTime-startNano)/1000000.0
@@ -208,15 +243,18 @@ class RequestGenerator(mapping: Map[PolicyRange,RemoteNode], request_info:java.u
 	
 	def run = {
 		while (running) {
-			startr = System.nanoTime
-			val request = generateRequest
-			request_gen_time = System.nanoTime
-			total_request_gen_time += (request_gen_time-startr)
-			request.start // actually send request
-			Thread.sleep(wait_time)
+			doRequest(request_queue.take)		
 		}
 	}
-	def stop = { running = false }
+	def doRequest(id:Long) = {
+		startr = System.nanoTime
+		val request = generateRequest
+		request_gen_time = System.nanoTime
+		total_request_gen_time += (request_gen_time-startr)
+		//logger.info("Starting request after "+request_gen_time+"-"+startr)
+		request.start // actually send request
+	}
+	def stop = { logger.info("Ending generation"); running = false }
 	private def generateRequest():ScadsRequestActor = {
 		// pick a key
 		key.f1 = rnd.nextInt(maxKey-minKey) + minKey
