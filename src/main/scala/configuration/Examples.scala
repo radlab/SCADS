540b538b2bd694d73a8689332ba867f899c572b5
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-03-01 12:46:42 -0800

    Remove deprecated configuration system.

--- src/main/scala/configuration/Examples.scala
+++
@@ -1,5 +0,0 @@
-package deploylib.configuration
-
-import deploylib.configuration.ValueConverstion._
-
-class ScadsEngineExample(port: Int) extends JavaService("../scads/scalaengine/target/scalaengine-1.0-SNAPSHOT-jar-with-dependencies.jar", "edu.berkeley.cs.scads.storage.JavaEngine", "-p " + port)
