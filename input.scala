bd8e56068011c1757223f2808adda5267753adb9
steve33671 <steve33671@85b7b6fe-2b2e-11df-a6fe-af80f4a49816>
2010-03-22 07:06:18 +0000

    reverted accidental commit in input.scala
    git-svn-id: https://avro-scala-compiler-plugin.googlecode.com/svn/trunk@15 85b7b6fe-2b2e-11df-a6fe-af80f4a49816

--- input.scala
+++ input.scala
@@ -5,10 +5,6 @@ import com.googlecode.avro.annotation.AvroRecord
 @AvroRecord
 case class FooBar(var x: Int, var y: Boolean, var z: String, var b: Array[Byte])
 
-object Baz {
-    val findMe = "abc"
-}
-
 @AvroRecord
 case class Baz(var x: Int, var y: Double)
 
