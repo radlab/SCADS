06958aea111c83dd675e78d37c2c84a2ac2a7d4c
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-03-23 21:22:48 -0700

    Add createNamespace method to TestScalaEngine

--- scalaengine/src/main/scala/TestScalaEngine.scala
+++ scalaengine/src/main/scala/TestScalaEngine.scala
@@ -2,6 +2,9 @@ package edu.berkeley.cs.scads.storage
 
 import edu.berkeley.cs.scads.comm._
 import org.apache.log4j.Logger
+import org.apache.avro.Schema
+import edu.berkeley.cs.scads.comm.Conversions._
+import org.apache.avro.util.Utf8
 
 object TestScalaEngine {
 	val logger = Logger.getLogger("scads.test")
@@ -13,6 +16,15 @@ object TestScalaEngine {
 	val handler = ScalaEngine.main(9000, "localhost:2181", Some(path), None, false)
 	val node = RemoteNode("localhost", 9000)
 	val cluster = new ScadsCluster(zooKeeper)
+
+  def createNamespace(ns: String, keySchema: Schema, valueSchema: Schema): Unit = {
+    cluster.createNamespace(ns, keySchema, valueSchema)
+    val cr = new ConfigureRequest
+    cr.namespace = ns
+    cr.partition = "1"
+    println("Making CR request: " + cr)
+    Sync.makeRequest(node, new Utf8("Storage"), cr)
+  }
 
 	def rmDir(dir: java.io.File): Boolean = {
 		if (dir.isDirectory()) {
