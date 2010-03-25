2abc5d77e7a9a3b60148b7924f871615ce4cd546
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-03-24 22:23:56 -0700

    Fix hack for enitity serialization and deserialization.

--- piql/src/main/scala/model/entity.scala
+++ piql/src/main/scala/model/entity.scala
@@ -5,7 +5,10 @@ import org.apache.avro.io.BinaryDecoder
 import org.apache.avro.specific.SpecificRecordBase
 import org.apache.avro.specific.SpecificRecord
 import org.apache.avro.specific.SpecificDatumReader
+
+import java.io.InputStream
 import java.io.ByteArrayInputStream
+import java.nio.ByteBuffer
 
 import edu.berkeley.cs.scads.storage.{ScadsCluster, Namespace}
 
@@ -17,6 +20,20 @@ class PiqlDataReader(schema: Schema) extends SpecificDatumReader[Any](schema) {
 
 abstract class EntityPart extends SpecificRecordBase with SpecificRecord {
 	val reader = new PiqlDataReader(getSchema)
+
+  override def parse(bytes: ByteBuffer): Unit = {
+    val buff = bytes.duplicate
+    val in = new InputStream() {
+      override def read(): Int = {
+        if(buff.remaining > 0)
+          buff.get
+        else
+          -1
+      }
+    }
+		val dec = new BinaryDecoder(in)
+    reader.read(this, dec)
+  }
 
 	override def parse(bytes: Array[Byte]): Unit = {
 		val in = new ByteArrayInputStream(bytes)
@@ -32,15 +49,9 @@ abstract class Entity(cluster: ScadsCluster) {
 
   def load(pk: SpecificRecordBase): Unit = {
     val storedValue = namespace.get(pk)
-    val keyArr = new Array[Byte](storedValue.key.remaining)
-    val valueArr = new Array[Byte](storedValue.value.remaining)
-    storedValue.key.get(keyArr)
-    storedValue.value.get(valueArr)
 
-    println(keyArr.toList)
-    key.parse(keyArr)
-    println(valueArr.toList)
-    value.parse(valueArr)
+    key.parse(storedValue.key)
+    value.parse(storedValue.value)
   }
 
   def save: Unit = {
