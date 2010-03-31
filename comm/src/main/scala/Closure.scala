3b24a27d17406a2d70ca1b7c9093354cc0844b85
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-03-30 20:21:43 -0700

    Make closure serializing functions more general.

--- comm/src/main/scala/Closure.scala
+++ comm/src/main/scala/Closure.scala
@@ -1,20 +1,34 @@
 package edu.berkeley.cs.scads.comm
 
 import java.io._
+import java.nio.ByteBuffer
+import org.apache.avro.specific.SpecificRecordBase
+
 
 object Closure {
-	def apply(cl: Array[Byte]): () => Unit = {
-		val buff = new ByteArrayInputStream(cl)
+	def apply(buff: InputStream): Object = {
 		val s = new ObjectInputStream(buff)
-		s.readObject.asInstanceOf[Function0[Unit]]
+		s.readObject
 	}
-}
 
-class Closure(cl: () => Unit) {
-	def toBytes() = {
+  def apply(cl: Function0[_]): Array[Byte] = {
 		val buff = new ByteArrayOutputStream()
 		val s = new ObjectOutputStream(buff);
 		s.writeObject(cl)
 		buff.toByteArray
-	}
+  }
+
+  def apply(cl: Function1[_,_]): Array[Byte] = {
+		val buff = new ByteArrayOutputStream()
+		val s = new ObjectOutputStream(buff);
+		s.writeObject(cl)
+		buff.toByteArray
+  }
+
+  def apply(cl: Function2[_,_,_]): Array[Byte] = {
+		val buff = new ByteArrayOutputStream()
+		val s = new ObjectOutputStream(buff);
+		s.writeObject(cl)
+		buff.toByteArray
+  }
 }
