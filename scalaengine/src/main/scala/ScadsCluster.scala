da25f8f74a65aad4f6ecd57da3a5b4f2e6c27dc3
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-05-03 13:06:54 -0700

    Hack until we have a list of servers so we can select a sane default.

--- scalaengine/src/main/scala/ScadsCluster.scala
+++ scalaengine/src/main/scala/ScadsCluster.scala
@@ -46,8 +46,9 @@ class ScadsCluster(root: ZooKeeperProxy#ZooKeeperNode) {
       false
   }
 
+  //TODO: The default should be to use a random server from the cluster... not the local test node.
   def createNamespace[KeyType <: SpecificRecordBase, ValueType <: SpecificRecordBase](ns: String, keySchema: Schema, valueSchema: Schema)(implicit keyType: scala.reflect.Manifest[KeyType], valueType: scala.reflect.Manifest[ValueType]): Namespace[KeyType, ValueType] = {
-    createNamespace[KeyType, ValueType](ns, keySchema, valueSchema, List[RemoteNode]())
+    createNamespace[KeyType, ValueType](ns, keySchema, valueSchema, List[RemoteNode](TestScalaEngine.node))
   }
 
 	def createNamespace[KeyType <: SpecificRecordBase, ValueType <: SpecificRecordBase](ns: String, keySchema: Schema, valueSchema: Schema, servers: List[RemoteNode])(implicit keyType: scala.reflect.Manifest[KeyType], valueType: scala.reflect.Manifest[ValueType]): Namespace[KeyType, ValueType] = {
