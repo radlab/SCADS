8ab860e82b31d641099c0c8706a82f5b261fa44c
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-05-03 12:40:33 -0700

    Clean up hack in MapSpec.

--- scalaengine/src/test/scala/MapSpec.scala
+++ scalaengine/src/test/scala/MapSpec.scala
@@ -11,11 +11,8 @@ import edu.berkeley.cs.scads.storage._
 object MapSpec extends SpecificationWithJUnit("KeyStore Specification") {
   val intRec = new IntRec
 
-  //Note: this object is a hack to prevent specs from trying to double create the namespaces.
-  object NS {
-    val gptest = TestScalaEngine.cluster.createNamespace[IntRec, IntRec]("getputtest", intRec.getSchema(), intRec.getSchema(), List(TestScalaEngine.node))
-    val fmtest = TestScalaEngine.cluster.createNamespace[IntRec, IntRec]("fmtest", intRec.getSchema(), intRec.getSchema(), List(TestScalaEngine.node))
-  }
+  val gptest = TestScalaEngine.cluster.getNamespace[IntRec, IntRec]("getputtest")
+  val fmtest = TestScalaEngine.cluster.getNamespace[IntRec, IntRec]("fmtest")
 
   object IntRec {
     def apply(i: Int): IntRec = {
@@ -31,16 +28,16 @@ object MapSpec extends SpecificationWithJUnit("KeyStore Specification") {
       ir1.f1 = 1234
       ir2.f1 = 5678
 
-      NS.gptest.put(ir1, ir2)
-      NS.gptest.get(ir1) must_== Some(ir2)
+      gptest.put(ir1, ir2)
+      gptest.get(ir1) must_== Some(ir2)
     }
 
     "implement flatMap" in {
       (1 to 10).foreach(i => {
-        NS.fmtest.put(IntRec(i), IntRec(i * 10))
+        fmtest.put(IntRec(i), IntRec(i * 10))
       })
 
-      NS.fmtest.flatMap {
+      fmtest.flatMap {
         case (k,v) => if(k.f1 % 2 == 0) List(IntRec(v.f1/10)) else Nil
       } must haveTheSameElementsAs ((1 to 10).filter(_ % 2 == 0).map(IntRec(_)))
 
