c27bbd73a87308af9dac53d0a13e1c96d0e21b2a
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-05-13 13:12:42 -0700

    Clean up engine tests.

--- scalaengine/src/test/scala/MapSpec.scala
+++ scalaengine/src/test/scala/MapSpec.scala
@@ -8,25 +8,16 @@ import edu.berkeley.cs.scads.comm.Conversions._
 
 import edu.berkeley.cs.scads.storage._
 
-object MapSpec extends SpecificationWithJUnit("KeyStore Specification") {
+object NamespaceSpec extends SpecificationWithJUnit("SCADS Namespace") {
   val intRec = new IntRec
 
   val gptest = TestScalaEngine.cluster.getNamespace[IntRec, IntRec]("getputtest")
   val fmtest = TestScalaEngine.cluster.getNamespace[IntRec, IntRec]("fmtest")
-
-  object IntRec {
-    def apply(i: Int): IntRec = {
-      val r = new IntRec
-      r.f1 = i
-      r
-    }
-  }
+  val rangetest = TestScalaEngine.cluster.getNamespace[IntRec, IntRec]("rangetest")
 
   "A SCADS Map" should {
     "implement get/put" in {
-      val (ir1, ir2) = (new IntRec, new IntRec)
-      ir1.f1 = 1234
-      ir2.f1 = 5678
+      val (ir1, ir2) = (IntRec(1234), IntRec(5478))
 
       gptest.put(ir1, ir2)
       gptest.get(ir1) must_== Some(ir2)
@@ -40,7 +31,13 @@ object MapSpec extends SpecificationWithJUnit("KeyStore Specification") {
       fmtest.flatMap {
         case (k,v) => if(k.f1 % 2 == 0) List(IntRec(v.f1/10)) else Nil
       } must haveTheSameElementsAs ((1 to 10).filter(_ % 2 == 0).map(IntRec(_)))
+    }
 
+    "getRange" in {
+      val data = (1 to 10).map(i => IntRec(i) -> IntRec(i))
+      rangetest ++= data
+
+      rangetest.getRange(null, null) must haveTheSameElementsAs(data)
     }
   }
 }
