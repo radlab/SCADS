b5a921f20d6aeeb019ea02af8c51599765e40c18
Stephen Tu <stephen_tu@berkeley.edu>
2010-05-21 11:56:24 -0700

    renamed configurator to TestConfigurator lower case class names

--- experiments/client/scadrclient/setup.scala
+++ experiments/client/scadrclient/setup.scala
@@ -8,33 +8,33 @@ import edu.berkeley.cs.scads.storage.{Namespace, TestScalaEngine}
 
 import piql._
 
-implicit val env = (new Configurator).configureTestCluster
+implicit val env = (new TestConfigurator).configureTestCluster
 
-val u1 = new User
+val u1 = new user
 u1.key.name = "marmbrus"
 u1.value.password = "pass"
 u1.value.email = "marmbrus@berkeley.edu"
 u1.save
 
-val u2 = new User
+val u2 = new user
 u2.key.name = "sltu"
 u2.value.password = "pass"
 u2.value.email = "sltu@cs.berkeley.edu"
 u2.save
 
-val s = new Subscription
+val s = new subscription
 s.value.approved = true
 s.key.owner.name = "marmbrus"
 s.key.target.name = "sltu"
 s.save
 
-val t1 = new Thought
+val t1 = new thought
 t1.key.owner.name = u1.name
 t1.key.timestamp = 1
 t1.value.text = "michael: Hey there scadr world!"
 t1.save
 
-val t2 = new Thought
+val t2 = new thought
 t2.key.owner.name = u2.name
 t2.key.timestamp = 1
 t2.value.text = "stephen: Hey there from me too!"
