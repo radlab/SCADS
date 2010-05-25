d824ba294fdefd04939ebc80869247b02ce49a50
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-05-24 19:52:31 -0700

    Add selections for simpleGets to query plan vis.

--- piql/src/main/scala/model/viz.scala
+++ piql/src/main/scala/model/viz.scala
@@ -2,12 +2,17 @@ package edu.berkeley.cs.scads.piql
 
 import java.io.File
 import edu.berkeley.cs.scads.piql.parser._
+import edu.berkeley.cs.scads.piql.DynamicDispatch._
+import edu.berkeley.cs.scads.storage.TestScalaEngine
 
 object DotNode {
   private val curId = new java.util.concurrent.atomic.AtomicInteger
   def nextId: String = "dotNode" + curId.getAndIncrement()
 }
-case class DotNode(label: String, children: List[DotNode] = Nil) { val id = DotNode.nextId }
+case class DotNode(label: String, children: List[DotNode] = Nil) {
+  val id = DotNode.nextId
+  def this(label: String, child: DotNode) = this(label, List(child))
+}
 
 object GraphVis {
   def main(args: Array[String]): Unit = {
@@ -15,16 +20,36 @@ object GraphVis {
     val ast = Compiler.getAST(piql)
     val boundAst = new Binder(ast).bind
     val opt = new Optimizer(boundAst).optimizedSpec
+    val code = ScalaGen(opt)
+    val compiler = new ScalaCompiler
+    compiler.compile(code)
+    val configurator = compiler.classLoader.loadClass("piql.Configurator")
+
+    implicit val env: Environment = configurator.callStatic("configure", List(TestScalaEngine.cluster)) match {
+      case e: Environment => e
+      case err => throw new RuntimeException("Bad configurator return type: " + err.getClass)
+    }
 
     getPlans(opt).map(generateGraph).foreach(g => println(DotGen(g)))
   }
 
-  def getPlans(spec: BoundSpec): Iterable[QueryPlan] = spec.orphanQueries.map(_._2).map(_.plan) ++ spec.entities.map(_._2).flatMap(_.queries).map(_._2).map(_.plan)
+  protected def getPlans(spec: BoundSpec): Iterable[QueryPlan] = spec.orphanQueries.map(_._2).map(_.plan) ++ spec.entities.map(_._2).flatMap(_.queries).map(_._2).map(_.plan)
 
-  def generateGraph(plan: QueryPlan): DotNode= {
+  protected def getPredicates(ns: String, keySpec: List[BoundValue], child: DotNode)(implicit env: Environment): DotNode = {
+    val namespace = env.namespaces(ns)
+
+    keySpec.zipWithIndex.foldLeft(child) {
+      case (subPlan: DotNode, (value: BoundValue, idx: Int)) => {
+        val fieldName = namespace.keySchema.getFields.get(idx).name
+        DotNode("selection " + fieldName + "=" + value, List(subPlan))
+      }
+    }
+  }
+
+  def generateGraph(plan: QueryPlan)(implicit env: Environment): DotNode = {
     plan match {
-      case SingleGet(ns, key) => DotNode("selection", List(DotNode(ns)))
-      case PrefixGet(ns, prefix, limit, ascending) => DotNode("selection", List(DotNode(ns)))
+      case SingleGet(ns, key) => getPredicates(ns, key, DotNode(ns))
+      case PrefixGet(ns, prefix, limit, ascending) => getPredicates(ns, prefix, DotNode(ns))
       case SequentialDereferenceIndex(ns, child) => DotNode("deref", List(DotNode(ns), generateGraph(child)))
       case PrefixJoin(ns, conditions, limit, ascending, child) => DotNode("join", List(DotNode(ns), generateGraph(child)))
       case PointerJoin(ns, conditions, child) => DotNode("join", List(DotNode(ns), generateGraph(child)))
