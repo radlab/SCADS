bc3de64d27b405c264d935d5c3ffc81c67427e3e
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-05-24 21:32:04 -0700

    first cut an sectioning off individual operators.

--- piql/src/main/scala/model/viz.scala
+++ piql/src/main/scala/model/viz.scala
@@ -5,12 +5,22 @@ import edu.berkeley.cs.scads.piql.parser._
 import edu.berkeley.cs.scads.piql.DynamicDispatch._
 import edu.berkeley.cs.scads.storage.TestScalaEngine
 
-object DotNode {
+case class DotGraph(name: String, components: Iterable[DotComponent])
+
+object DotComponent {
   private val curId = new java.util.concurrent.atomic.AtomicInteger
-  def nextId: String = "dotNode" + curId.getAndIncrement()
+  def nextId = curId.getAndIncrement()
 }
-case class DotNode(label: String, children: List[DotNode] = Nil) {
-  val id = DotNode.nextId
+
+abstract class DotComponent {val id: String}
+
+case class SubGraph(label: String, nodes: List[DotComponent]) extends DotComponent {
+  val graphId = "cluster" + DotComponent.nextId
+  val id = nodes.first.id
+}
+
+case class DotNode(label: String, children: List[DotComponent] = Nil) extends DotComponent {
+  val id = "dotNode" + DotComponent.nextId
   def this(label: String, child: DotNode) = this(label, List(child))
 }
 
@@ -19,8 +29,8 @@ object GraphVis {
     val piql = Compiler.readFile(new File(args(0)))
     val ast = Compiler.getAST(piql)
     val boundAst = new Binder(ast).bind
-    val opt = new Optimizer(boundAst).optimizedSpec
-    val code = ScalaGen(opt)
+    val spec = new Optimizer(boundAst).optimizedSpec
+    val code = ScalaGen(spec)
     val compiler = new ScalaCompiler
     compiler.compile(code)
     val configurator = compiler.classLoader.loadClass("piql.Configurator")
@@ -30,7 +40,13 @@ object GraphVis {
       case err => throw new RuntimeException("Bad configurator return type: " + err.getClass)
     }
 
-    getPlans(opt).map(generateGraph).foreach(g => println(DotGen(g)))
+    val orphanPlans = spec.orphanQueries.map(q => SubGraph(q._1, List(generateGraph(q._2.plan))))
+    val instancePlans = spec.entities.flatMap(e => {
+      e._2.queries.map(q => SubGraph(e._1 + "." + q._1, List(generateGraph(q._2.plan))))
+    })
+
+    println(DotGen(DotGraph("QueryPlans", orphanPlans ++ instancePlans)))
+    System.exit(0)
   }
 
   protected def getPlans(spec: BoundSpec): Iterable[QueryPlan] = spec.orphanQueries.map(_._2).map(_.plan) ++ spec.entities.map(_._2).flatMap(_.queries).map(_._2).map(_.plan)
@@ -46,13 +62,13 @@ object GraphVis {
     }
   }
 
-  def generateGraph(plan: QueryPlan)(implicit env: Environment): DotNode = {
+  def generateGraph(plan: QueryPlan)(implicit env: Environment): DotComponent = {
     plan match {
-      case SingleGet(ns, key) => getPredicates(ns, key, DotNode(ns))
-      case PrefixGet(ns, prefix, limit, ascending) => getPredicates(ns, prefix, DotNode(ns))
-      case SequentialDereferenceIndex(ns, child) => DotNode("deref", List(DotNode(ns), generateGraph(child)))
-      case PrefixJoin(ns, conditions, limit, ascending, child) => DotNode("join", List(DotNode(ns), generateGraph(child)))
-      case PointerJoin(ns, conditions, child) => DotNode("join", List(DotNode(ns), generateGraph(child)))
+      case SingleGet(ns, key) => SubGraph("SingleGet", List(getPredicates(ns, key, DotNode(ns))))
+      case PrefixGet(ns, prefix, limit, ascending) => SubGraph("PrefixGet", List(getPredicates(ns, prefix, DotNode(ns))))
+      case SequentialDereferenceIndex(ns, child) => SubGraph("SequentialDeref", List(DotNode("deref", List(DotNode(ns), generateGraph(child)))))
+      case PrefixJoin(ns, conditions, limit, ascending, child) => SubGraph("PrefixJoin", List(DotNode("join", List(DotNode(ns), generateGraph(child)))))
+      case PointerJoin(ns, conditions, child) => SubGraph("PointerJoin", List(DotNode("join", List(DotNode(ns), generateGraph(child)))))
       case Materialize(entityType, child) => generateGraph(child)
       case Selection(equalityMap, child) => DotNode("selection", List(generateGraph(child)))
       case Sort(fields, ascending, child) => DotNode("sort", List(generateGraph(child)))
@@ -61,20 +77,28 @@ object GraphVis {
   }
 }
 
-object DotGen extends Generator[DotNode] {
+object DotGen extends Generator[DotGraph] {
   private val curId = new java.util.concurrent.atomic.AtomicInteger
 
-  protected def generate(graph: DotNode)(implicit sb: StringBuilder, indnt: Indentation): Unit = {
-    outputBraced("digraph Plan" + curId.getAndIncrement()) {
-      def outputNode(node: DotNode): Unit = output(node.id, "[label=", quote(node.label), "];")
-      def outputEdge(node1: DotNode, node2: DotNode):Unit = output(node1.id, " -> ", node2.id, ";")
-      traverse(graph, outputNode, outputEdge)
+  protected def generate(graph: DotGraph)(implicit sb: StringBuilder, indnt: Indentation): Unit = {
+    outputBraced("digraph ", graph.name) {
+      graph.components.foreach(generateSubGraph)
     }
   }
 
-  private def traverse(graph: DotNode, nodeFunc: Function1[DotNode, Unit], edgeFunc: Function2[DotNode, DotNode, Unit]): Unit = {
-    nodeFunc(graph)
-    graph.children.foreach(traverse(_, nodeFunc, edgeFunc))
-    graph.children.foreach(edgeFunc(graph, _))
+  protected def generateSubGraph(graph: DotComponent)(implicit sb: StringBuilder, indnt: Indentation): Unit = {
+    graph match {
+      case node: DotNode => {
+        output(node.id, "[label=", quote(node.label), "];")
+        node.children.foreach(generateSubGraph)
+        node.children.map(_.id).foreach(id => output(node.id, " -> ", id, ";"))
+      }
+      case subGraph: SubGraph => {
+        outputBraced("subgraph ", subGraph.graphId) {
+          output("label=", quote(subGraph.label), ";")
+          subGraph.nodes.foreach(generateSubGraph)
+        }
+      }
+    }
   }
 }
