55f00eb36d6a67e564f80872926571e64d2d776c
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-05-25 20:55:05 -0700

    Colors and shapes.

--- piql/src/main/scala/model/viz.scala
+++ piql/src/main/scala/model/viz.scala
@@ -19,7 +19,7 @@ case class SubGraph(label: String, nodes: List[DotComponent]) extends DotCompone
   val id = nodes.head.id
 }
 
-case class DotNode(label: String, children: List[DotComponent] = Nil) extends DotComponent {
+case class DotNode(label: String, children: List[DotComponent] = Nil, shape: String = "box", fillcolor: String = "azure3", style: String = "none") extends DotComponent {
   val id = "dotNode" + DotComponent.nextId
   def this(label: String, child: DotNode) = this(label, List(child))
 }
@@ -57,7 +57,7 @@ object GraphVis {
     keySpec.zipWithIndex.foldLeft(child) {
       case (subPlan: DotNode, (value: BoundValue, idx: Int)) => {
         val fieldName = namespace.keySchema.getFields.get(idx).name
-        DotNode("selection " + fieldName + "=" + value, List(subPlan))
+        DotNode("selection " + fieldName + "=" + value, List(subPlan), shape="ellipse")
       }
     }
   }
@@ -68,7 +68,7 @@ object GraphVis {
     conditions.zipWithIndex.foldLeft(child) {
       case (subPlan: DotNode, (value: JoinCondition, idx: Int)) => {
         val fieldName = namespace.keySchema.getFields.get(idx).name
-        DotNode("selection " + fieldName + "=" + value, List(subPlan))
+        DotNode("selection " + fieldName + "=" + value, List(subPlan), shape="ellipse")
       }
     }
   }
@@ -78,12 +78,12 @@ object GraphVis {
       case SingleGet(ns, key) => SubGraph("SingleGet", List(getPredicates(ns, key, DotNode(ns))))
       case PrefixGet(ns, prefix, limit, ascending) => SubGraph("PrefixGet", List(getPredicates(ns, prefix, DotNode(ns))))
       case SequentialDereferenceIndex(ns, child) => SubGraph("SequentialDeref", List(DotNode("deref", List(DotNode(ns), generateGraph(child)))))
-      case PrefixJoin(ns, conditions, limit, ascending, child) => SubGraph("PrefixJoin", List(getJoinPredicates(ns, conditions, DotNode("join", List(DotNode(ns), generateGraph(child))))))
-      case PointerJoin(ns, conditions, child) => SubGraph("PointerJoin", List(getJoinPredicates(ns, conditions, DotNode("join", List(DotNode(ns), generateGraph(child))))))
+      case PrefixJoin(ns, conditions, limit, ascending, child) => SubGraph("PrefixJoin", List(getJoinPredicates(ns, conditions, DotNode("join", List(DotNode(ns), generateGraph(child)), shape="diamond"))))
+      case PointerJoin(ns, conditions, child) => SubGraph("PointerJoin", List(getJoinPredicates(ns, conditions, DotNode("join", List(DotNode(ns), generateGraph(child)), shape="diamond"))))
       case Materialize(entityType, child) => generateGraph(child)
-      case Selection(equalityMap, child) => DotNode("selection", List(generateGraph(child)))
-      case Sort(fields, ascending, child) => DotNode("sort", List(generateGraph(child)))
-      case TopK(k, child) => DotNode("topk " + k, List(generateGraph(child)))
+      case Selection(equalityMap, child) => DotNode("selection", List(generateGraph(child)), style="filled")
+      case Sort(fields, ascending, child) => DotNode("sort", List(generateGraph(child)), style="filled")
+      case TopK(k, child) => DotNode("topk " + k, List(generateGraph(child)), style="filled")
     }
   }
 }
@@ -101,7 +101,7 @@ object DotGen extends Generator[DotGraph] {
   protected def generateSubGraph(graph: DotComponent)(implicit sb: StringBuilder, indnt: Indentation): Unit = {
     graph match {
       case node: DotNode => {
-        output(node.id, "[label=", quote(node.label), "];")
+        output(node.id, "[label=", quote(node.label), ", shape=", node.shape, ", fillcolor=", node.fillcolor, ", style=", node.style, "];")
         node.children.foreach(generateSubGraph)
         node.children.map(_.id).foreach(id => output(id, " -> ", node.id, ";"))
       }
