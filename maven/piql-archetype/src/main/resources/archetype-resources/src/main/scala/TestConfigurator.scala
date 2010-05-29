772b1f78d35af02ef5ded7a3376e1718b66ec801
Stephen Tu <stephen_tu@berkeley.edu>
2010-05-29 16:26:42 -0700

    updated TestConfigurator with an all method

--- maven/piql-archetype/src/main/resources/archetype-resources/src/main/scala/TestConfigurator.scala
+++ maven/piql-archetype/src/main/resources/archetype-resources/src/main/scala/TestConfigurator.scala
@@ -1,5 +1,24 @@
 package piql
 
+import edu.berkeley.cs.scads.piql.{ Entity, Environment, QueryExecutor }
+
 class TestConfigurator {
-	def configureTestCluster = Configurator.configure()
+  def configureTestCluster = Configurator.configure()
+}
+
+class QueryHelper extends QueryExecutor {
+  def all(entityName: String, limit: Int, offset: Int, backwards: Boolean)(implicit env: Environment): Seq[Entity[_,_]] = { 
+    val ns = "ent_" + entityName
+    env.namespaces.get(ns) match {
+      case Some(namespace) =>  
+        val kvSeq = namespace.getRange(null, null, limit, offset, backwards) // fetch everything, limit to limit, ascending 
+        val clazzName = "piql." + entityName
+        val entClass = Class.forName(clazzName)
+        if (entClass eq null) 
+          throw new IllegalArgumentException("Could not find class for name: " + clazzName)
+        materialize(entClass.asInstanceOf[Class[Entity[_,_]]], kvSeq)
+      case None =>
+        throw new IllegalArgumentException("Bad entity name: " + entityName)
+    }   
+  }
 }
