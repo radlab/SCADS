dbf5c8e28aa9c1e0aae36642e1f05d163d9ae292
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-07-17 02:08:20 -0700

    Add exmaple ScadsCluster usage.

--- scalaengine/src/main/scala/util/TestScalaEngine.scala
+++ scalaengine/src/main/scala/util/TestScalaEngine.scala
@@ -33,3 +33,15 @@ object TestScalaEngine {
 		dir.delete();
 	}
 }
+
+import com.googlecode.avro.marker.AvroRecord
+case class IntRec(var x: Int) extends AvroRecord
+
+object Example {
+  def main(args: Array[String]): Unit = {
+    val cluster = TestScalaEngine.cluster
+    val ns = cluster.getNamespace[IntRec, IntRec]("testns")
+    ns.put(IntRec(1), IntRec(2))
+    println("Received Record:" + ns.get(IntRec(1)))
+  }
+}
