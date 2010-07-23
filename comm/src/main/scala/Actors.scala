33225d519005564e70784de824930cd8bc9b53fa
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-07-23 15:01:38 -0700

    Clean up the specific RemoteActorProxy types.

--- comm/src/main/scala/Actors.scala
+++ comm/src/main/scala/Actors.scala
@@ -30,7 +30,7 @@ object Actors {
 /* Generic Remote Actor Handle */
 case class RemoteActor(var host: String, var port: Int, var id: ActorId) extends RemoteActorProxy with AvroRecord
 
-/* Specific types for different services */
+/* Specific types for different services. Note: these types are mostly for readability as typesafety isn't enforced when serialized individualy*/
 case class StorageService(var host: String, var port: Int, var id: ActorId) extends RemoteActorProxy with AvroRecord
 case class PartitionService(var host: String, var port: Int, var id: ActorId) extends RemoteActorProxy with AvroRecord
 
@@ -52,7 +52,7 @@ trait RemoteActorProxy {
     def receiver: Actor = throw new RuntimeException("Unimplemented")
   }
 
-  def !(body: MessageBody)(implicit sender: RemoteActor): Unit = {
+  def !(body: MessageBody)(implicit sender: RemoteActorProxy): Unit = {
     MessageHandler.sendMessage(toRemoteNode, Message(Some(sender.id), id, None, body))
   }
 
@@ -78,6 +78,10 @@ trait RemoteActorProxy {
   }
 
   def !!(body: MessageBody): Future[MessageBody] = throw new RuntimeException("Unimplemented")
+
+  /* Handle type conversion methods.  Note: Not typesafe */
+  def toPartitionService: PartitionService = new PartitionService(host, port, id)
+  def toStorageService: StorageService = new StorageService(host, port, id)
 }
 
 
