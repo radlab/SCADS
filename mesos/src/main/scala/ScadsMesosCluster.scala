3f8103a2ca95dd175d647eef18bef1030db30af9
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-07-26 21:22:27 -0700

    More work on mesos scheduler.

--- mesos/src/main/scala/ScadsMesosCluster.scala
+++ mesos/src/main/scala/ScadsMesosCluster.scala
@@ -1,22 +1,14 @@
 package edu.berkeley.cs.scads.mesos
 
-import nexus._
+import mesos._
 import java.io.File
 import org.apache.log4j.Logger
 
-object Test {
-  def main(args: Array[String]): Unit = {
-    System.loadLibrary("nexus")
-    org.apache.log4j.BasicConfigurator.configure()
-    new NexusSchedulerDriver(new ScadsScheduler(), "1@169.229.48.70:9999").run();
-  }
-}
-
-class ScadsScheduler extends Scheduler {
+object ScadsScheduler extends Scheduler {
   val logger = Logger.getLogger("scads.mesos.scheduler")
   var taskId = 0
   override def getFrameworkName(d: SchedulerDriver): String = "SCADS Framework"
-  override def getExecutorInfo(d: SchedulerDriver): ExecutorInfo = new ExecutorInfo("/home/eecs/marmbrus/scads_executor", Array[Byte]())
+  override def getExecutorInfo(d: SchedulerDriver): ExecutorInfo = new ExecutorInfo("/work/marmbrus/mesos/scads_executor", Array[Byte]())
   override def registered(d: SchedulerDriver, fid: String): Unit = logger.info("Registered SCADS Framework.  Fid: " + fid)
 
   override def resourceOffer(d: SchedulerDriver, oid: String, offers: SlaveOfferVector) = {
@@ -27,7 +19,7 @@ class ScadsScheduler extends Scheduler {
     val offer = offers.get(0)
     taskParams.set("cpus", "1")
     taskParams.set("mem", "134217728")
-    tasks.add(new TaskDescription(taskId, offer.getSlaveId(), "task" + taskId, taskParams, Array[Byte]()))
+    tasks.add(new TaskDescription(taskId, offer.getSlaveId(), "task" + taskId, taskParams, "".getBytes))
     taskId += 1
 
     val params = new StringMap()
@@ -38,5 +30,26 @@ class ScadsScheduler extends Scheduler {
 
   def statusUpdate(d: SchedulerDriver, code: Int, message: String): Unit = {
     logger.info("Status Update: " + code + " " + message)
+  }
+
+  def main(args: Array[String]): Unit = {
+    System.loadLibrary("mesos")
+    org.apache.log4j.BasicConfigurator.configure()
+    new MesosSchedulerDriver(this, "1@169.229.48.70:9999").run();
+  }
+}
+
+object ScadsExecutor extends Executor {
+  System.loadLibrary("mesos")
+
+  override def launchTask(d: ExecutorDriver, task: TaskDescription): Unit = {
+    println("Starting storage handler" + task.getTaskId())
+    edu.berkeley.cs.scads.storage.ScalaEngine.main(Some("scads"), Some("r2:2181"), None, None, true)
+  }
+
+  def main(args: Array[String]): Unit = {
+    System.loadLibrary("mesos")
+    val driver = new MesosExecutorDriver(this)
+    driver.run()
   }
 }
