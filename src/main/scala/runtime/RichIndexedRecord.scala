74bd633d7e7a1d1bae2d150a8689cf8e08729f8f
marmbrus <marmbrus@85b7b6fe-2b2e-11df-a6fe-af80f4a49816>
2010-08-11 18:47:32 +0000

    Rename AvroScala to RichIndexedRecord.  Add implicit conversion to runtime package.
    git-svn-id: https://avro-scala-compiler-plugin.googlecode.com/svn/trunk@62 85b7b6fe-2b2e-11df-a6fe-af80f4a49816

---
+++ src/main/scala/runtime/RichIndexedRecord.scala
@@ -0,0 +1,56 @@
+package com.googlecode.avro
+package runtime
+
+import java.io.ByteArrayOutputStream
+
+import scala.collection.JavaConversions._
+
+import org.apache.avro.io.{BinaryDecoder, BinaryEncoder, DecoderFactory, JsonDecoder, JsonEncoder}
+import org.apache.avro.generic.{GenericData, GenericDatumReader, GenericDatumWriter, IndexedRecord}
+
+/**
+ * Collection of implicit conversions to scala-ify the Avro Java Library.
+ */
+class RichIndexedRecord[T <: IndexedRecord](val rec: T) {
+  lazy val reader = new GenericDatumReader[T](rec.getSchema())
+
+  @inline def toJson: String = {
+    val outBuffer = new ByteArrayOutputStream
+    val encoder = new JsonEncoder(rec.getSchema(), outBuffer)
+    val writer = new GenericDatumWriter[IndexedRecord](rec.getSchema())
+    writer.write(rec, encoder)
+    encoder.flush()
+    new String(outBuffer.toByteArray)
+  }
+
+  @inline def toBytes: Array[Byte] = {
+    val outBuffer = new ByteArrayOutputStream
+    val encoder = new BinaryEncoder(outBuffer)
+    val writer = new GenericDatumWriter[IndexedRecord](rec.getSchema())
+    writer.write(rec, encoder)
+    outBuffer.toByteArray
+  }
+
+  @inline def parse(data: String): T = {
+    val decoder = new JsonDecoder(rec.getSchema, data)
+    reader.read(rec, decoder)
+  }
+
+  @inline def parse(data: Array[Byte]): T = {
+    val decoder = DecoderFactory.defaultFactory().createBinaryDecoder(data, null)
+    reader.read(rec, decoder)
+  }
+
+  @inline def toGenericRecord: GenericData.Record = {
+    val genRec = new GenericData.Record(rec.getSchema())
+    rec.getSchema().getFields().foreach(f => genRec.put(f.pos, rec.get(f.pos)))
+    genRec
+  }
+
+  def compare(lhs: T): Int = GenericData.get.compare(rec, lhs, rec.getSchema())
+  def >(lhs: T): Boolean = compare(lhs) > 0
+  def <(lhs: T): Boolean = compare(lhs) < 0
+  def >=(lhs: T): Boolean = compare(lhs) >= 0
+  def <=(lhs: T): Boolean = compare(lhs) <= 0
+  def ==(lhs: T): Boolean = compare(lhs) == 0
+}
--- src/main/scala/runtime/package.scala
+++ src/main/scala/runtime/package.scala
@@ -1,6 +1,6 @@
 package com.googlecode.avro
 
-import org.apache.avro.generic.GenericContainer
+import org.apache.avro.generic.{GenericContainer, IndexedRecord}
 
 package object runtime {
   type TypedSchema[T] = org.apache.avro.TypedSchema[T]
@@ -8,4 +8,6 @@ package object runtime {
     val schema = TypedSchemas.findSchema[C]
     new TypedSchema[C](schema)
   }
+
+  implicit def toRichIndexedRecord[T <: IndexedRecord](rec: T): RichIndexedRecord[T] = new RichIndexedRecord(rec)
 }
