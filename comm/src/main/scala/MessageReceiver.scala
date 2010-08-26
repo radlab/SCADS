bc7fcab4b00397b354d1480bc0ebac9c1b2fb755
Stephen Tu <stephen_tu@berkeley.edu>
2010-08-25 17:50:52 -0700

    1) provide nicer running semantics for startup() 2) fix possible race condition with immediate registration of StorageService to MessageHandler 3) provide more informative documentation on subclassing StorageService

--- comm/src/main/scala/MessageReceiver.scala
+++ comm/src/main/scala/MessageReceiver.scala
@@ -3,7 +3,8 @@ package edu.berkeley.cs.scads.comm
 import scala.actors._
 import scala.concurrent.SyncVar
 
-import java.util.concurrent.{BlockingQueue, ArrayBlockingQueue, ThreadPoolExecutor, TimeUnit}
+import java.util.concurrent.{ BlockingQueue, ArrayBlockingQueue, 
+                              CountDownLatch, ThreadPoolExecutor, TimeUnit}
 
 import org.apache.log4j.Logger
 
@@ -23,22 +24,47 @@ case class ActorService(a: Actor) extends MessageReceiver {
 /**
  * Executes requests of type MessageType on a threadpool by calling a user
  * supplied process method.
+ *
+ * WARNING: Currently, invocations to `startup` run in the constructor of
+ * ServiceHandler. This means that subclasses will not be properly constructed
+ * (its constructor will not have run yet) when `startup` is invoked. To get
+ * around this issue, any members of a subclass which need to be referenced in
+ * `startup` <b>cannot</b> be vals (or NPE will ensue). Either use lazy vals, or
+ * methods.
+ *
+ * TODO: fix this problem
  */
 abstract class ServiceHandler[MessageType <: MessageBody] extends MessageReceiver {
-  implicit val remoteHandle = MessageHandler.registerService(this)
+
   protected val logger: Logger
 
   /* Threadpool for execution of incoming requests */
-  protected val outstandingRequests = new ArrayBlockingQueue[Runnable](1024)
+  protected val outstandingRequests = new ArrayBlockingQueue[Runnable](1024) // TODO: read from config
   protected val executor = new ThreadPoolExecutor(5, 20, 30, TimeUnit.SECONDS, outstandingRequests)
 
-  startup()
+  /* Latch for waiting for startup to finish */
+  private val startupGuard = new CountDownLatch(1)
 
+  /* Registration (publication of this instance) must happen after
+   * initialization statements above. otherwise, NPE will ensue if a message
+   * arrives immediately (since the thread pool has not been initialized yet,
+   * etc) */
+  implicit val remoteHandle = MessageHandler.registerService(this)
+
+  // TODO: use an explicit startup pattern - see warning message above
+  startup()
+  startupGuard.countDown() /* signals startup completion */
+
+  /* Guaranteed that no invocations to process will occur during method */
   protected def startup(): Unit
+  /* Guaranteed that no invocations to process will occur during method */
   protected def shutdown(): Unit
+
+  /* Callback for when a message is received */
   protected def process(src: Option[RemoteActorProxy], msg: MessageType): Unit
 
   def stop: Unit = {
+    startupGuard.await() /* Let the service start up properly first, before shutting down */
     MessageHandler.unregisterActor(remoteHandle)
     executor.shutdown()
     shutdown()
@@ -48,7 +74,7 @@ abstract class ServiceHandler[MessageType <: MessageBody] extends MessageReceive
   class Request(src: Option[RemoteActorProxy], req: MessageBody) extends Runnable {
     def run():Unit = req match {
       case op: MessageType =>
-        try process(src, op) catch {
+        try { startupGuard.await(); process(src, op) } catch {
           case e: Throwable => {
             /* Get the stack trace */
             val stackTrace = e.getStackTrace().mkString("\n")
