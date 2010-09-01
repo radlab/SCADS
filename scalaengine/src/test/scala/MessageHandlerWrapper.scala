0fda82ed1a9ea6efe8a655ff4a924eca953577d9
Tim Kraska <kraska@cs.berkeley.edu>
2010-09-01 13:40:04 -0700

    buggy version

---
+++ scalaengine/src/test/scala/MessageHandlerWrapper.scala
@@ -0,0 +1,12 @@
+package
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tim
+ * Date: Aug 31, 2010
+ * Time: 5:28:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MessageHandlerWrapper
