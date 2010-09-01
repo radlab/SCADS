d079860fd2565d146e169d485c285e041b2c7a49
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-08-31 17:52:14 -0700

    Scads now runs on mesos!

--- mesos/src/main/scala/ScadsMesosCluster.scala
+++ mesos/src/main/scala/ScadsMesosCluster.scala
@@ -6,11 +6,13 @@ import org.apache.log4j.Logger
 
 import edu.berkeley.cs.scads.comm._
 
+import scala.collection.JavaConversions._
+
 object ScadsScheduler {
   def main(args: Array[String]): Unit = {
     System.loadLibrary("mesos")
     org.apache.log4j.BasicConfigurator.configure()
-    new MesosSchedulerDriver(new ScadsScheduler(), "1@169.229.48.74:5050").run();
+    new MesosSchedulerDriver(new ScadsScheduler(), "1@169.229.48.70:5050").run();
   }
 
 }
@@ -22,25 +24,27 @@ class ScadsScheduler extends Scheduler {
   override def getExecutorInfo(d: SchedulerDriver): ExecutorInfo = new ExecutorInfo("/work/marmbrus/mesos/scads_executor", Array[Byte]())
   override def registered(d: SchedulerDriver, fid: String): Unit = logger.info("Registered SCADS Framework.  Fid: " + fid)
 
-  override def resourceOffer(d: SchedulerDriver, oid: String, offs: SlaveOfferVector) = {
-    val offers = (0 to offs.capacity.toInt - 1).map(offs.get)
+  override def resourceOffer(d: SchedulerDriver, oid: String, offers: java.util.List[SlaveOffer]) = {
     logger.info("Got offer: " + offers)
 
-    val tasks = new TaskDescriptionVector()
-    val taskParams = new StringMap()
+    val tasks = new java.util.LinkedList[TaskDescription]
+    val taskParams = new java.util.TreeMap[String, String]
     offers.foreach(offer => {
-      taskParams.set("cpus", offer.getParams.get("cpus"))
-      taskParams.set("mem", offer.getParams.get("mem"))
-      tasks.add(new TaskDescription(taskId, offer.getSlaveId(), "task" + taskId, taskParams, JvmProcess("/work/marmbrus/mesos/mesos-2.1.0-SNAPSHOT-jar-with-dependencies.jar", "edu.berkeley.cs.scads.storage.ScalaEngine", "--zooKeeper r6:2181" :: Nil ).toBytes))
-      taskId += 1
+      if(taskId < 10) {
+        taskParams.put("cpus", offer.getParams.get("cpus"))
+        taskParams.put("mem", offer.getParams.get("mem"))
+        tasks.add(new TaskDescription(taskId, offer.getSlaveId(), "task" + taskId, taskParams, JvmProcess("/work/marmbrus/mesos/mesos-2.1.0-SNAPSHOT-jar-with-dependencies.jar", "edu.berkeley.cs.scads.storage.ScalaEngine", "--zooKeeper" :: "169.229.48.70:2181" :: "--verbose" :: Nil ).toBytes))
+        taskId += 1
+      }
     })
 
-    val params = new StringMap()
+    val params = new java.util.TreeMap[String, String]
     d.replyToOffer(oid, tasks, params)
   }
 
-  def statusUpdate(d: SchedulerDriver, code: Int, message: String): Unit = {
-    logger.info("Status Update: " + code + " " + message)
+  override def statusUpdate(d: SchedulerDriver, status: TaskStatus): Unit = {
+    logger.info("Status Update: " + status.getTaskId + " " + status.getState)
+    logger.debug(new String(status.getData))
   }
 
 }
