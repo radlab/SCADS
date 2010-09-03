71f1b2b98ded6880d16d11898d2a6c3c1b050766
steve33671 <steve33671@85b7b6fe-2b2e-11df-a6fe-af80f4a49816>
2010-09-03 21:55:18 +0000

    remove examples here- these are out of date
    git-svn-id: https://avro-scala-compiler-plugin.googlecode.com/svn/branches/implicits@93 85b7b6fe-2b2e-11df-a6fe-af80f4a49816

--- examples/test_classes.scala
+++
@@ -1,8 +0,0 @@
-package localhost.test
-
-import com.googlecode.avro.annotation.AvroRecord
-
-object RecordASC
-@AvroRecord case class RecordASC (var x: Int, var y: String, var z: Boolean)
-
-case class RecordJS (var x: Int, var y: String, var z: Boolean) extends java.io.Serializable
