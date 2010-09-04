956a3a48c1a43ebc24e85aeea7dae417f3913bfa
steve33671 <steve33671@85b7b6fe-2b2e-11df-a6fe-af80f4a49816>
2010-09-04 01:30:03 +0000

    add new parse / toBytes methods, cache readers/writers
    git-svn-id: https://avro-scala-compiler-plugin.googlecode.com/svn/trunk@101 85b7b6fe-2b2e-11df-a6fe-af80f4a49816

--- src/main/scala/runtime/ScalaSpecificRecord.scala
+++ src/main/scala/runtime/ScalaSpecificRecord.scala
@@ -1,26 +1,45 @@
 package com.googlecode.avro
 package runtime
 
+import org.apache.avro.Schema
 import org.apache.avro.io.{BinaryEncoder, BinaryDecoder, DecoderFactory}
 import org.apache.avro.specific.{SpecificDatumReader, SpecificDatumWriter, SpecificRecord}
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.io._
 
 trait ScalaSpecificRecord extends SpecificRecord {
+
+  private final lazy val __decoderFactory__ = (new DecoderFactory).configureDirectDecoder(true)
+  private final lazy val __writer__ = new SpecificDatumWriter[this.type](getSchema)
+  private final lazy val __reader__ = new SpecificDatumReader[this.type](getSchema) {
+    override def newRecord(old: AnyRef, schema: Schema) =
+      if (old ne null) old // a bit of a hack (no checking for class instance equality)
+                           // but for normal usages, old should always be an
+                           // appropriate instance (since our records are
+                           // typesafe)
+      else super.newRecord(old, schema)
+  }
+
   def toBytes: Array[Byte] = {
-    val out    = new ByteArrayOutputStream
-    val enc    = new BinaryEncoder(out)
-    val writer = new SpecificDatumWriter[ScalaSpecificRecord](getSchema)
-    writer.write(this, enc)
+    val out = new ByteArrayOutputStream(128)
+    toBytes(out)
     out.toByteArray
   }
 
+  def toBytes(outputStream: OutputStream) {
+    val enc = new BinaryEncoder(outputStream)
+    __writer__.write(this, enc)
+  }
+
   def parse(data: Array[Byte]): this.type = {
-    val stream  = new ByteArrayInputStream(data)
-    val factory = new DecoderFactory
-    val dec     = factory.createBinaryDecoder(stream, null) // new decoder
-    val reader  = new SpecificDatumReader[this.type](getSchema);
-    reader.read(this, dec)
+    val stream = new ByteArrayInputStream(data)
+    parse(stream)
+  }
+
+  def parse(inputStream: InputStream): this.type = {
+    val dec = __decoderFactory__.createBinaryDecoder(inputStream, null) // new decoder
+    __reader__.read(this, dec)
     this
   }
+
 }
