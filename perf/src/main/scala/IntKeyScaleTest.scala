70605caef62577b5682eb8c712abf9aee813aed6
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-09-11 15:49:08 -0700

    Allow the number of reads done per test to change, allow reads to timeout with out failing the whole test, provied read performance summary.

--- perf/src/main/scala/IntKeyScaleTest.scala
+++ perf/src/main/scala/IntKeyScaleTest.scala
@@ -8,6 +8,8 @@ import com.googlecode.avro.marker._
 
 import org.apache.zookeeper.CreateMode
 
+import net.lag.logging.Logger
+
 import ParallelConversions._
 
 case class WriteClient(var cluster: String, var clientId: Int) extends AvroRecord
@@ -19,8 +21,9 @@ case class ReadPerfResult(var numKeys: Int, var startTime: Long, var endTime: Lo
 object IntKeyScaleTest extends optional.Application {
   implicit val zooRoot = RClusterZoo.root
   implicit def toOption[A](a: A) = Option(a)
+  val logger = Logger()
 
-  def main(clusterSize: Int, recsPerServer: Int = 100): Unit = {
+  def main(clusterSize: Int, recsPerServer: Int = 10000, readCount: Int = 1000): Unit = {
     println("Begining cluster scale test with " + clusterSize + " servers.")
     val cluster = ScadsMesosCluster(clusterSize)
     println("Cluster located at: " + cluster.root)
@@ -34,7 +37,7 @@ object IntKeyScaleTest extends optional.Application {
     val procDesc = JvmProcess(
       "/work/marmbrus/mesos/perf-2.1.0-SNAPSHOT.jar:/work/marmbrus/mesos:/work/marmbrus/mesos/mesos-scads-2.1.0-SNAPSHOT-jar-with-dependencies.jar",
       "edu.berkeley.cs.scads.perf.IntKeyScaleClient",
-      "--clusterSize" :: clusterSize.toString :: "--recsPerServer" :: recsPerServer.toString ::"--clusterName" :: cluster.root.name :: Nil)
+      "--clusterSize" :: clusterSize.toString :: "--recsPerServer" :: recsPerServer.toString ::"--clusterName" :: cluster.root.name :: "--readCount" :: readCount.toString :: Nil)
 
     val coordination = cluster.root.getOrCreate("coordination")
     val clients = new ServiceScheduler("IntKey Scale Test")
@@ -49,7 +52,12 @@ object IntKeyScaleTest extends optional.Application {
     println("Test Done")
 
     writeResults.getRange(None, None).map(r => "Client: %d, %f".format(r._1.clientId, r._2.numKeys.toFloat / ((r._2.endTime - r._2.startTime) / 1000))).foreach(println)
-    readResults.getRange(None, None).foreach(println)
+    val r = readResults.getRange(None, None)
+    val readStart = r.map(_._2.startTime).min
+    val readEnd = r.map(_._2.endTime).max
+    val readsTotal = r.map(_._2.numKeys).sum
+    println("Total Reads: " + readsTotal)
+    println("Read Performance: " + readsTotal.toFloat / ((readEnd - readStart) / 1000))
 
     System.exit(0)
   }
@@ -58,8 +66,9 @@ object IntKeyScaleTest extends optional.Application {
 object IntKeyScaleClient extends optional.Application {
   implicit val zooRoot = RClusterZoo.root
   implicit def toOption[A](a: A) = Option(a)
+  val logger = Logger()
 
-  def main(clusterSize: Int, recsPerServer: Int, clusterName: String): Unit = {
+  def main(clusterSize: Int, recsPerServer: Int, clusterName: String, readCount: Int): Unit = {
     val clusterRoot = zooRoot(clusterName)
     val coordination = clusterRoot("coordination")
     val cluster = new ScadsCluster(clusterRoot)
@@ -87,8 +96,15 @@ object IntKeyScaleClient extends optional.Application {
 
     readResults ++= (1 to 10).pmap(threadId => {
       val startTime = System.currentTimeMillis
-      (1 to 1000).foreach(i => ns.get(IntRec(scala.util.Random.nextInt(clusterSize * recsPerServer))))
-      (ReadClient(clusterName, clientId, threadId), ReadPerfResult(10000, startTime, System.currentTimeMillis))
+      var successes = 0
+      (1 to readCount).foreach(i =>
+        try {
+          ns.get(IntRec(scala.util.Random.nextInt(clusterSize * recsPerServer)))
+          successes += 1
+        }
+        catch { case e =>  logger.debug("Exception during read %s", e)}
+      )
+      (ReadClient(clusterName, clientId, threadId), ReadPerfResult(successes, startTime, System.currentTimeMillis))
     })
 
     registerAndBlock("endRead")
