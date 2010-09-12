863611dcf37cc2f706faf0aee8ac30ed84b254da
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-09-11 18:55:40 -0700

    Start of new PIQL operators.

--- piql/src/main/scala/model/queryexecution.scala
+++ piql/src/main/scala/model/queryexecution.scala
@@ -1,197 +1,31 @@
 package edu.berkeley.cs.scads.piql
 
-import scala.collection.mutable.HashMap
-import org.apache.log4j.Logger
-import edu.berkeley.cs.scads.piql.parser.{BoundValue, BoundIntegerValue, BoundStringValue,
-                                          BoundFixedValue, BoundTrueValue, BoundFalseValue, BoundAvroRecordValue, BoundRange}
-import org.apache.avro.specific.{SpecificRecord, SpecificRecordBase}
-import org.apache.avro.generic.{IndexedRecord, GenericData}
-import edu.berkeley.cs.scads.storage.Namespace
-import edu.berkeley.Log2
+import org.apache.avro.generic.IndexedRecord
 
-abstract sealed class JoinCondition
-case class AttributeCondition(attrName: String) extends JoinCondition
-case class BoundValueLiteralCondition[T](fieldValue: BoundFixedValue[T]) extends JoinCondition
+object PhysicalOperators {
 
-case class EntityClass(name: String)
+  type Namespace = edu.berkeley.cs.scads.storage.Namespace[IndexedRecord, IndexedRecord]
+  type KeyGenerator = Seq[Value]
 
-/* Query Plan Nodes */
-abstract class QueryPlan
-abstract class TupleProvider extends QueryPlan
-abstract class EntityProvider extends QueryPlan
-case class SingleGet(namespace: String, key: List[BoundValue]) extends TupleProvider
-case class PrefixGet(namespace: String, prefix: List[BoundValue], limit: BoundRange, ascending: Boolean) extends TupleProvider
-case class SequentialDereferenceIndex(targetNamespace: String, child: TupleProvider) extends TupleProvider
-case class PrefixJoin(namespace: String, conditions: Seq[JoinCondition], limit: BoundRange, ascending: Boolean, child: EntityProvider) extends TupleProvider
-case class PointerJoin(namespace: String, conditions: Seq[JoinCondition], child: EntityProvider) extends TupleProvider
-case class Materialize(entityType: EntityClass, child: TupleProvider) extends EntityProvider
-case class Selection(equalityMap: HashMap[String, BoundValue], child: EntityProvider) extends EntityProvider
-case class Sort(fields: List[String], ascending: Boolean, child: EntityProvider) extends EntityProvider
-case class TopK(k: BoundRange, child: EntityProvider) extends EntityProvider
+  abstract class Value
+  case class FixedValue(v: Any) extends Value
 
-class Environment {
-  var namespaces: Map[String, Namespace[SpecificRecordBase, SpecificRecordBase]] = null
-}
+  case class Limit(count: Int)
 
-abstract trait QueryExecutor {
-	val qLogger = Logger.getLogger("scads.queryexecution")
-	/* Type Definitions */
-	type TupleStream = Seq[(SpecificRecordBase, SpecificRecordBase)]
-	type EntityStream = Seq[Entity[_,_]]
+  trait Predicate
+  case class Equality(v1: Value, v2: Value) extends Predicate
 
-  implicit def toBoundInt(i: Int) = BoundIntegerValue(i)
-  implicit def toBoundString(s: String) = BoundStringValue(s)
-  implicit def toBoundBoolean(b: Boolean) =
-    if (b)
-      BoundTrueValue
-    else
-      BoundFalseValue
+  /* Query Plan Nodes */
+  abstract class PhysicalOperator
 
-  implicit def toBoundRecord(rec: SpecificRecordBase) = BoundAvroRecordValue(rec)
+  /* Remote Operators */
+  case class IndexLookup(namespace: Namespace, key: KeyGenerator) extends PhysicalOperator
+  case class IndexScan(namespace: Namespace, keyPrefix: KeyGenerator, limit: Limit, ascending: Boolean) extends PhysicalOperator
+  case class SequentialDereferenceIndex(targetNamespace: Namespace, child: PhysicalOperator) extends PhysicalOperator
+  case class PrefixJoin(namespace: Namespace, keyPrefix: KeyGenerator, limit: Limit, ascending: Boolean, child: PhysicalOperator) extends PhysicalOperator
+  case class PointerJoin(namespace: Namespace, key: KeyGenerator, child: PhysicalOperator) extends PhysicalOperator
 
-	/* Tuple Providers */
-	protected def singleGet(namespace: String, key: List[BoundValue])(implicit env: Environment): TupleStream = {
-    qLogger.debug("test")
-    Log2.debug(qLogger, "singleGet", namespace, key)
-    val ns = env.namespaces(namespace)
-    val keyRec = ns.keyClass.newInstance()
-    key.zipWithIndex.foreach {
-      case(v: BoundFixedValue[_], idx: Int) => keyRec.put(idx, v.value)
-    }
-
-    val result = ns.get(keyRec) match {
-      case Some(v) => List((keyRec, v))
-      case None => Nil
-    }
-
-    Log2.debug(qLogger, "singleGet Result:", result)
-    return result
-  }
-
-  //TODO: Use limit/ascending parameters
-	protected def prefixGet(namespace: String, prefix: List[BoundValue], limit: BoundValue, ascending: Boolean)(implicit env: Environment): TupleStream = {
-    Log2.debug(qLogger, "prefixGet", namespace, prefix, limit, boolean2Boolean(ascending))
-    val ns = env.namespaces(namespace)
-    val key = ns.keyClass.newInstance()
-    prefix.zipWithIndex.foreach {
-      case (value: BoundFixedValue[_], idx: Int) => key.put(idx, value.value)
-    }
-    val result = ns.getPrefix(key, prefix.length)
-    Log2.debug(qLogger, "singleGet result:", result)
-    return result
-  }
-
-  //TODO: Deal with values that return None
-	protected def sequentialDereferenceIndex(targetNamespace: String, child: TupleStream)(implicit env: Environment): TupleStream = {
-    Log2.debug(qLogger, "sequentialDereferenceIndex", targetNamespace, child)
-    val ns = env.namespaces(targetNamespace)
-    val result = child.map(c => (c._2, ns.get(c._2).get))
-    Log2.debug(qLogger, "sequentialDereferenceIndex result:", result)
-    return result
-  }
-
-  private def mkKey(keyClass: Class[EntityPart], conditions: List[JoinCondition], entity: Entity[_,_]): SpecificRecordBase = {
-    val key = keyClass.newInstance()
-    val keyParts = conditions.flatMap {
-      case AttributeCondition(attrName) => entity.get(attrName) match {
-        case ep: EntityPart => ep.flatValues
-        case value => List(value)
-      }
-      case bv: BoundValueLiteralCondition[_] => List(bv.fieldValue.value)
-    }
-
-    println("keyParts:" + keyParts)
-
-    keyParts.zipWithIndex.foreach {
-      case(v: Any, idx: Int) => {println("setting " + idx + " " + v); key.flatPut(idx, v)}
-    }
-    return key
-  }
-
-  //TODO: parallelize
-	protected def prefixJoin(namespace: String, conditions: List[JoinCondition], limit: BoundIntegerValue, ascending: Boolean, child: EntityStream)(implicit env: Environment): TupleStream = {
-    Log2.debug(qLogger, "prefixJoin", namespace, conditions, limit, boolean2Boolean(ascending), child)
-    val ns = env.namespaces(namespace)
-    val result = child.flatMap(c => {
-      val key = mkKey(ns.keyClass.asInstanceOf[Class[EntityPart]], conditions, c)
-      Log2.debug(qLogger, "prefixJoin doing get on key: ", key)
-      ns.getPrefix(key, conditions.length, limit.value, ascending)
-    })
-    Log2.debug(qLogger, "prefixJoin result: ", result)
-    return result
-  }
-
-	protected def pointerJoin(namespace: String, conditions: List[JoinCondition], child: EntityStream)(implicit env: Environment): TupleStream = {
-    Log2.debug(qLogger, "pointerJoin", namespace, conditions, child)
-    val ns = env.namespaces(namespace)
-    val result = child.map(c => {
-      val key = mkKey(ns.keyClass.asInstanceOf[Class[EntityPart]], conditions, c)
-
-      Log2.debug(qLogger, "pointerJoin doing get on key: ", key)
-      (key, ns.get(key).get)
-    })
-    Log2.debug(qLogger, "pointerJoin result:", result)
-    return result
-  }
-
-	/* Entity Providers */
-	protected def materialize(entityClass: Class[Entity[_,_]], child: TupleStream)(implicit env: Environment): EntityStream = {
-    Log2.debug(qLogger, "materialize", entityClass, child)
-    val result = child.map(c => {
-      def mkEntityClass(key: Array[Byte], value: Array[Byte]): Entity[SpecificRecordBase, SpecificRecordBase] = {
-        val e = entityClass.newInstance().asInstanceOf[Entity[SpecificRecordBase,SpecificRecordBase]]
-        e.key.parse(key)
-        e.value.parse(value)
-        e
-      }
-      val retVal = mkEntityClass(c._1.toBytes, c._2.toBytes)
-      val cpy = mkEntityClass(c._1.toBytes, c._2.toBytes)
-      retVal.oldEntity = cpy.asInstanceOf[retVal.EntityType]
-      retVal
-    })
-    Log2.debug(qLogger, "materialize result:", result)
-    return result
-  }
-
-	protected def selection(equalityMap: HashMap[String, BoundValue], child: EntityStream): EntityStream = {
-    Log2.debug(qLogger, "selection", equalityMap, child)
-    val result = child.filter(c => {
-      equalityMap.map {
-        // TODO: make BoundFixedValue have a compareTo method so we don't have
-        // to check for specific types
-        case (attrName: String, bar: BoundAvroRecordValue) =>
-          c.get(attrName) match {
-            case sr: SpecificRecord =>
-              /** This kludge is because the equals method in
-               * SpecificRecordBase requires that the name of the schema is
-               * identical to be considered equal, and since only require
-               * structural equality, this hack is necessary */
-              (bar.value compareTo sr) == 0
-            case _ => false
-          }
-        case (attrName: String, bv: BoundFixedValue[_]) =>
-          c.get(attrName) equals bv.value
-      }.reduceLeft(_&_)
-    })
-    Log2.debug(qLogger, "selection result: ", result)
-    return result
-  }
-
-	protected def sort(fields: List[String], ascending: Boolean, child: EntityStream): EntityStream = {
-    Log2.debug(qLogger, "sort", fields, boolean2Boolean(ascending), child)
-    val comparator = (a: Entity[_,_], b: Entity[_,_]) => {
-      fields.map(f => (a.get(f), b.get(f)) match {
-        case (x: Integer, y: Integer) => x.intValue() < y.intValue()
-        case (x: String, y: String) => x < y
-      }).reduceLeft(_&_)
-    }
-
-    val ret = child.toArray
-    scala.util.Sorting.stableSort(ret, comparator)
-    val result = if(ascending) ret else ret.reverse
-    Log2.debug(qLogger, "sort result:", result)
-    return result
-  }
-
-	protected def topK(k: BoundIntegerValue, child: EntityStream): EntityStream = child.take(k.value)
+  case class Selection(predicate: Predicate, child: PhysicalOperator) extends PhysicalOperator
+  case class Sort(fields: List[String], ascending: Boolean, child: PhysicalOperator) extends PhysicalOperator
+  case class TopK(k: Limit, child: PhysicalOperator) extends PhysicalOperator
 }
