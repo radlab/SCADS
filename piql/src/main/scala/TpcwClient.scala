27229ed9cf2b732ef96e0ec2798d974cc1203f74
Tim Kraska <kraska@cs.berkeley.edu>
2010-09-23 17:26:30 -0700

    TPC-W schema and first queries

---
+++ piql/src/main/scala/TpcwClient.scala
@@ -0,0 +1,58 @@
+package edu.berkeley.cs.scads.piql
+
+import edu.berkeley.cs.scads.storage._
+import edu.berkeley.cs.avro.marker._
+
+import org.apache.avro.util._
+
+class TpcwClient(cluster: ScadsCluster, executor: QueryExecutor) {
+    val address = cluster.getNamespace[AddressKey, AddressValue]("address")
+    val author = cluster.getNamespace[AuthorKey, AuthorValue]("author")
+    val xacts = cluster.getNamespace[CcXactsKey, CcXactsValue]("xacts")
+    val country = cluster.getNamespace[CountryKey, CountryValue]("country")
+    val customer = cluster.getNamespace[CustomerKey, CustomerValue]("customer")
+    val customerNameIndex = cluster.getNamespace[CustomerNameKey, CustomerKey]("customer_index")  //Extra index
+    val item = cluster.getNamespace[ItemKey, ItemValue]("item")
+    val itemSubjectDateTitleIndex = cluster.getNamespace[ItemSubjectDateTitleIndexKey, ItemKey]("item_subject_date_title_index")
+    val orderline = cluster.getNamespace[OrderLineKey, OrderLineValue]("orderline")
+    val order = cluster.getNamespace[OrdersKey, OrdersValue]("orders")
+
+    implicit def toGeneric(ns: SpecificNamespace[_, _]) = ns.genericNamespace
+
+    val homeWIPlan = IndexLookupJoin(customer, Array(AttributeValue(0, 1)), IndexLookup(customerNameIndex, Array(ParameterValue(0))))
+    def homeWI(username: String): QueryResult =
+      exec(homeWIPlan, new Utf8(username))
+
+
+    //TODO Still missing the like operator. Does the like really work here???
+    val newProductPlan = IndexLookupJoin(
+                            author,
+                            Array(AttributeValue(0, 1)), 
+                            IndexLookupJoin(
+                               item,
+                               Array(AttributeValue(0, 1)),
+                               IndexLookup(itemSubjectDateTitleIndex, Array(ParameterValue(0))))
+                          )
+    def newProductWI(subject: String): QueryResult =
+      exec(newProductPlan, new Utf8(username))
+
+    val productDetailPlan = IndexLookupJoin(
+                               author,
+                               Array(AttributeValue(0, 2)),
+                               IndexLookup(item, Array(ParameterValue(0)))
+                            )
+    def productDetailWI(bookId: Int): QueryResult =
+      exec(newProductPlan, bookId)
+
+  
+
+    def exec(plan: QueryPlan, args: Any*) = {
+      val iterator = executor(plan, args:_*)
+      iterator.open
+      iterator.toList
+    }
+
+  //AttributeValue does it start counting from 0 or 1???
+  //Questions: Why das Attribute value take a record ID?
+  //should we rename KeyGenerator to projection?
+}
---
+++ piql/src/main/scala/TpcwSchema.scala
@@ -0,0 +1,12 @@
+package
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tim
+ * Date: Sep 23, 2010
+ * Time: 2:46:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class TpcwSchema
