fbc9c5919ef1d32c59ce74c9adaf66ea7177ff63
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-09-28 23:55:05 -0700

    make available server advertisements sequential ephemeral.

--- scalaengine/src/main/scala/server/StorageHandler.scala
+++ scalaengine/src/main/scala/server/StorageHandler.scala
@@ -138,7 +138,7 @@ class StorageHandler(env: Environment, val root: ZooKeeperProxy#ZooKeeperNode)
     /* Register with the zookeper as an available server */
     val availServs = root.getOrCreate("availableServers")
     logger.debug("Created StorageHandler" + remoteHandle.toString)
-    availServs.createChild(remoteHandle.toString, remoteHandle.toBytes, CreateMode.EPHEMERAL)
+    availServs.createChild(remoteHandle.toString, remoteHandle.toBytes, CreateMode.EPHEMERAL_SEQUENTIAL)
 
     /* Reopen partitions */
     val cursor = partitionDb.openCursor(null, null)
