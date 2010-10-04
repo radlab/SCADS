eb9ec739c0b90439d733da27f512267a018a68fa
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-10-04 12:54:07 -0700

    Move the JavaExecutor and ServiceScheduler to deploylib.

--- mesos/src/main/resources/java_executor.r
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec /usr/lib/jvm/java-6-sun/bin/java -Djava.library.path=/work/mesos-production/lib/java -cp /work/marmbrus/mesos/log4j.properties:/work/marmbrus/mesos/mesos-scads-2.1.0-SNAPSHOT-jar-with-dependencies.jar edu.berkeley.cs.scads.mesos.JavaExecutor $@
--- mesos/upload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn assembly:assembly -DskipTests
-rsync -av target/mesos-scads-2.1.0-SNAPSHOT-jar-with-dependencies.jar r12:/work/marmbrus/mesos
