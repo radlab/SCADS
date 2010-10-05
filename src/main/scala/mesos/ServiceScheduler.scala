9ca91a06802b006dd69cf94f1664e634651ff747
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-10-04 19:08:09 -0700

    Switch to ExperimentScheduler.

--- src/main/scala/mesos/ServiceScheduler.scala
+++ src/main/scala/mesos/ServiceScheduler.scala
@@ -7,26 +7,26 @@ import net.lag.logging.Logger
 
 import scala.collection.JavaConversions._
 
-object ServiceScheduler {
+object ExperimentScheduler {
   System.loadLibrary("mesos")
 
-  def apply(name: String, mesosMaster: String = "1@" + java.net.InetAddress.getLocalHost.getHostAddress + ":5050") = new ServiceScheduler(name, mesosMaster)
+  def apply(name: String, mesosMaster: String = "1@" + java.net.InetAddress.getLocalHost.getHostAddress + ":5050") = new ExperimentScheduler(name, mesosMaster)
 }
 
-class ServiceScheduler protected (name: String, mesosMaster: String) extends Scheduler {
-  /* Self reference for use in the driver thread */
-  self =>
-
+class ExperimentScheduler protected (name: String, mesosMaster: String) extends Scheduler {
   val logger = Logger()
   var taskId = 0
+  var driver = new MesosSchedulerDriver(this, mesosMaster)
 
-  val thread = new Thread() {
-    override def run(): Unit = new MesosSchedulerDriver(self, mesosMaster).run();
+  val driverThread = new Thread("ExperimentScheduler Mesos Driver Thread") { override def run(): Unit = driver.run() }
+  driverThread.start()
+
+  type ProcessList = IndexedSeq[JvmProcess]
+  var outstandingExperiments = List[ProcessList]()
+
+  def scheduleExperiment(processes: ProcessList): Unit = synchronized {
+    outstandingExperiments ::= processes
   }
-  thread.start()
-
-  case class ServiceDescription(mem: Int, cpus: Int, desc: JvmProcess)
-  var outstandingTasks = List[ServiceDescription]()
 
   override def getFrameworkName(d: SchedulerDriver): String = "SCADS Service Framework: " + name
   override def getExecutorInfo(d: SchedulerDriver): ExecutorInfo = new ExecutorInfo("/root/java_executor", Array[Byte]())
@@ -34,24 +34,27 @@ class ServiceScheduler protected (name: String, mesosMaster: String) extends Sch
 
   override def resourceOffer(d: SchedulerDriver, oid: String, offers: java.util.List[SlaveOffer]) = {
     val tasks = new java.util.LinkedList[TaskDescription]
-    val taskParams = new java.util.TreeMap[String, String]
 
+    var remainingExperiments = List[ProcessList]()
     synchronized {
-     outstandingTasks = outstandingTasks.filter(task => {
-        val slotIndex = offers.findIndexOf(o => o.getParams.get("cpus").toInt >= task.cpus && o.getParams.get("mem").toInt >= task.mem)
+      for(procList <- outstandingExperiments) {
+        if(procList.size <= offers.size) {
+          procList.foreach(proc => {
+            val offer = offers.remove(0)
+            val taskParams = Map(List("mem", "cpus").map(k => k -> offer.getParams.get(k)):_*)
+            val task = new TaskDescription(taskId, offer.getSlaveId, proc.mainclass, taskParams, proc.toBytes)
+            taskId += 1
 
-        if(slotIndex != -1) {
-          val offer = offers.get(slotIndex)
-          offers.remove(slotIndex)
-          tasks.add(new TaskDescription(taskId, offer.getSlaveId(), task.desc.mainclass, Map("mem" -> task.mem.toString, "cpus" -> task.cpus.toString), task.desc.toBytes))
-          taskId += 1
-          false
+            logger.info("Scheduling experiment: %s", procList)
+            tasks add task
+          })
         }
         else {
-          logger.info("Unable to schedule: " + task)
-          true
+          logger.debug("Not enough resources to schedule experiment of size %d", procList.size)
+          remainingExperiments ::= procList
         }
-      })
+      }
+      outstandingExperiments = remainingExperiments
     }
 
     d.replyToOffer(oid, tasks, Map[String,String]())
@@ -68,8 +71,5 @@ class ServiceScheduler protected (name: String, mesosMaster: String) extends Sch
     }
   }
 
-  def runService(mem: Int, cores: Int, desc: JvmProcess): Unit = synchronized {
-    logger.debug("Running Service %s with %d cores and %dMb RAM", desc, cores, mem)
-    outstandingTasks ::= ServiceDescription(mem, cores, desc)
-  }
+  def stop = driver.stop
 }
