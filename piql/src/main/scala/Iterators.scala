66800cf454cbae5e674ffa6517061b112a8b0304
Stephen Tu <stephen_tu@berkeley.edu>
2010-10-13 14:56:07 -0700

    add lazy operators

--- piql/src/main/scala/Iterators.scala
+++ piql/src/main/scala/Iterators.scala
@@ -120,9 +120,11 @@ trait QueryExecutor {
   }
 }
 
+
+
 class SimpleExecutor extends QueryExecutor {
 
-  implicit def toOption[A](a: A)= Option(a)
+  implicit def toOption[A](a: A): Option[A] = Option(a)
 
   def apply(plan: QueryPlan)(implicit ctx: Context): QueryIterator = plan match {
     case IndexLookup(namespace, key) => {
@@ -619,6 +621,246 @@ class ParallelExecutor extends SimpleExecutor {
       }
     }
 
+    case _ => super.apply(plan)
+  }
+
+}
+
+
+class LazyExecutor extends SimpleExecutor {
+
+  override def apply(plan: QueryPlan)(implicit ctx: Context): QueryIterator = plan match {
+    case IndexLookup(namespace, key) => {
+      new QueryIterator {
+        val name = "LazyIndexLookup"
+          
+        private val boundKey = bindKey(namespace, key)
+
+        private var accessed = false
+        private lazy val result = namespace.get(boundKey)
+
+        def open {}
+        def close { accessed = true }
+
+        def hasNext = accessed match {
+          case false => result.isDefined
+          case true  => false
+        }
+
+        def next = {
+          if (!hasNext)
+            throw new ju.NoSuchElementException("Next on empty iterator")
+          accessed = true
+          Array(boundKey, result.get)
+        }
+      }
+    }
+    case IndexScan(namespace, keyPrefix, _, ascending) => { // we don't care about limit here
+        new QueryIterator {
+          val name = "LazyIndexScan"
+
+          private val boundKeyPrefix = bindKey(namespace, keyPrefix)
+
+          private var result: (Record, Record) = null 
+
+          private var offset = 0
+          private var limitReached = false
+
+          @inline private def doFetch() {
+            logger.debug("BoundKeyPrefix: %s", boundKeyPrefix)
+            val res = namespace.getRange(boundKeyPrefix, boundKeyPrefix, offset=offset, limit=1, ascending=ascending)
+            logger.debug("IndexScan Fetch Returned %s, with offset %d, limit %d", res, offset, 1)
+            offset += res.size
+            if (res.isEmpty) {
+              limitReached = true
+              result = null
+            } else {
+              result = res.head
+            }
+            assert(limitReached || (result ne null))
+          }
+
+          def open {}
+
+          def close {
+            result = null
+            limitReached = true
+          }
+
+          def hasNext = 
+            if (result ne null) true
+            else if (!limitReached) {
+              doFetch()
+              hasNext
+            } else false
+
+          def next = {
+            if (!hasNext)
+              throw new ju.NoSuchElementException("Next on empty iterator")
+            val ret = Array(result._1, result._2)
+            result = null
+            ret
+          }
+        }
+    }
+    case IndexLookupJoin(namespace, key, child) => {
+      new QueryIterator {
+        val name = "LazyIndexLookupJoin"
+
+        private val childIterator = apply(child)
+        private var nextTuple: Tuple = null
+
+        private var needsInit = true
+
+        def open {}
+        def close {
+          if (needsInit) needsInit = false
+          else childIterator.close
+        }
+
+        def hasNext = {
+          if (needsInit) {
+            needsInit = false
+            getNext
+          }
+          (nextTuple != null)
+        }
+
+        def next = {
+          val ret = nextTuple
+          getNext
+          ret
+        }
+
+        private def getNext: Unit = {
+          while(childIterator.hasNext) {
+            val childTuple = childIterator.next
+            val boundKey = bindKey(namespace, key, childTuple)
+            val value = namespace.get(boundKey)
+
+            if(value.isDefined) {
+              nextTuple = childTuple ++ Array[Record](boundKey, value.get)
+              return
+            }
+          }
+          nextTuple = null
+        }
+      }
+    }
+    case IndexMergeJoin(namespace, keyPrefix, sortFields, _, ascending, child) => {
+      new QueryIterator {
+        val name = "LazyIndexMergeJoin"
+
+        private val childIterator = apply(child)
+
+        /** (key, child tup, offset, limit reached?) */
+        private var tupleData: Array[(Record, Tuple, Int, Boolean)] = null
+
+        private var tupleBuffers: Array[IndexedSeq[Tuple]] = null
+
+        private var bufferPos: Array[Int] = null
+
+        private var nextTuple: Tuple = null
+
+        private var needsInit = true
+
+        private def init() {
+          childIterator.open
+
+          val tupleDatum = childIterator.map(childValue => {
+            val boundKeyPrefix = bindKey(namespace, keyPrefix, childValue)
+            val records = namespace.getRange(boundKeyPrefix, boundKeyPrefix, limit=1, ascending=ascending)
+            logger.debug("IndexMergeJoin Prefetch Using Key %s: %s", boundKeyPrefix, records)
+
+            val recIdxSeq = records.map(r => childValue ++ Array[Record](r._1, r._2)).toIndexedSeq
+            (boundKeyPrefix, childValue, records.size, records.size < 1, recIdxSeq)
+          }).toSeq
+
+          tupleData = tupleDatum.map(x => (x._1, x._2, x._3, x._4)).toArray
+
+          tupleBuffers = tupleDatum.map(_._5).toArray
+          bufferPos = Array.fill(tupleBuffers.size)(0)
+
+          getNext // load the first result
+        }
+
+        def open {}
+
+        def close { 
+          childIterator.close
+          needsInit = false
+        }
+
+        def hasNext = {
+          if (needsInit) {
+            init()
+            needsInit = false
+          }
+          (nextTuple != null)
+        }
+
+        def next = {
+          if (!hasNext)
+            throw new ju.NoSuchElementException("Next on empty iterator")
+          val ret = nextTuple
+          getNext
+          ret
+        }
+
+        private def fillBuffer(i: Int) {
+          val (key, tup, offset, limitReached) = tupleData(i)
+          assert(!limitReached)
+          val records = namespace.getRange(key, key, offset=offset, limit=1, ascending=ascending)
+          logger.debug("IndexMergeJoin Fetch Using Key %s: %s", key, records)
+          tupleBuffers(i) ++= records.map(r => tup ++ Array[Record](r._1, r._2)).toIndexedSeq
+          tupleData(i) = ((key, tup, offset + records.size, records.size < 1))
+        }
+
+        private def bufferLimitReached(i: Int) =
+          tupleData(i)._4
+
+        private def getNext: Unit = {
+
+          // find the first available buffer
+          var minIdx = -1 
+          var idx = 0
+          while (minIdx == -1 && idx < tupleBuffers.size) {
+
+            // if this buffer has already been scanned over but we can still fetch from KV store
+            if (bufferPos(idx) == tupleBuffers(idx).size && !bufferLimitReached(idx)) { 
+              fillBuffer(idx) // do the fetch
+            }
+
+            // if there is a buffer with contents, then we've found the start
+            if (bufferPos(idx) < tupleBuffers(idx).size) { 
+              minIdx = idx
+            }
+            idx += 1
+          }
+
+          if (minIdx == -1) nextTuple = null
+          else { 
+            for(i <- ((minIdx + 1) to (tupleBuffers.size - 1))) {
+              if (bufferPos(i) == tupleBuffers(i).size && !bufferLimitReached(i)) {
+                fillBuffer(i)
+              }
+
+              if (bufferPos(i) < tupleBuffers(i).size) {
+                if((ascending && (compareTuples(tupleBuffers(i)(bufferPos(i)), tupleBuffers(minIdx)(bufferPos(minIdx)), sortFields) < 0)) ||
+                  (!ascending && (compareTuples(tupleBuffers(i)(bufferPos(i)), tupleBuffers(minIdx)(bufferPos(minIdx)), sortFields) > 0))) {
+                  minIdx = i
+                }
+              }
+            }
+            nextTuple = tupleBuffers(minIdx)(bufferPos(minIdx))
+            bufferPos(minIdx) += 1 
+            // NO prefetching here if minIdx buffer becomes entirely scanned-
+            // this is unlike ParallelIndexMergeJoin which does a prefetch for
+            // this case
+          }
+        }
+      }
+    }
     case _ => super.apply(plan)
   }
 
