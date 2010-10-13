20ed8e98f979842a0ff5428237e3d57c767a6d44
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-10-12 22:05:34 -0700

    upload into framework directory on instance.

--- src/main/scala/mesos/MesosEC2.scala
+++ src/main/scala/mesos/MesosEC2.scala
@@ -7,11 +7,13 @@ import config._
 import java.io.File
 
 object MesosEC2 extends ConfigurationActions {
+  val rootDir = new File("/root/mesos/frameworks/deploylib")
   def updateDeploylib: Unit = {
     val executorScript = Util.readFile(new File("src/main/resources/java_executor"))
     EC2Instance.activeInstances.pforeach(inst => {
-      inst.upload(new File("target/deploy-2.1-SNAPSHOT-jar-with-dependencies.jar"), new File("/root"))
-      createFile(inst, new File("/root/java_executor"), executorScript, "755")
+      createDirectory(inst, rootDir)
+      uploadFile(inst, new File("target/deploy-2.1-SNAPSHOT-jar-with-dependencies.jar"), rootDir)
+      createFile(inst, new File(rootDir, "java_executor"), executorScript, "755")
     })
   }
 }
