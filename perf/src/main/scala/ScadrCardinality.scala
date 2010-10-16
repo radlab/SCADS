8046d4622fd42428c0f7ecb4f13865c19ce44f59
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-10-15 17:20:03 -0700

    Interchangeable executor, record client config.

--- perf/src/main/scala/ScadrCardinality.scala
+++ perf/src/main/scala/ScadrCardinality.scala
@@ -11,16 +11,16 @@ import avro.marker._
 import deploylib._
 import deploylib.mesos._
 
-case class ScadrCardinalityClient(var cardinality: Int, var cluster: String, var clientId: Int, var iteration: Int, var threadId: Int) extends AvroRecord
-case class ScadrCardinalityResult(var times: Histogram, var skips: Int) extends AvroRecord
 
 object ScadrCardinalityTest extends Experiment {
-  val results = resultCluster.getNamespace[ScadrCardinalityClient, ScadrCardinalityResult]("scadrCardinality")
+  case class ResultKey(var clientConfig: LoadClient, var clientId: Int, var iteration: Int, var threadId: Int) extends AvroRecord
+  case class ResultValue(var times: Histogram, var skips: Int) extends AvroRecord
+  val results = resultCluster.getNamespace[ResultKey, ResultValue]("scadrCardinality")
 
   def clear = results.getRange(None, None).foreach(r => results.put(r._1, None))
 
   def printResults: Unit = {
-    val runs = results.getRange(None, None).groupBy(k => (k._1.cluster, k._1.iteration)).filterNot(_._1._2 == 1).values
+    val runs = results.getRange(None, None).groupBy(k => (k._1.clientConfig, k._1.iteration)).filterNot(_._1._2 == 1).values
     runs.foreach(run => {
       val totalRequests = run.map(_._2.times.buckets.sum).sum
       val aggregrateHistogram = run.map(_._2.times).reduceLeft(_ + _)
@@ -29,11 +29,11 @@ object ScadrCardinalityTest extends Experiment {
       val quantile99ResponseTime = cumulativeHistogram.findIndexOf(_ >= totalRequests * 0.99) * aggregrateHistogram.bucketSize
       val quantile999ResponseTime = cumulativeHistogram.findIndexOf(_ >= totalRequests * 0.999) * aggregrateHistogram.bucketSize
 
-      println(List(run.head._1.cardinality, totalRequests, quantile50ResponseTime, quantile99ResponseTime, quantile999ResponseTime).mkString("\t"))
+      println(List(run.head._1.clientConfig.followingCardinality, run.head._1.clientConfig.executorClass, totalRequests, quantile50ResponseTime, quantile99ResponseTime, quantile999ResponseTime).mkString("\t"))
     })
   }
 
-  def run(followingCardinality: Int, clusterSize: Int = 10)(implicit classpath: Seq[ClassSource], scheduler: ExperimentScheduler): ZooKeeperProxy#ZooKeeperNode = {
+  def run(followingCardinality: Int, executorClass: String = "edu.berkeley.cs.scads.piql.SimpleExecutor", clusterSize: Int = 10)(implicit classpath: Seq[ClassSource], scheduler: ExperimentScheduler): ZooKeeperProxy#ZooKeeperNode = {
     val expRoot = newExperimentRoot
 
     scheduler.scheduleExperiment(
@@ -43,7 +43,8 @@ object ScadrCardinalityTest extends Experiment {
           expRoot.canonicalAddress,
           clusterSize,
           clusterSize,
-          followingCardinality
+          followingCardinality,
+          executorClass
         )
       ) * clusterSize
     )
@@ -51,12 +52,13 @@ object ScadrCardinalityTest extends Experiment {
     expRoot
   }
 
-  case class LoadClient(var clusterAddress: String, var numServers: Int, var numClients: Int, var followingCardinality: Int, var iterations: Int = 5, var threads: Int = 50, var runLengthMin: Int = 5 ) extends AvroRecord with Runnable {
+  case class LoadClient(var clusterAddress: String, var numServers: Int, var numClients: Int, var followingCardinality: Int, var executorClass: String, var iterations: Int = 5, var threads: Int = 50, var runLengthMin: Int = 5 ) extends AvroRecord with Runnable {
     def run() = {
       val clusterRoot = ZooKeeperNode(clusterAddress)
       val coordination = clusterRoot.getOrCreate("coordination")
       val cluster = new ScadsCluster(clusterRoot)
-      val scadrClient = new ScadrClient(cluster, new SimpleExecutor)
+      var executor = Class.forName(executorClass).newInstance.asInstanceOf[QueryExecutor]
+      val scadrClient = new ScadrClient(cluster, executor)
       val loader = new ScadrLoader(scadrClient, numClients,
         numUsers = numServers * 10000,
         numThoughtsPerUser = 100,
@@ -103,7 +105,7 @@ object ScadrCardinalityTest extends Experiment {
           }
 
           logger.info("Thread %d complete", threadId)
-          (ScadrCardinalityClient(followingCardinality, clusterAddress, clientId, iteration, threadId), ScadrCardinalityResult(histogram, skips))
+          (ResultKey(this, clientId, iteration, threadId), ResultValue(histogram, skips))
         })
 
         coordination.registerAndAwait("iteration" + iteration, numClients)
