82beffa86903baaadd9e916720f55b71e2a35f0d
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-10-16 12:22:33 -0700

    Add some deployment helper functions.

--- perf/src/main/scala/Util.scala
+++ perf/src/main/scala/Util.scala
@@ -1,6 +1,37 @@
-package edu.berkeley.cs.scads.perf
+package edu.berkeley.cs.scads
+package perf
 
 import scala.concurrent.SyncVar
+
+import comm._
+import deploylib._
+import deploylib.config._
+import deploylib.mesos._
+import deploylib.ec2._
+
+import java.io.File
+
+object Deploy extends ConfigurationActions {
+  implicit def toFile(str: String) = new java.io.File(str)
+
+  def classpath = System.getProperty("java.class.path").split(":")
+  def s3Classpath = (classpath :+ "target/perf-2.1.0-SNAPSHOT-jar-with-dependencies.jar").filter(_ endsWith "jar").map(f => S3CachedJar(S3Cache.getCacheUrl(new File(f))))
+  def codeS3Classpath = s3Classpath.map(j => """S3CachedJar("%s")""".format(j.url)).toList.toString
+
+  def deployJars: Unit = {
+    EC2Instance.activeInstances.pforeach(i => i.upload("target/perf-2.1.0-SNAPSHOT-jar-with-dependencies.jar", "/root"))
+    EC2Instance.activeInstances.pforeach(i => i.upload("target/perf-2.1.0-SNAPSHOT.jar", "/root"))
+    EC2Instance.activeInstances.pforeach(i => i.upload("setup.scala", "/root"))
+  }
+
+  def deployCurrentClassPath: Unit = {
+    val localSetupFile = Util.readFile("setup.scala")
+    val remoteFileLines = ("implicit val classpath = %s".format(codeS3Classpath)) :+
+      localSetupFile.split("\n").filterNot(_ contains "classpath")
+
+    createFile(MesosEC2.master, "/root/setup.scala", remoteFileLines.mkString("\n"), "644")
+  }
+}
 
 object Future {
 	def apply[A](f: => A): Future[A] = new Future(f)
