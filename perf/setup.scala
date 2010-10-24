e74b32bcf971c1b326221057977d3c78f84f8b10
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-10-23 17:47:55 -0700

    add zookeeper to perf setup.

--- perf/setup.scala
+++ perf/setup.scala
@@ -14,3 +14,4 @@ import scadr.scale.ScadrScaleExperiment
 
 implicit val scheduler = LocalExperimentScheduler(System.getProperty("user.name") + " console", "1@10.123.6.101:5050")
 implicit def classpath = Deploy.s3Classpath
+lazy val ec2zoo = ZooKeeperNode("zk://mesos-ec2/")
