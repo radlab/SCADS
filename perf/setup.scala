d3075b0a6ddc6f8a24435a6c64091674430179f5
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-10-27 00:56:51 -0700

    pull out specfic conf vars.

--- perf/setup.scala
+++ perf/setup.scala
@@ -10,8 +10,3 @@ import java.io.File
 
 import edu.berkeley.cs.scads.perf._
 import scadr.cardinality.CardinalityExperiment
-import scadr.scale.ScadrScaleExperiment
-
-implicit val scheduler = LocalExperimentScheduler(System.getProperty("user.name") + " console", "1@10.123.6.101:5050")
-implicit def classpath = Deploy.s3Classpath
-lazy val ec2zoo = ZooKeeperNode("zk://mesos-ec2/")
