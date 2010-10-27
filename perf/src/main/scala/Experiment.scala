69d200607ec4095ad16136204b34a1ab02950c45
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-10-27 00:53:13 -0700

    Move AvroClientMain out.

--- perf/src/main/scala/Experiment.scala
+++ perf/src/main/scala/Experiment.scala
@@ -8,17 +8,10 @@ import edu.berkeley.cs.scads.storage._
 import edu.berkeley.cs.avro.runtime._
 import edu.berkeley.cs.avro.marker._
 
-import org.apache.avro.generic.IndexedRecord
 import org.apache.zookeeper.CreateMode
 
 import java.io.File
 import net.lag.logging.Logger
-
-abstract trait AvroClient extends IndexedRecord {
-  val logger = Logger()
-
-  def run(clusterRoot: ZooKeeperProxy#ZooKeeperNode)
-}
 
 class ExperimentalScadsCluster(root: ZooKeeperProxy#ZooKeeperNode) extends ScadsCluster(root) {
   val logger = Logger()
@@ -28,26 +21,6 @@ class ExperimentalScadsCluster(root: ZooKeeperProxy#ZooKeeperNode) extends Scads
       logger.info("Waiting for cluster to start " + cluster.getAvailableServers.size + " of " + clusterSize + " ready.")
       Thread.sleep(1000)
     }
-  }
-}
-
-object AvroClientMain {
-  val logger = Logger()
-
-  def main(args: Array[String]): Unit = {
-    if(args.size == 3)
-    try {
-      val clusterRoot = ZooKeeperNode(args(1))
-      Class.forName(args(0)).newInstance.asInstanceOf[AvroClient].parse(args(2)).run(clusterRoot)
-    }
-    catch {
-      case error => {
-        logger.fatal(error, "Exeception in Main Thread.  Killing process.")
-        System.exit(-1)
-      }
-    }
-    else
-      println("Usage: " + this.getClass.getName + "<class name> <zookeeper address> <json encoded avro client description>")
   }
 }
 
@@ -68,7 +41,7 @@ abstract trait Experiment {
       "edu.berkeley.cs.scads.storage.ScalaEngine",
       "--clusterAddress" :: clusterAddress :: Nil)
 
-  implicit def clientJvmProcess(loadClient: AvroClient, clusterRoot: ZooKeeperProxy#ZooKeeperNode)(implicit classpath: Seq[ClassSource]): JvmProcess =
+  def clientJvmProcess(loadClient: AvroClient, clusterRoot: ZooKeeperProxy#ZooKeeperNode)(implicit classpath: Seq[ClassSource]): JvmProcess =
     JvmProcess(classpath,
       "edu.berkeley.cs.scads.perf.AvroClientMain",
       loadClient.getClass.getName :: clusterRoot.canonicalAddress :: loadClient.toJson :: Nil)
