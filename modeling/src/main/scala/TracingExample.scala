c3d2b1a5d20e3fe8047ef8de3fe43b01f363e12c
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-11-05 17:18:41 -0700

    Create modeling subproject with initial tracing executor and example usage.

---
+++ modeling/src/main/scala/TracingExample.scala
@@ -0,0 +1,45 @@
+package edu.berkeley.cs
+package scads
+package piql
+package modeling
+
+import comm._
+import storage._
+import piql._
+import avro.runtime._
+
+import net.lag.logging.Logger
+
+import java.io.File
+
+object TracingExample {
+
+  val logger = Logger()
+  def main(args: Array[String]): Unit = {
+    val cluster = TestScalaEngine.getTestCluster
+    val executor = new ParallelExecutor with TracingExecutor {
+      val traceFile = new File("piqltrace.avro")
+    }
+    val client = new ScadrClient(cluster, executor, maxSubscriptions=10)
+
+    /* Sample data */
+    client.users.put(UserKey("marmbrus"), UserValue("Berkeley"))
+    client.users.put(UserKey("kcurtis"), UserValue("Berkeley"))
+    client.subscriptions.put(SubscriptionKey("marmbrus", "kcurtis"), SubscriptionValue(approved=true))
+    client.subscriptions.put(SubscriptionKey("kcurtis", "marmbrus"), SubscriptionValue(approved=true))
+    client.thoughts.put(ThoughtKey("marmbrus", 1), ThoughtValue("Soon there will be new syntax that joins the key and the value into one class"))
+  
+    /* Run some queries */
+    logger.info("Running thoughtstream query 10 times")
+    (1 to 10).foreach(i => client.thoughtstream("kcurtis", 10))
+
+    Thread.sleep(1000)
+
+    //Flush trace messages to the file
+    executor.flush()
+
+    val inFile = AvroInFile[QueryExecutionTrace](executor.traceFile)
+    inFile.foreach(println)
+    System.exit(0)
+  }
+}
---
+++ modeling/src/main/scala/TracingExecutor.scala
@@ -0,0 +1,85 @@
+package edu.berkeley.cs
+package scads
+package piql
+
+import avro.runtime._
+import avro.marker._
+import org.apache.avro.file.CodecFactory
+
+import java.io.File
+import java.util.concurrent.{ArrayBlockingQueue, TimeUnit}
+
+
+case class QueryExecutionTrace(var timestamp: Long, var threadName: String, var iteratorName: String, var planId: Int, var operation: String) extends AvroRecord
+
+abstract trait TracingExecutor extends QueryExecutor {
+  val traceFile: File
+  lazy val outputFile = AvroOutFile[QueryExecutionTrace](traceFile, CodecFactory.deflateCodec(5))
+
+  /* A list of messages that will be written to disk async by a seperate thread */
+  val pendingTraceMessages = new ArrayBlockingQueue[QueryExecutionTrace](1024)
+  val ioThread = new Thread("QueryTraceWriter") {
+    override def run(): Unit = {
+      while(true) {
+        outputFile.append(pendingTraceMessages.take)
+      }
+    }
+  }
+  ioThread.start
+
+  def flush(): Unit = {
+    while(pendingTraceMessages.size > 0) {
+      logger.info("Waiting for %d pendingTraceMessages to be written.", pendingTraceMessages.size)
+      Thread.sleep(100)
+    }
+    outputFile.flush
+  }
+
+  abstract override def apply(plan: QueryPlan)(implicit ctx: Context): QueryIterator = {
+    new TracingIterator(super.apply(plan), plan.hashCode)
+  }
+
+
+  class TracingIterator(child: QueryIterator, planId: Int) extends QueryIterator {
+    val name = "TracingIterator"
+
+    /* Place a trace message on the queue of messages to be written to disk.  If space isn't available issue a warning */
+    protected def recordTrace(operation: String): Boolean = {
+      val msg = QueryExecutionTrace(
+        System.nanoTime,
+        Thread.currentThread.getName,
+        child.name,
+        planId,
+        operation)
+
+      val success = pendingTraceMessages.offer(msg, 100, TimeUnit.MILLISECONDS)
+      if(!success)
+        logger.warning("Failed to record trace message: %s", msg)
+      success
+    }
+
+    def open = {
+      recordTrace("open")
+      child.open
+    }
+
+    def close = {
+      recordTrace("close")
+      child.close
+    }
+
+    def hasNext = {
+      recordTrace("hasNext")
+      val childHasNext = child.hasNext
+      logger.debug("%s hasNext: %b", child.name ,childHasNext)
+      childHasNext
+    }
+
+    def next = {
+      recordTrace("next")
+      val nextValue = child.next
+      logger.ifDebug {child.name + " next: " + nextValue.toList}
+      nextValue
+    }
+  }
+}
