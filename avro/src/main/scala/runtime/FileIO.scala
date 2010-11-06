d13e0a6fe5497dd9b16d9645d9948c8d633c72f2
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-11-05 17:17:59 -0700

    add compression codec option to AvroOutFile.

--- avro/src/main/scala/runtime/FileIO.scala
+++ avro/src/main/scala/runtime/FileIO.scala
@@ -3,7 +3,7 @@ package runtime
 
 import java.io.File
 import org.apache.avro.specific.{SpecificDatumReader, SpecificDatumWriter}
-import org.apache.avro.file.{DataFileReader, DataFileWriter}
+import org.apache.avro.file.{DataFileReader, DataFileWriter, CodecFactory}
 
 object AvroInFile {
   def apply[RecordType <: ScalaSpecificRecord](file: File)(implicit schema: TypedSchema[RecordType]): DataFileReader[RecordType] with Iterator[RecordType] = {
@@ -12,8 +12,9 @@ object AvroInFile {
 }
 
 object AvroOutFile {
-  def apply[RecordType <: ScalaSpecificRecord](file: File)(implicit schema: TypedSchema[RecordType]): DataFileWriter[RecordType] = {
+  def apply[RecordType <: ScalaSpecificRecord](file: File, codec: Option[CodecFactory] = None)(implicit schema: TypedSchema[RecordType]): DataFileWriter[RecordType] = {
     val writer = new DataFileWriter(new SpecificDatumWriter[RecordType](schema))
+    codec.foreach(writer.setCodec)
     writer.create(schema, file)
     return writer
   }
