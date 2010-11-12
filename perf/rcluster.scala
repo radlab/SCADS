26558adaaa16e7a3c8576a51d737827210699c5d
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-11-12 13:58:21 -0800

    Use only zoo1 until all three of them are up regularly.

--- perf/rcluster.scala
+++ perf/rcluster.scala
@@ -1,3 +1,3 @@
 implicit val scheduler = LocalExperimentScheduler(System.getProperty("user.name") + " console", "1@mesos-master.millennium.berkeley.edu:5050", "/work/deploylib/java_executor")
 implicit def classpath = Deploy.workClasspath
-implicit val ec2zoo = RClusterZoo.root
+implicit val zookeeper = ZooKeeperNode("zk://zoo1.millennium.berkeley.edu/")
