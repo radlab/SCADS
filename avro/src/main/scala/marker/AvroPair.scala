82b36b8cd451bf1c107e298e7f6925efb30c5e29
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-11-11 18:20:53 -0800

    Revert "Provide stub implementations of key/valueImpl for Avro pair to get around a bug in ensime."
    This reverts commit 121d3e35e0c76055fdbf6c30ac2ce7af01384909.

--- avro/src/main/scala/marker/AvroPair.scala
+++ avro/src/main/scala/marker/AvroPair.scala
@@ -1,12 +1,11 @@
 package edu.berkeley.cs.avro
 package marker
 
-import java.lang.RuntimeException
 import org.apache.avro.generic.GenericRecord
 
 trait AvroPair extends AvroRecord {
-  protected def keyImpl: GenericRecord = throw new RuntimeException("Unimplemented")
-  protected def valueImpl: GenericRecord = throw new RuntimeException("Unimplemented")
+  protected def keyImpl: GenericRecord
+  protected def valueImpl: GenericRecord
   lazy val key: GenericRecord = keyImpl
   lazy val value: GenericRecord = valueImpl
 }
