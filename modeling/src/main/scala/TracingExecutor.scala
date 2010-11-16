fedd774b22cc924f7f968584f1680c9a247e9ce9
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-11-15 16:43:13 -0800

    Draft of adding QueryStart/QueryEnd messages to tracefile.

--- modeling/src/main/scala/TracingExecutor.scala
+++ modeling/src/main/scala/TracingExecutor.scala
@@ -8,15 +8,17 @@ import avro.marker._
 import java.io.File
 import java.util.concurrent.{ArrayBlockingQueue, TimeUnit}
 
-
-case class QueryExecutionTrace(var timestamp: Long, var threadName: String, var iteratorName: String, var planId: Int, var operation: String) extends AvroRecord
+sealed trait TracingMessage extends AvroUnion
+case class QueryExecutionTrace(var timestamp: Long, var threadName: String, var iteratorName: String, var planId: Int, var operation: String) extends AvroRecord with TracingMessage
+case class QueryStart(var name: String) extends TracingMessage
+case class QueryEnd(var name: String)
 
 abstract trait TracingExecutor extends QueryExecutor {
   val traceFile: File
-  lazy protected val outputFile = AvroOutFile[QueryExecutionTrace](traceFile, CodecFactory.deflateCodec(5))
+  lazy protected val outputFile = AvroOutFile[TracingMessage](traceFile, CodecFactory.deflateCodec(5))
 
   /* A list of messages that will be written to disk async by a seperate thread */
-  protected val pendingTraceMessages = new ArrayBlockingQueue[QueryExecutionTrace](1024)
+  protected val pendingTraceMessages = new ArrayBlockingQueue[TracingMessage](1024)
   protected val ioThread = new Thread("QueryTraceWriter") {
     override def run(): Unit = {
       while(true) {
@@ -25,6 +27,13 @@ abstract trait TracingExecutor extends QueryExecutor {
     }
   }
   ioThread.start
+
+  def recordMessage(msg: TracingMessage): Unit = {
+    val success = pendingTraceMessages.offer(msg, 0, TimeUnit.MILLISECONDS)
+      if(!success)
+        logger.warning("Failed to record trace message: %s", msg)
+    success
+  }
 
   def flush(): Unit = {
     while(pendingTraceMessages.size > 0) {
@@ -38,23 +47,17 @@ abstract trait TracingExecutor extends QueryExecutor {
     new TracingIterator(super.apply(plan), plan.hashCode)
   }
 
-
   protected class TracingIterator(child: QueryIterator, planId: Int) extends QueryIterator {
     val name = "TracingIterator"
 
     /* Place a trace message on the queue of messages to be written to disk.  If space isn't available issue a warning */
     protected def recordTrace(operation: String): Boolean = {
-      val msg = QueryExecutionTrace(
+      recordMessage(QueryExecutionTrace(
         System.nanoTime,
         Thread.currentThread.getName,
         child.name,
         planId,
-        operation)
-
-      val success = pendingTraceMessages.offer(msg, 0, TimeUnit.MILLISECONDS)
-      if(!success)
-        logger.warning("Failed to record trace message: %s", msg)
-      success
+        operation))
     }
 
     def open = {
