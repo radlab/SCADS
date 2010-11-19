0fe8e24445e4fa30da29ae959e279ebc99be45ca
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-11-18 16:44:37 -0800

    Fix type bounds on Avro In/Out File.

--- avro/src/main/scala/runtime/FileIO.scala
+++ avro/src/main/scala/runtime/FileIO.scala
@@ -2,17 +2,18 @@ package edu.berkeley.cs.avro
 package runtime
 
 import java.io.File
+import org.apache.avro.generic.GenericContainer
 import org.apache.avro.specific.{SpecificDatumReader, SpecificDatumWriter}
 import org.apache.avro.file.{DataFileReader, DataFileWriter, CodecFactory}
 
 object AvroInFile {
-  def apply[RecordType <: ScalaSpecificRecord](file: File)(implicit schema: TypedSchema[RecordType]): DataFileReader[RecordType] with Iterator[RecordType] = {
+  def apply[RecordType <: GenericContainer](file: File)(implicit schema: TypedSchema[RecordType]): DataFileReader[RecordType] with Iterator[RecordType] = {
     new DataFileReader[RecordType](file, new SpecificDatumReader[RecordType](schema)) with Iterator[RecordType]
   }
 }
 
 object AvroOutFile {
-  def apply[RecordType <: ScalaSpecificRecord](file: File, codec: Option[CodecFactory] = None)(implicit schema: TypedSchema[RecordType]): DataFileWriter[RecordType] = {
+  def apply[RecordType <: GenericContainer](file: File, codec: Option[CodecFactory] = None)(implicit schema: TypedSchema[RecordType]): DataFileWriter[RecordType] = {
     val writer = new DataFileWriter(new SpecificDatumWriter[RecordType](schema))
     codec.foreach(writer.setCodec)
     writer.create(schema, file)
