04815f469836847c30e17c120652b5a5b5b46a5f
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-11-29 21:54:57 -0800

    Modified tracing example using PIQL language and optimizer.

--- modeling/src/main/scala/TracingExample.scala
+++ modeling/src/main/scala/TracingExample.scala
@@ -6,48 +6,73 @@ package modeling
 import comm._
 import storage._
 import piql._
+import avro.marker._
 import avro.runtime._
 
 import net.lag.logging.Logger
 
 import java.io.File
 
+case class R1(var f1: Int) extends AvroPair
+case class R2(var f1: Int, var f2: Int) extends AvroPair
+
 object TracingExample {
 
   val logger = Logger()
   def main(args: Array[String]): Unit = {
-    if(args.size != 1) {
-      println("Usage: <cluster address>")
-      System.exit(1)
+    /* Connect to scads cluster */
+    val cluster = args.size match {
+      case 0 => TestScalaEngine.newScadsCluster()
+      case 1 => new ScadsCluster(ZooKeeperNode(args(0)))
+      case _ => {
+	println("Usage: [cluster address]")
+	System.exit(1)
+	return
+      }
     }
 
-    val cluster = new ScadsCluster(ZooKeeperNode(args(0)))
+    /* get namespaces */
+    val r1 = cluster.getNamespace[R1]("r1")
+    val r2 = cluster.getNamespace[R2]("r2")
+
+    /* create executor that records trace to fileSink */
     val fileSink = new FileTraceSink(new File("piqltrace.avro"))
-    val executor = new ParallelExecutor with TracingExecutor {
+    implicit val executor = new ParallelExecutor with TracingExecutor {
       val sink = fileSink
     }
 
+    /* Register a listener that will record all messages sent/recv to fileSink */
     val messageTracer = new MessagePassingTracer(fileSink)
     MessageHandler.registerListener(messageTracer)
 
-    val client = new ScadrClient(cluster, executor, maxSubscriptions=10)
+    /* Bulk load some test data into the namespaces */
+    r1 ++= (1 to 10).view.map(i => R1(i))
+    r2 ++= (1 to 10).view.flatMap(i => (1 to 10).map(j => R2(i,j)))
 
-    /* Sample data */
-    client.users.put(UserKey("marmbrus"), UserValue("Berkeley"))
-    client.users.put(UserKey("kcurtis"), UserValue("Berkeley"))
-    client.subscriptions.put(SubscriptionKey("marmbrus", "kcurtis"), SubscriptionValue(approved=true))
-    client.subscriptions.put(SubscriptionKey("kcurtis", "marmbrus"), SubscriptionValue(approved=true))
-    client.thoughts.put(ThoughtKey("marmbrus", 1), ThoughtValue("Soon there will be new syntax that joins the key and the value into one class"))
+    /**
+     * Write queries against relations and create optimized function using .toPiql
+     * toPiql uses implicit executor defined above to run queries
+     */
+    val getQuery = r1.where("f1".a === 0).toPiql
+    val getRangeQuery = r2.where("f1".a === 0)
+			  .limit(10).toPiql
+
   
     /* Run some queries */
-    fileSink.recordEvent(QueryEvent("thoughtstream", "start"))
-    client.thoughtstream("kcurtis", 10)
-    fileSink.recordEvent(QueryEvent("thoughtstream", "end"))
+    (1 to 10).foreach(i => {
+      fileSink.recordEvent(QueryEvent("getQuery" + i, "start"))
+      getQuery(Nil)
+      fileSink.recordEvent(QueryEvent("getQuery" + i, "end"))
 
+      fileSink.recordEvent(QueryEvent("getRangeQuery" + i, "start"))
+      getRangeQuery(Nil)
+      fileSink.recordEvent(QueryEvent("getRangeQuery" + i, "end"))
+    })
     
     //Flush trace messages to the file
     fileSink.flush()
 
+    /* Read back in trace file */
     val inFile = AvroInFile[ExecutionTrace](fileSink.traceFile)
     inFile.foreach(println)
     System.exit(0)}
