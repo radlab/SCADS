582c3fe9dd89f373b5ea308a04eb02b67be40907
Michael Armbrust <marmbrus@cs.berkeley.edu>
2010-12-01 22:48:08 -0800

    bring back hand coded plans for scadr client.

--- piql/scadr/src/main/scala/ScadrClient.scala
+++ piql/scadr/src/main/scala/ScadrClient.scala
@@ -3,7 +3,7 @@ package scads
 package piql
 package scadr
 
-import storage._
+import storage.ScadsCluster
 import avro.marker._
 
 import org.apache.avro.util._
@@ -15,7 +15,6 @@ case class User(var username: String) extends AvroPair {
 case class Thought(var owner: String, var timestamp: Int) extends AvroPair {
   var text: String = _
 }
-
 
 case class Subscription(var owner: String, var target: String) extends AvroPair {
   var approved: Boolean = _
@@ -31,10 +30,78 @@ class ScadrClient(val cluster: ScadsCluster, executor: QueryExecutor, maxSubscri
   // createNamespace calls to happen first (and after instantiating this
   // class)
 
-  lazy val users = cluster.getNamespace[User]("users")
-  lazy val thoughts = cluster.getNamespace[Thought]("thoughts")
-  lazy val subscriptions = cluster.getNamespace[Subscription]("subscriptions")
-  lazy val tags = cluster.getNamespace[HashTag]("tags")
+  //HACK: typecast to namespace
+  lazy val users = cluster.getNamespace[User]("users").asInstanceOf[Namespace]
+  lazy val thoughts = cluster.getNamespace[Thought]("thoughts").asInstanceOf[Namespace]
+  lazy val subscriptions = cluster.getNamespace[Subscription]("subscriptions").asInstanceOf[Namespace]
+  lazy val tags = cluster.getNamespace[HashTag]("tags").asInstanceOf[Namespace]
 
+  /* Optimized queries */
   val findUser = users.where("username".a === (0.?)).toPiql
+
+  /* Old hand coded plans */
+  type QueryArgs = Seq[Any]
+
+  private lazy val myThoughtsPlan =
+    LocalStopAfter(ParameterLimit(1, maxResultsPerPage),
+      IndexScan(thoughts, Array(ParameterValue(0)), ParameterLimit(1, maxResultsPerPage), false))
+  val myThoughts = (args: QueryArgs) => exec(myThoughtsPlan, args)
+
+  private lazy val usersFollowedByPlan =
+    IndexLookupJoin(users, Array(AttributeValue(0, 1)),
+      IndexScan(subscriptions, Array(ParameterValue(0)), ParameterLimit(1, maxResultsPerPage), true))
+
+  private lazy val usersFollowedByStopAfterPlan =
+    LocalStopAfter(ParameterLimit(1, maxResultsPerPage), usersFollowedByPlan)
+
+  /**
+   * Who am I following?
+   */
+  def usersFollowedBy = (args: QueryArgs) => exec(usersFollowedByStopAfterPlan, args)
+
+  def usersFollowedByPaginate(username: String, count: Int): PageResult = {
+    val iterator = executor(usersFollowedByPlan, new Utf8(username), count)
+    val res = new PageResult(iterator, count)
+    res.open
+    res
+  }
+
+  private lazy val usersFollowingPlan =
+    LocalStopAfter(ParameterLimit(1, maxResultsPerPage),
+      IndexLookupJoin(users, Array(AttributeValue(0, 1)),
+        IndexScan(users, Array(ParameterValue(0)), ParameterLimit(1, maxResultsPerPage), true)))
+
+  /**
+   * Who is following ME?
+   */
+  def usersFollowing = (args: QueryArgs) => exec(usersFollowingPlan, args)
+
+  private lazy val thoughtStreamPlan = // (sub_owner, sub_target), (sub_approved), (thought_owner, thought_timestamp), (thought_text)
+    IndexMergeJoin(thoughts, Array(AttributeValue(0, 1)), Array(AttributeValue(2, 1)), ParameterLimit(1, maxResultsPerPage), false,
+      LocalSelection(EqualityPredicate(ConstantValue(true), AttributeValue(1, 0)), // (owner, target), (approved)
+        IndexScan(subscriptions, Array(ParameterValue(0)), FixedLimit(maxSubscriptions), true) // (owner, target), (approved)
+))
+
+  private lazy val thoughtStreamStopAfterPlan =
+    LocalStopAfter(ParameterLimit(1, maxResultsPerPage), thoughtStreamPlan)
+
+  def thoughtstream = (args: QueryArgs) => exec(thoughtStreamStopAfterPlan, args)
+
+  def thoughtstreamPaginate(username: String, count: Int): PageResult = {
+    val iterator = executor(thoughtStreamPlan, new Utf8(username), count)
+    val res = new PageResult(iterator, count)
+    res.open
+    res
+  }
+
+  /* SELECT thoughts.*
+     FROM thoughts
+       JOIN tags ON thoughts.owner = tags.owner AND thoughts.timestamp = tags.timestamp
+     WHERE tags.tag = [1: tag]
+     ORDER BY timestamp DESC */
+  private lazy val thoughsByHashTagPlan =
+    LocalStopAfter(ParameterLimit(1, maxResultsPerPage),
+      IndexLookupJoin(thoughts, Array(AttributeValue(0, 2), AttributeValue(0, 1)),
+        IndexScan(tags, Array(ParameterValue(0)), ParameterLimit(1, maxResultsPerPage), false)))
+  def thoughtsByHashTag = (args: QueryArgs) => exec(thoughsByHashTagPlan, args)
 }
