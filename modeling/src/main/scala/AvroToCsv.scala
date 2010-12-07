29474d863968d201e21119f65f8baf74b0f53bec
ksauer <ksauer@w8722fd0x91.(none)>
2010-12-06 19:05:13 -0800

    changed avro to csv conversion

--- modeling/src/main/scala/AvroToCsv.scala
+++ modeling/src/main/scala/AvroToCsv.scala
@@ -27,7 +27,7 @@ object AvroToCsv {
     println("timestamp, thread, operationLevel, operationType, iteratorFunctionCall, start")
     inFile.map {
       case ExecutionTrace(timestamp, threadId, QueryEvent(queryName, start)) => List(timestamp, threadId, "query", queryName, 0, start).mkString(",")
-      case ExecutionTrace(timestamp, threadId, IteratorEvent(iteratorName, plan, op, start)) => List(timestamp, threadId, "iterator", iteratorName, op, start).mkString(",")
+      case ExecutionTrace(timestamp, threadId, IteratorEvent(iteratorName, plan, op, start)) => List(timestamp, threadId, "iterator", iteratorName + ":" + plan, op, start).mkString(",")
       case ExecutionTrace(timestamp, threadId, MessageEvent(msg)) => List(timestamp, threadId, "message", msg.body.getSchema.getName, 0, 0).mkString(",")
     }.foreach(println)
 
