0f546780e4ed866d730c4707a2857e51b83520ea
Stephen Tu <stephen_tu@berkeley.edu>
2011-01-04 14:12:43 -0800

    IntKeyScaleTest now does puts in addition to gets

--- perf/src/main/scala/Experiment.scala
+++ perf/src/main/scala/Experiment.scala
@@ -49,3 +49,9 @@ abstract trait Experiment {
     scheduler.scheduleExperiment(clientJvmProcess(clientDesc, cluster.root) * clientDesc.numClients)
   }
 }
+
+trait ExperimentMain {
+  implicit val scheduler = LocalExperimentScheduler(System.getProperty("user.name") + " console", "1@mesos-master.millennium.berkeley.edu:5050", "/work/deploylib/java_executor")
+  implicit def classpath = Deploy.workClasspath
+  implicit val zookeeper = ZooKeeperNode("zk://zoo1.millennium.berkeley.edu/")
+}
