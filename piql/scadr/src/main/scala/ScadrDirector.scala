64f1777bbfb045e5d5defd92128e1ed393d23ba1
Beth Trushkowsky <trush@eecs.berkeley.edu>
2011-01-08 18:31:02 -0800

    namespaces are lowercase

--- piql/scadr/src/main/scala/ScadrDirector.scala
+++ piql/scadr/src/main/scala/ScadrDirector.scala
@@ -5,15 +5,18 @@ import net.lag.logging.Logger
 
 object ScadrDirectorTest {
   org.apache.log4j.Logger.getRootLogger.setLevel(org.apache.log4j.Level.WARN)
-  val partitionsPerNamespace = 1
   var sched:ScadsServerScheduler = null
 
+  /**
+  * Start a cluster with one storage engine per namespace using mesos slaves
+  * creates the scadr namespaces
+  */
   def init(zoopath:String, mesospath:String):Seq[GenericNamespace] = {
     // connect to mesos and zookeeper
   	val node = ZooKeeperNode(zoopath)
   	ScadsServerScheduler
   	sched = ScadsServerScheduler("director",mesospath,node.canonicalAddress)
-  	sched.addServers( List("User","Thought","Subscription").map(ns => ns+"node0") )
+  	sched.addServers( List("user","thought","subscription").map(ns => ns+"node0") )
   	
   	// wait until have enough servers for one per namespace
 		Thread.sleep(10*1000)
@@ -23,23 +26,30 @@ object ScadrDirectorTest {
 			Director.cluster = new ScadsCluster(node)
 		}
 
-		List( ("User",classOf[edu.berkeley.cs.scads.piql.scadr.User].newInstance),("Thought",classOf[edu.berkeley.cs.scads.piql.scadr.Thought].newInstance),("Subscription",classOf[edu.berkeley.cs.scads.piql.scadr.Subscription].newInstance) ).map(entry => {
+		List( ("user",classOf[edu.berkeley.cs.scads.piql.scadr.User].newInstance),("thought",classOf[edu.berkeley.cs.scads.piql.scadr.Thought].newInstance),("subscription",classOf[edu.berkeley.cs.scads.piql.scadr.Subscription].newInstance) ).map(entry => {
 		  Director.cluster.createNamespace(entry._1, entry._2.key.getSchema, entry._2.value.getSchema, List((None,Director.cluster.getAvailableServers(entry._1))))
 		})
 		
   }
   
+  /**
+  * Start a local test cluster with one storage engine per namespace
+  * creates the scadr namespaces
+  */
   def initTest():Seq[GenericNamespace] = {
     	Director.cluster = new ManagedScadsCluster(TestScalaEngine.newScadsCluster(0).root)
-  		List("User","Thought","Subscription").foreach(ns => Director.cluster match {case m:ManagedScadsCluster => m.addNamedNode(ns+"first") })
+  		List("user","thought","subscription").foreach(ns => Director.cluster match {case m:ManagedScadsCluster => m.addNamedNode(ns+"first") })
   		
-  		List( ("User",classOf[edu.berkeley.cs.scads.piql.scadr.User].newInstance),("Thought",classOf[edu.berkeley.cs.scads.piql.scadr.Thought].newInstance),("Subscription",classOf[edu.berkeley.cs.scads.piql.scadr.Subscription].newInstance) ).map(entry => {
+  		List( ("user",classOf[edu.berkeley.cs.scads.piql.scadr.User].newInstance),("thought",classOf[edu.berkeley.cs.scads.piql.scadr.Thought].newInstance),("subscription",classOf[edu.berkeley.cs.scads.piql.scadr.Subscription].newInstance) ).map(entry => {
   		  Director.cluster.createNamespace(entry._1, entry._2.key.getSchema, entry._2.value.getSchema, List((None,Director.cluster.getAvailableServers(entry._1))))
   		})
   }
   
+  /**
+  * given the namespaces and zookeeper path, create and start a director per namespace
+  */
   def start(namespaceStrings:Seq[String], zoopath:String):Seq[Director] = {
-    System.setProperty("doEmpty","true")
+    System.setProperty("doEmpty","true") // run the empty policy, which does nothing but observe workload stats
 		val directors = namespaceStrings.map(ns => Director(1,ns,sched))
 		directors.foreach(_.run( ZooKeeperNode(zoopath) ))
 		directors
