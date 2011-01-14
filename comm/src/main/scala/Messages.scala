3bdea2c100b3144f058b37fcd147f4fcea5dbf61
ksauer <ksauer@w8722fd0x91.(none)>
2011-01-14 14:11:58 -0800

    added message types for modeling

--- comm/src/main/scala/Messages.scala
+++ comm/src/main/scala/Messages.scala
@@ -106,3 +106,9 @@ sealed trait TraceEvent extends AvroUnion
 case class QueryEvent(var queryName: String, var start: Boolean) extends AvroRecord with TraceEvent
 case class IteratorEvent(var iteratorName: String, var planId: Int, var operation: String, var start: Boolean) extends AvroRecord with TraceEvent
 case class MessageEvent(var message: Message) extends AvroRecord with TraceEvent
+
+// messages for varying query params - used during data collection to facilitate log parsing
+case class ChangeRangeLengthEvent(var numDataItems: Int) extends AvroRecord with TraceEvent
+case class WarmupEvent(var warmupLengthInMinutes: Int, var start: Boolean) extends AvroRecord with TraceEvent	// to avoid JIT effects on latency measurements
+
+
