9cd721a98ddbf858af5ece15d885f11ebff884b9
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-01-14 01:15:21 -0800

    Clean up the ServiceScheduler meta-framework naming

--- deploylib/src/main/scala/mesos/ExperimentDaemon.scala
+++ deploylib/src/main/scala/mesos/ExperimentDaemon.scala
@@ -5,7 +5,7 @@ import edu.berkeley.cs.scads.comm._
 
 import net.lag.logging.Logger
 
-class RemoteExperimentScheduler extends ExperimentScheduler with MessageReceiver {
+class RemoteServiceScheduler extends ExperimentScheduler with MessageReceiver {
   val logger = Logger()
   val remoteService = RemoteActor("mesos-ec2", 9001, ActorNumber(0))
   implicit val returnAddress = MessageHandler.registerService(this)
@@ -19,21 +19,11 @@ class RemoteExperimentScheduler extends ExperimentScheduler with MessageReceiver
   }
 }
 
-class ExperimentService(mesosMaster: String, executor: String) extends LocalExperimentScheduler("ExperimentDaemon", mesosMaster, executor) with ServiceHandler[ExperimentOperation] {
-  def startup: Unit = {
-    RClusterZoo.root.getOrCreate("scads/experimentService").data = remoteHandle.toBytes
-  }
-
+class ServiceScheduler(mesosMaster: String, executor: String) extends LocalExperimentScheduler("ExperimentDaemon", mesosMaster, executor) with ServiceHandler[ExperimentOperation] {
+  def startup: Unit = null
   def shutdown: Unit = null
 
   def process(src: Option[RemoteActorProxy], msg: ExperimentOperation) = msg match {
     case RunExperiment(processes) => scheduleExperiment(processes)
-  }
-}
-
-object ExperimentDaemon extends optional.Application {
-  def main(mesosMaster: Option[String]): Unit = {
-    System.loadLibrary("mesos")
-    new ExperimentService(mesosMaster.getOrElse("1@" + java.net.InetAddress.getLocalHost.getHostName + ":5050"), "/usr/local/mesos/frameworks/deploylib/java_executor")
   }
 }
