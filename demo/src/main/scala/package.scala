58afe0a70e15b4959bfe19aa666785e29bbf94d6
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-01-14 16:38:26 -0800

    add script for making your master the primary one for the radlab demo.

--- demo/src/main/scala/package.scala
+++ demo/src/main/scala/package.scala
@@ -6,11 +6,17 @@ package object demo {
   import scads.comm._
   import deploylib.mesos._
 
-  def fixServiceSchedulerPid: Unit = {
-    DemoConfig.serviceSchedulerNode.data = RemoteActor(MesosEC2.master.publicDnsName, 9000, ActorNumber(0)).toBytes
-  }
+  def setupMesosMaster: Unit = {
+    try MesosEC2.master catch {
+      case _ => MesosEC2.startMaster()
+    }
 
-  def setMesosMaster: Unit = {
+    MesosEC2.master.pushJars
+    MesosEC2.master.executeCommand("killall java")
+    MesosEC2.master.createFile(new java.io.File("/root/serviceScheduler"), "#!/bin/bash\n/root/jrun edu.berkeley.cs.radlab.demo.ServiceSchedulerDaemon >> /root/serviceScheduler.log 2>&1")
+    MesosEC2.master ! "chmod 755 /root/serviceScheduler"
+    MesosEC2.master ! "start-stop-daemon --make-pidfile --start --background --pidfile /var/run/serviceScheduler.pid --exec /root/serviceScheduler"
+    DemoConfig.serviceSchedulerNode.data = RemoteActor(MesosEC2.master.publicDnsName, 9000, ActorNumber(0)).toBytes
     DemoZooKeeper.root.getOrCreate("demo/mesosMaster").data = MesosEC2.clusterUrl.getBytes
   }
 
