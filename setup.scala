3f29894bf6a517990ee6b53d80aac9de633a8b01
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-01-14 16:38:38 -0800

    add piql/gradit to setup for demo

--- setup.scala
+++ setup.scala
@@ -1,4 +1,6 @@
 import edu.berkeley.cs.radlab.demo._
 import edu.berkeley.cs.scads.storage._
 import edu.berkeley.cs.scads.comm._
+import edu.berkeley.cs.scads.piql._
+import edu.berkeley.cs.scads.piql.gradit._
 import deploylib.mesos._
