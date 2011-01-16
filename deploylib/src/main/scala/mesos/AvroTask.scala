0342ce6022ea18729f604c5a60b70615d45630fa
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-01-15 17:37:56 -0800

    Use explicit specific decoder when decoding avrotask.

--- deploylib/src/main/scala/mesos/AvroTask.scala
+++ deploylib/src/main/scala/mesos/AvroTask.scala
@@ -3,7 +3,10 @@ package mesos
 
 import edu.berkeley.cs.scads.comm._
 import edu.berkeley.cs.avro.runtime._
+import org.apache.avro.io.JsonDecoder
+import org.apache.avro.specific.SpecificDatumReader
 import org.apache.avro.generic.IndexedRecord
+
 import net.lag.logging.Logger
 
 abstract trait AvroTask extends IndexedRecord {
@@ -21,23 +24,26 @@ abstract trait AvroTask extends IndexedRecord {
       this.getClass.getName :: this.toJson :: Nil)
 }
 
-
 object AvroTaskMain {
   val logger = Logger()
 
   def main(args: Array[String]): Unit = {
-    if(args.size == 2)
+    if (args.size == 2)
       try {
-        Class.forName(args(0)).newInstance.asInstanceOf[AvroTask].parse(args(1)).run()
-	logger.info("Run method returned, terminating AvroClient")
-	System.exit(0)
-      }
-      catch {
+        val taskClass = Class.forName(args(0))
+        val task = taskClass.newInstance.asInstanceOf[AvroTask]
+        val reader = new SpecificDatumReader[AvroTask](task.getSchema)
+        val decoder = new JsonDecoder(task.getSchema, args(1))
+
+        reader.read(task, decoder).run()
+        logger.info("Run method returned, terminating AvroClient")
+        System.exit(0)
+      } catch {
         case error => {
           logger.fatal(error, "Exeception in Main Thread.  Killing process.")
           System.exit(-1)
+        }
       }
-    }
     else {
       println("Usage: " + this.getClass.getName + "<class name> <json encoded avro client description>")
       System.exit(-1)
