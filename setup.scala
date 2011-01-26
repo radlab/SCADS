bb69e135dd5b96a191c826b1c0605225c0495ff7
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-01-25 21:30:40 -0800

    add testScadrClient to demo setup script.

--- setup.scala
+++ setup.scala
@@ -3,6 +3,9 @@ import edu.berkeley.cs.scads.storage._
 import edu.berkeley.cs.scads.comm._
 import edu.berkeley.cs.scads.piql._
 import edu.berkeley.cs.scads.piql.gradit._
+import edu.berkeley.cs.scads.piql.scadr._
 import deploylib.mesos._
 
 lazy val testGraditClient = new GraditClient(TestScalaEngine.newScadsCluster(), new DashboardReportingExecutor())
+
+lazy val testScadrClient = new ScadrClient(TestScalaEngine.newScadsCluster(), new DashboardReportingExecutor())
