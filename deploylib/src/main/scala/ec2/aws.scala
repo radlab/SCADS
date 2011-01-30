53ec66cf4818fd9619fe9c846024999afe90f465
Andy Konwinski <andyk@berkeley.edu>
2011-01-30 00:47:32 -0800

    Adding bundleNewAMI() function allowing any running EC2Instance to be bunldled into a new AMI. Also, making more credential environement variables available.

--- deploylib/src/main/scala/ec2/aws.scala
+++ deploylib/src/main/scala/ec2/aws.scala
@@ -1,10 +1,15 @@
 package deploylib.ec2
 
+import java.io.File
+
 /**
  * Abstract class that is used to get the the accessKeyId and the secretAcccess key from the environmental variables <code>AWS_ACCESS_KEY_ID</code> and <code>AWS_SECRET_ACCESS_KEY</code> respectively.
  * It is used by the EC2Instance object and S3Cache.
  */
-abstract class AWSConnection {
+trait AWSConnection {
   protected val accessKeyId = System.getenv("AWS_ACCESS_KEY_ID")
   protected val secretAccessKey = System.getenv("AWS_SECRET_ACCESS_KEY")
+  protected val ec2PrivateKey = new File(System.getenv("EC2_PRIVATE_KEY"))
+  protected val ec2Cert = new File(System.getenv("EC2_CERT"))
+  protected val userID = new File(System.getenv("AWS_USER_ID"))
 }
