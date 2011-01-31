89cfafed854286eaa46585f350a82a0b93d6ad0e
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-01-30 22:48:30 -0800

    update test for optimizer

--- piql/src/main/scala/TestOptimizer.scala
+++ piql/src/main/scala/TestOptimizer.scala
@@ -21,14 +21,28 @@ object Relations {
 }
 
 object TestOptimizer {
-  import Relations._
-
   def main(args: Array[String]): Unit = {
-    implicit val executor = new SimpleExecutor
+    implicit val executor = new SimpleExecutor with DebugExecutor
+
+    val cluster = TestScalaEngine.newScadsCluster()
+    val r1 = cluster.getNamespace[R1]("r1")
+    val r2 = cluster.getNamespace[R2]("r2")
+    val r2Prime = cluster.getNamespace[R2]("r2Prime")
+
     r1 ++= (1 to 10).map(i => R1(i))
     val query = r1.where("f1".a === 1).toPiql()
+    println(query())
 
-    println(query(Nil))
+    for(i <- (1 to 10); j <- (1 to 10)) {
+      val r = R2(i,j)
+      r2.put(r.key, r.value)
+    }
+
+    println(r2.getRange(None,None))
+     val query2 = r2.where("f1".a === 1).limit(0.?, 10).toPiql()
+    println(query2(1))
+    println(query2(2))
+
 
     System.exit(0)
   }
