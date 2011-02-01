b959c0fc9349d318365007176a9c6b3c214a30d3
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-01-31 22:44:05 -0800

    import base berkeley cs package in setup.

--- setup.scala
+++ setup.scala
@@ -4,6 +4,7 @@ import edu.berkeley.cs.scads.comm._
 import edu.berkeley.cs.scads.piql._
 import edu.berkeley.cs.scads.piql.gradit._
 import edu.berkeley.cs.scads.piql.scadr._
+import edu.berkeley.cs._
 import deploylib.mesos._
 
 lazy val testGraditClient = new GraditClient(TestScalaEngine.newScadsCluster(), new DashboardReportingExecutor())
