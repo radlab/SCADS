e061d89e19aea7ece29940cc649dc4ef899ccfd0
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-01-31 22:43:06 -0800

    make files defs so that we don't break people who don't have the vars set.

--- deploylib/src/main/scala/ec2/aws.scala
+++ deploylib/src/main/scala/ec2/aws.scala
@@ -10,6 +10,6 @@ trait AWSConnection {
   protected val accessKeyId = System.getenv("AWS_ACCESS_KEY_ID")
   protected val secretAccessKey = System.getenv("AWS_SECRET_ACCESS_KEY")
   protected val ec2PrivateKey = new File(System.getenv("EC2_PRIVATE_KEY"))
-  protected val ec2Cert = new File(System.getenv("EC2_CERT"))
-  protected val userID = new File(System.getenv("AWS_USER_ID"))
+  protected def ec2Cert = new File(System.getenv("EC2_CERT"))
+  protected def userID = new File(System.getenv("AWS_USER_ID"))
 }
