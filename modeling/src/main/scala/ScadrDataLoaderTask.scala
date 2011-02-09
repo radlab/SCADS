cb96f4fd13c3aa10f4780e712cab88eb48c4c77d
ksauer <ksauer@w8722fd0x91.(none)>
2011-02-08 21:28:50 -0800

    added task for loading scadr data into scads

---
+++ modeling/src/main/scala/ScadrDataLoaderTask.scala
@@ -0,0 +1,61 @@
+package edu.berkeley.cs
+package scads
+package piql
+package modeling
+
+import deploylib.mesos._
+import comm._
+import storage._
+import piql._
+import perf._
+import piql.scadr._
+import avro.marker._
+import avro.runtime._
+
+import net.lag.logging.Logger
+import java.io.File
+import java.net._
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable._
+
+import com.amazonaws.services.sns._
+import com.amazonaws.auth._
+import com.amazonaws.services.sns.model._
+
+case class ScadrDataLoaderTask(
+  var params: RunParams
+) extends AvroTask with AvroRecord {
+  def run(): Unit = {
+    /* setting up cluster */
+    println("setting up cluster...")
+    val clusterRoot = ZooKeeperNode(params.clusterAddress)
+    val cluster = new ExperimentalScadsCluster(clusterRoot)
+    cluster.blockUntilReady(params.numStorageNodes)
+
+    /* load data */
+    println("loading data...")
+    val scadrClient = new ScadrClient(cluster, new SimpleExecutor)
+    //val scadrLoader = new ScadrLoader(scadrClient, 1, 1)
+    val scadrLoader = new ScadrLoader(
+      scadrClient, //val client: ScadrClient,
+      1, //val replicationFactor: Int,
+      1, //val numClients: Int, // number of clients to split the loading by
+      100000, //val numUsers: Int = 100,
+      100, //val numThoughtsPerUser: Int = 10,
+      100, //val numSubscriptionsPerUser: Int = 10,
+      5 //val numTagsPerThought: Int = 5)
+    )
+    scadrLoader.createNamespaces
+    val scadrData = scadrLoader.getData(0)  // check sizes
+    println("user data size: " + scadrData.userData.size)
+    println("thought data size: " + scadrData.thoughtData.size)
+    println("subscription data size: " + scadrData.subscriptionData.size)
+    scadrData.load
+    
+    println("users:  " + scadrClient.users.getRange(None, None).size)
+    println("thoughts:  " + scadrClient.thoughts.getRange(None, None).size)
+    println("subscriptions:  " + scadrClient.subscriptions.getRange(None, None).size)
+    println("done!")
+  }
+}
