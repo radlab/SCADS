c89a0177502f24597a43995c3df1502cd1ab4252
ksauer <ksauer@w8722fd0x91.(none)>
2011-02-10 17:54:45 -0800

    changed run params to use cluster params

--- modeling/src/main/scala/RunParams.scala
+++ modeling/src/main/scala/RunParams.scala
@@ -14,40 +14,26 @@ import avro.runtime._
 import net.lag.logging.Logger
 
 case class RunParams(
-	var clusterAddress: String, 
+  var clusterParams: ClusterParams,
 	var queryType: String,
-	var baseCardinality: Int, 
 	var warmupLengthInMinutes: Int = 5, 
-	var numStorageNodes: Int = 1, 
 	var numQueriesPerCardinality: Int = 1000, 
-	var sleepDurationInMs: Int = 100,
-	var dataLowerBound: Int = 10
+	var sleepDurationInMs: Int = 100
 ) extends AvroRecord {
-	def getNumDataItems: Int = {
-		getMaxCardinality*10
-	}
-	
-	def getMaxCardinality: Int = {
-		getCardinalityList.sortWith(_ > _).head
-	}
-	
-	def getCardinalityList: List[Int] = {
-		((baseCardinality*0.5).toInt :: (baseCardinality*0.75).toInt :: baseCardinality :: baseCardinality*2 :: baseCardinality*10 :: baseCardinality*100:: Nil)
-	}
-
   // should get rid of this
 	def getExperimentDescription: String = {
 		"experiment had the following params:\n" + toString
 	}
 	
+	// should really have toString method in ClusterParams
 	override def toString: String = {
 		List(
   		"experiment had the following params:",
-			"  clusterAddress: " + clusterAddress.toString,
+			"  clusterAddress: " + clusterParams.clusterAddress.toString,
 			"  queryType: " + queryType.toString,
-			"  baseCardinality: " + baseCardinality.toString,
+			"  baseCardinality: " + clusterParams.baseCardinality.toString,
 			"  warmupLengthInMinutes: " + warmupLengthInMinutes.toString,
-			"  numStorageNodes: " + numStorageNodes.toString,
+			"  numStorageNodes: " + clusterParams.numStorageNodes.toString,
 			"  numQueriesPerCardinality: " + numQueriesPerCardinality.toString,
 			"  sleepDurationInMs: " + sleepDurationInMs.toString
 		).mkString("\n")
