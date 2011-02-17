107c49efb300621d21c369d1d9ee99230246dbb3
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-02-17 13:39:01 -0800

    add operator for injecting local user tuples instead of going to the key value store.

---
+++ modeling/src/main/scala/LocalUserOperator.scala
@@ -0,0 +1,36 @@
+package edu.berkeley.cs
+package scads
+package piql
+package modeling
+
+import scadr._
+
+import collection.mutable.ArrayBuffer
+
+case class LocalUserOperator() extends QueryPlan
+
+class LocalUserExecutor extends ParallelExecutor {
+  override def apply(plan: QueryPlan)(implicit ctx: Context): QueryIterator = plan match {
+    case LocalUserOperator() => {
+      new QueryIterator {
+        val name = "LocalUserOperator"
+	private var iterator: Iterator[Tuple] = null
+
+	private def toUser(idx: Int) = ArrayBuffer(new User("User%010d".format(idx)))
+	private def randomUser(numUsers: Int) = toUser(scala.util.Random.nextInt(numUsers) + 1)
+
+        def open: Unit = {
+	  val numTuples = ctx.parameters(0).asInstanceOf[Int]
+	  val maxUserId = ctx.parameters(1).asInstanceOf[Int]
+	  iterator = Array.fill(numTuples)(randomUser(maxUserId)).iterator
+        }
+        def close: Unit =
+          iterator = null
+
+        def hasNext = iterator.hasNext
+        def next = iterator.next
+      }
+    }
+    case otherPlan => super.apply(otherPlan)
+  }
+}
