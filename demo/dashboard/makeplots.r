0c00c10711f4d2ab059e3ee9a43cc425231ed63b
Ariel Rabkin <asrabkin@Constellation.wan>
2011-02-21 22:36:56 -0800

    break up SCADS by app

--- demo/dashboard/makeplots.r
+++ demo/dashboard/makeplots.r
@@ -14,9 +14,9 @@ con <- dbConnect(MySQL(), dbname = "radlabmetrics" , user="radlab_dev", password
 TZShift = 8*3600	#PST = GMT - 8 hours
 PERIODS = c(10, 60, 300)	#minutes
 WEBAPP_STATS = FALSE
-SCADS_STATS = FALSE
+SCADS_STATS = TRUE
 PIQL_STATS = FALSE
-RAIN_STATS = TRUE
+RAIN_STATS = FALSE
 
 mostRecentPt <- function(tableName) {
 	val <- dbGetQuery(con,paste("select timestamp from ",tableName," order by timestamp DESC limit 1", sep=''))
@@ -32,6 +32,9 @@ if(RAIN_STATS) {
 	for(period in PERIODS) {
 		startT <- (mostRecent - 1000* 60 * period -1) 
 		tracknamesL <- dbGetQuery(con, paste("select DISTINCT trackName from rainStats where timestamp > ",startT," order by trackName",sep=''))
+		
+		if (nrow(tracknamesL) > 0)  {
+			
 		tracknames = tracknamesL[[1]]
 
 		appNum = 0
@@ -39,6 +42,7 @@ if(RAIN_STATS) {
 		png(file=reqRatePlotFile, bg="transparent")
 		allTimesRaw = dbGetQuery(con, paste("select DISTINCT timestamp from rainStats where timestamp > ",startT," order by timestamp",sep=''))
 		allTimesPosix = as.POSIXct( ( allTimesRaw[TRUE,1]/1000 - TZShift), origin="1970-01-01")
+		
 		maxYTuple = dbGetQuery(con, paste("select totalResponseTime,operationsSuccessful from rainStats where timestamp > ",startT," order by totalResponseTime/operationsSuccessful DESC limit 1",sep=''))
 		maxY = maxYTuple[[1]]/maxYTuple[[2]]
 		yrange <- c(0,maxY * 1.2) 
@@ -62,7 +66,7 @@ if(RAIN_STATS) {
 
 		legend( x="topleft", inset=0.05, as.character(tracknames), cex=1.2, col=1:appNum, bg="white", pch=21:22, lty=1:2)
 		dev.off()
-		
+		}
 	}
 }
 
@@ -128,42 +132,36 @@ if(WEBAPP_STATS) {
 		}
 
 		
-		# plot avg CPU utilization for SCADr
-		cpuSeries = dbGetQuery(con,paste("select timestamp,averageUtilization from appReqRate where webAppID = 'SCADr' and timestamp > ",startT," order by timestamp DESC ",sep=''))
-		if (nrow(cpuSeries) > 0) {
-			cpuPlotFile=paste("scadrAverageCpuUtilization-",period,".png",sep='')
-			png(file= cpuPlotFile, bg="transparent")
-		
-			times <- as.POSIXct( (cpuSeries[TRUE,1]/1000 - TZShift), origin="1970-01-01")
-			par(oma=c(1,1,1,2))
+		# plot avg CPU utilizations
+		allTimesRaw = dbGetQuery(con, paste("select DISTINCT timestamp from appReqRate where timestamp > ",startT," order by timestamp",sep=''))
+		allTimesPosix = as.POSIXct( ( allTimesRaw[TRUE,1]/1000 - TZShift), origin="1970-01-01")
+
+		maxYTuple = dbGetQuery(con, paste("select averageUtilization from appReqRate where timestamp > ",startT," order by averageUtilization DESC limit 1",sep=''))
+		yrange <- c(0,maxYTuple[[1]] * 1.2) 
+
+		appIDs = c("SCADr","gRADit","comRADes")
+		cpuPlotFile=paste("averageCpuUtilization-",period,".png",sep='')
+		png(file= cpuPlotFile, bg="transparent")
+		appNum = 0
+		for(appID in appIDs) {		
+			appNum = appNum + 1
+			cpuSeries = dbGetQuery(con,paste("select timestamp,averageUtilization from appReqRate where webAppID = '",appID,"' and timestamp > ",startT," order by timestamp DESC ",sep=''))
+			if (nrow(cpuSeries) > 0) {
 			
-			yrange = c(0, 1.2*max(cpuSeries[TRUE,2], na.rm=TRUE))
-			plot(times, cpuSeries[TRUE,2], xlab="", ylab="Average CPU utilization", col="red", xaxt="n", type="o", main="",cex.lab=1.2, ylim=yrange)
+				times <- as.POSIXct( (cpuSeries[TRUE,1]/1000 - TZShift), origin="1970-01-01")
+				par(oma=c(1,1,1,2))
+				par(new=T)
+				plot(times, cpuSeries[TRUE,2], xlab="", ylab="", col=appNum, xaxt="n", type="o", main="",cex.lab=1.2, ylim=yrange)
 			
-			axis.POSIXct(1, times, format="%Y-%m-%d %H:%M:%S", labels = TRUE)
-			mtext("time", side=1,line=3,cex=1.2)
-			mtext("Web server average CPU allocation for SCADr",side=3,cex=1.4,line=2)
 		}
-
-
-		# plot avg CPU utilization for gRADit
-		cpuSeries = dbGetQuery(con,paste("select timestamp,averageUtilization from appReqRate where webAppID = 'gRADit' and timestamp > ",startT," order by timestamp DESC ",sep=''))
-		if (nrow(cpuSeries) > 0) {
-			cpuPlotFile=paste("graditAverageCpuUtilization-",period,".png",sep='')
-			png(file= cpuPlotFile, bg="transparent")
-		
-			times <- as.POSIXct( (cpuSeries[TRUE,1]/1000 - TZShift), origin="1970-01-01")
-			par(oma=c(1,1,1,2))
-			
-			yrange = c(0, 1.2*max(cpuSeries[TRUE,2], na.rm=TRUE))
-			plot(times, cpuSeries[TRUE,2], xlab="", ylab="Average CPU utilization", col="red", xaxt="n", type="o", main="",cex.lab=1.2, ylim=yrange)
-			
-			axis.POSIXct(1, times, format="%Y-%m-%d %H:%M:%S", labels = TRUE)
-			mtext("time", side=1,line=3,cex=1.2)
-			mtext("Web server average CPU allocation for gRADit",side=3,cex=1.4,line=2)
-		}
-
 	}
+	axis.POSIXct(1, allTimesPosix, format="%Y-%m-%d %H:%M:%S", labels = TRUE)
+	mtext("time", side=1,line=3,cex=1.2)
+	mtext("Average CPU utilization",side=2,cex=1.2)
+	mtext("Web server average CPU allocation",side=3,cex=1.4,line=2)
+	legend( x="topleft", inset=0.05, as.character(appIDs), cex=1.0, col=1:appNum, bg="white", pch=21:22, lty=1:2)
+
+	}  #end loop over periods
 } #end webapp stats
 
 ############# PIQL stats
@@ -219,78 +217,97 @@ if(SCADS_STATS) {
 	for(period in PERIODS) {
 		startT = (mostRecent - 1000* 60 * period -1)
 	
-	#First get the data...
-		namespaceNamesL <- dbGetQuery(con, paste("select DISTINCT namespace from namespaceReqRate where timestamp > ",
-						 startT," order by namespace",sep=''))
-		namespaceNames = namespaceNamesL[[1]]
-		dataByNamespace = vector("list", dim(namespaceNamesL)[1])
+	appIDs = c("SCADr","gRADit","comRADes")
+	for(appID in appIDs) {
+		print(paste("starting on",appID))
 	
-		i = 0
-		maxReqs = maxServers = 0
-		for(namespace in namespaceNames) {
-			i = i + 1
-			series <- dbGetQuery(con, paste("select timestamp,aggRequestRate,numServers from namespaceReqRate where timestamp > ",startT," and namespace = \"", namespace ,"\" order by timestamp DESC ",sep=''))
-			dataByNamespace[[i]] = series
-			maxReqs = max(maxReqs, max(series[TRUE,2]))
-			maxServers = max(maxServers, max(series[TRUE,3]))
-		}
-		rawAXTime <- dbGetQuery(con, paste("select DISTINCT timestamp from namespaceReqRate where timestamp > ",
-					 startT," order by timestamp DESC",sep=''))
-	
-		axTimes = as.POSIXct( ( rawAXTime[TRUE,1]/1000 - TZShift), origin="1970-01-01")
-	
-	## Now plot
-		reqRatePlotFile=paste("scadsReqRate-",period,".png",sep='')
-		png(file=reqRatePlotFile, bg="transparent")
-	
-		yrange<- c(0, maxReqs * 1.1)
+		#First get the data...
+			namespaceNamesL <- dbGetQuery(con, paste("select DISTINCT namespace from namespaceReqRate where timestamp > ",
+							 startT," and appName = '",appID, "' order by namespace",sep=''))
+			if(length(namespaceNamesL) > 0) {
+				
+			namespaceNames = namespaceNamesL[[1]]
+			dataByNamespace = vector("list", dim(namespaceNamesL)[1])
+				print(paste("found ", length(dataByNamespace), "distinct namespaces for",appID))
 
-		i = 0
-		for(namespace in namespaceNames) {
-			i = i+1		
-			series = dataByNamespace[[i]]
-			times <- as.POSIXct( (series[TRUE,1]/1000 - TZShift), origin="1970-01-01")
-			par(new=T)
-			
-			if (nrow(series) > 0) {
-				plot(times, series[TRUE,2], ylab="", xlab="", col=i, xaxt="n", type="o", main="",ylim=yrange)
-			}
-		}
-		axis.POSIXct(1, axTimes, format="%Y-%m-%d %H:%M:%S", labels = TRUE)
-	# axis(2, series[TRUE,2])
-		mtext("SCADS request rate",side=3,cex=1.4,line=2)
-		mtext("requests per second", side=2, line =3,cex=1.2)
-		mtext("time", side=1, line =2.5,cex=1.2)
-
-		legend( x="topleft", inset=0.05, as.character(namespaceNames), cex=1.0, col=1:i, bg="white", pch=21:22, lty=1:2)
-	
-		par(new=F)
-		dev.off()
-	
-	##Now for machines
-		yrange<- c(0, maxServers * 1.1)
-		reqRatePlotFile=paste("scadsMachineAlloc-",period,".png",sep='')
-		png(file=reqRatePlotFile, bg="transparent")
+			shortNames = rep(0,length(namespaceNames))
+			splitNames = strsplit(namespaceNames,'_')
+			for(i in 1:length(namespaceNames)) 
+				shortNames[i] = splitNames[[i]][1]
+			shortNames = as.character(shortNames)
+			print(paste("successfully computed short names, length =",length(shortNames)))
 			i = 0
-		for(namespace in namespaceNames) {
-			i = i+1		
-			series = dataByNamespace[[i]]
-				times <- as.POSIXct( (series[TRUE,1]/1000 - TZShift), origin="1970-01-01")
-			par(new=T)
-			
-			if (nrow(series) > 0) {
-				plot(times, series[TRUE,3], ylab="", xlab="", col=i,	xaxt="n", type="o", main="",ylim=yrange,cex=1.2)
+			maxReqs = maxServers = 0
+			for(namespace in namespaceNames) {
+				i = i + 1
+				series <- dbGetQuery(con, paste("select timestamp,aggRequestRate,numServers from namespaceReqRate where timestamp > ",startT," and namespace = \"", namespace ,"\" and appName = '",appID, "' order by timestamp DESC ",sep=''))
+				dataByNamespace[[i]] = series
+				maxReqs = max(maxReqs, max(series[TRUE,2]))
+				maxServers = max(maxServers, max(series[TRUE,3]))
 			}
-		}
-		axis.POSIXct(1, axTimes, format="%Y-%m-%d %H:%M:%S", labels = TRUE)
-		mtext("SCADS server allocations",side=3,cex=1.4,line=2)
-		mtext("time", side=1, line =2.5,cex=1.2)
-		mtext("servers", side=2, line =3,cex=1.2)
+			rawAXTime <- dbGetQuery(con, paste("select DISTINCT timestamp from namespaceReqRate where timestamp > ",
+						 startT," and appName = '",appID, "' order by timestamp DESC",sep=''))
+			print("done with queries")
+		
+			axTimes = as.POSIXct( ( rawAXTime[TRUE,1]/1000 - TZShift), origin="1970-01-01")
+		
+		## Now plot
+			reqRatePlotFile=paste("scadsReqRate-",appID,"-",period,".png",sep='')
+			png(file=reqRatePlotFile, bg="transparent")
+		
+			yrange<- c(0, maxReqs * 1.1)
 	
-		legend( x="topleft", inset=0.05, as.character(namespaceNames), cex=1.0, col=1:i, bg="white", pch=21:22, lty=1:2)
-		par(new=F)
-		dev.off()
-	}
+			i = 0
+			for(namespace in namespaceNames) {
+				print(paste("plotting req rate for namespace",namespace))
+
+				i = i+1		
+				series = dataByNamespace[[i]]
+				
+				if (nrow(series) > 0) {
+					times <- as.POSIXct( (series[TRUE,1]/1000 - TZShift), origin="1970-01-01")
+					par(new=T)
+					plot(times, series[TRUE,2], ylab="", xlab="", col=i, xaxt="n", type="o", main="",ylim=yrange)
+				}
+			}
+			axis.POSIXct(1, axTimes, format="%Y-%m-%d %H:%M:%S", labels = TRUE)
+			mtext(paste("SCADS request rate:",appID),side=3,cex=1.4,line=2)
+			mtext("requests per second", side=2, line =3,cex=1.2)
+			mtext("time", side=1, line =2.5,cex=1.2)
+	
+			legend( x="topleft", inset=0.05, shortNames, cex=1.0, col=1:i, bg="white", pch=21:22, lty=1:2)
+		
+			par(new=F)
+			dev.off()
+		
+		##Now for machines
+			yrange<- c(0, maxServers * 1.1)
+			reqRatePlotFile=paste("scadsMachineAlloc-",appID,"-",period,".png",sep='')
+			png(file=reqRatePlotFile, bg="transparent")
+				i = 0
+			for(namespace in namespaceNames) {
+				print(paste("plotting allocation for namespace",namespace))
+
+				i = i+1		
+				series = dataByNamespace[[i]]
+				if (nrow(series) > 0) {
+					times <- as.POSIXct( (series[TRUE,1]/1000 - TZShift), origin="1970-01-01")
+					par(new=T)
+					plot(times, series[TRUE,3], ylab="", xlab="", col=i,	xaxt="n", type="o", main="",ylim=yrange,cex=1.2)
+				}
+			}
+			axis.POSIXct(1, axTimes, format="%Y-%m-%d %H:%M:%S", labels = TRUE)
+			print("plotting legend")
+			mtext(paste("SCADS server allocations:",appID),side=3,cex=1.4,line=2)
+			mtext("time", side=1, line =2.5,cex=1.2)
+			mtext("servers", side=2, line =3,cex=1.2)
+		
+			legend( x="topleft", inset=0.05, shortNames, cex=1.0, col=1:i, bg="white", pch=21:22, lty=1:2)
+			par(new=F)
+			dev.off()
+			}
+		} #end per appID
+	} #end per time period
 }
 
 dbDisconnect(con)
