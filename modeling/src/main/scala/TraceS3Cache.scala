4ed6bd4e4602a1da660e7e9a93dbff95955f3073
Kristal Curtis <kcurtis@eecs.berkeley.edu>
2011-03-02 12:10:46 -0800

    added latest version of s3 uploader

--- modeling/src/main/scala/TraceS3Cache.scala
+++ modeling/src/main/scala/TraceS3Cache.scala
@@ -14,14 +14,14 @@ import org.jets3t.service.acl.AccessControlList
 
 object TraceS3Cache extends AWSConnection {
 
-  protected val credentials = new AWSCredentials("AKIAILGVHXBVDZKFJZQQ", "VdB4xNttSvG8DOeF90XQI4jqg6EOi6L00nt0Lq3n") // your credentials here
-  val s3Service = new RestS3Service(credentials)
+  protected val s3Credentials = new AWSCredentials("AKIAILGVHXBVDZKFJZQQ", "VdB4xNttSvG8DOeF90XQI4jqg6EOi6L00nt0Lq3n") // your credentials here
+  val s3Service = new RestS3Service(s3Credentials)
   val bucketName = "piql-modeling" // make this your bucket name
   val bucket = s3Service.createBucket(bucketName)
 
-  def uploadFile(file: File, suffix: String = "") {
+  def uploadFile(file: File, prefix: String = "", suffix: String = "") {
     val obj = new S3Object(file)
-    obj.setKey(file.getName() + "-" + suffix)
+    obj.setKey(prefix + "/" + file.getName() + "-" + suffix)
     obj.setAcl(AccessControlList.REST_CANNED_PUBLIC_READ)
     s3Service.putObject(bucket, obj)
   }
