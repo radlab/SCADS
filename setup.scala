d2ede2bdd06e0c2db2572784f28ab8baf4d0a00b
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-03-06 20:19:52 -0800

    fix master setup for experiment running.

--- setup.scala
+++ setup.scala
@@ -1,13 +1,8 @@
-import edu.berkeley.cs.radlab.demo._
 import edu.berkeley.cs.scads.storage._
 import edu.berkeley.cs.scads.comm._
 import edu.berkeley.cs.scads.piql._
-import edu.berkeley.cs.scads.piql.gradit._
 import edu.berkeley.cs.scads.piql.scadr._
-import edu.berkeley.cs.scads.piql.comrades._
+import edu.berkeley.cs.scads.piql.modeling.Experiments._
 import edu.berkeley.cs._
 import deploylib.mesos._
-
-lazy val testGraditClient = new GraditClient(TestScalaEngine.newScadsCluster(), new DashboardReportingExecutor())
-lazy val testScadrClient = new ScadrClient(TestScalaEngine.newScadsCluster(), new DashboardReportingExecutor())
-lazy val testComradesClient = new ComradesClient(TestScalaEngine.newScadsCluster(), new DashboardReportingExecutor())
+import deploylib.ec2._
