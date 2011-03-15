9315937e75ca00f067c531191dc1099d9d9a302b
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-03-15 11:41:24 -0700

    read from a setup file instead of hardcoding imports.

--- repl/src/main/scala/edu/berkeley/cs/comet/Repl.scala
+++ repl/src/main/scala/edu/berkeley/cs/comet/Repl.scala
@@ -40,19 +40,7 @@ class Repl extends CometActor {
   this ! InitRepl
 
 //TODO: this probably shouldn't be hardcoded here
-val initSeq = """
-import deploylib._
-import deploylib.mesos._
-import deploylib.ec2._
-import edu.berkeley.cs.scads.comm._
-import edu.berkeley.cs.scads.storage._
-import edu.berkeley.cs.scads.piql._
-import edu.berkeley.cs.avro.marker._
-import edu.berkeley.cs.avro.runtime._
-import edu.berkeley.cs.radlab.demo._
-import java.io.File
-import edu.berkeley.cs.scads.perf._
-"""
+val initSeq = deploylib.Util.readFile(new java.io.File("setup.scala"))
 
   override def lowPriority = {
     case InitRepl => {
