0a00b8af115ba71856b9d05fdd3b762c8e3439fe
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-03-16 18:46:57 -0700

    better logging in s3 cache

--- modeling/src/main/scala/util/TraceS3Cache.scala
+++ modeling/src/main/scala/util/TraceS3Cache.scala
@@ -13,6 +13,7 @@ import org.jets3t.service.security.AWSCredentials
 import org.jets3t.service.acl.AccessControlList
 
 object TraceS3Cache extends AWSConnection {
+  protected val logger = Logger()
    protected val s3Credentials = new AWSCredentials(accessKeyId, secretAccessKey) // your credentials here
   val s3Service = new RestS3Service(s3Credentials)
   val bucketName = "piql-modeling-marmbrus" // make this your bucket name
@@ -20,7 +21,10 @@ object TraceS3Cache extends AWSConnection {
 
   def uploadFile(file: File, prefix: String = "", suffix: String = "") {
     val obj = new S3Object(file)
-    obj.setKey(prefix + "/" + file.getName() + "-" + suffix + "-" + System.currentTimeMillis)
+    val key = prefix + "/" + file.getName() + "-" + suffix
+
+    logger.info("Uploading file %s to s3://%s/%s", file, bucketName, key) 
+    obj.setKey(key)
     obj.setAcl(AccessControlList.REST_CANNED_PUBLIC_READ)
     s3Service.putObject(bucket, obj)
   }
