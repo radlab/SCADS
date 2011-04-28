baf98aa4779a745b83ce06b44094a4a2d83250af
Kristal Curtis <kcurtis@eecs.berkeley.edu>
2011-04-28 13:33:57 -0700

    updated QueryDescription to store result cardinality; fixed white space

--- modeling/src/main/scala/collection/QueryRunnerTask.scala
+++ modeling/src/main/scala/collection/QueryRunnerTask.scala
@@ -30,7 +30,8 @@ case class CardinalityList(values: IndexedSeq[Int]) extends ParameterGenerator {
 case class QuerySpec(query: OptimizedQuery, paramGenerators: Seq[ParameterGenerator])
 
 
-case class QueryDescription(var queryName: String, var parameters: Seq[Int]) extends AvroRecord
+case class QueryDescription(var queryName: String, var parameters: Seq[Int], var resultCardinality: Int) extends AvroRecord
+
 case class Result(var queryDesc: QueryDescription, var hostname: String, var timestamp: Long) extends AvroPair {
   var iteration: Int = _
   var clientConfig: QueryRunnerTask = null
@@ -76,11 +77,11 @@ case class QueryRunnerTask(var numClients: Int,
 
     val executor =
       if(traceIterators)
-	new ParallelExecutor with TracingExecutor {
-	  val sink = traceSink
-	}
+	      new ParallelExecutor with TracingExecutor {
+	        val sink = traceSink
+	      }
       else
-	new ParallelExecutor
+	      new ParallelExecutor
     
     if(traceMessages) {
       logger.info("registering listener...")
@@ -98,56 +99,57 @@ case class QueryRunnerTask(var numClients: Int,
 
       logger.info("Beginning iteration %d", iteration)
       (1 to threads).pmap(threadId => {
-	val seed = java.net.InetAddress.getLocalHost.getHostName + System.currentTimeMillis + threadId
-	val rand = new Random(seed.hashCode)
-	val runTime = iterationLengthMin * 60 * 1000L
-	val iterationStartTime = getTime
-	var queryCounter = 0
+	      val seed = java.net.InetAddress.getLocalHost.getHostName + System.currentTimeMillis + threadId
+	      val rand = new Random(seed.hashCode)
+	      val runTime = iterationLengthMin * 60 * 1000L
+	      val iterationStartTime = getTime
+	      var queryCounter = 0
 
-	while(getTime - iterationStartTime < runTime) {
-	  val querySpec = querySpecs(rand.nextInt(querySpecs.size))
-	  val params = querySpec.paramGenerators.map(_.getValue(rand))
-	  val runParams = params.map(_._1)
-	  val paramsDesc = params.flatMap(_._2)
-	  val queryDesc = QueryDescription(querySpec.query.name.getOrElse("unnamed"), paramsDesc)
+	      while(getTime - iterationStartTime < runTime) {
+	        val querySpec = querySpecs(rand.nextInt(querySpecs.size))
+	        val params = querySpec.paramGenerators.map(_.getValue(rand))
+	        val runParams = params.map(_._1)
+	        val paramsDesc = params.flatMap(_._2)
+	        val queryDesc = QueryDescription(querySpec.query.name.getOrElse("unnamed"), paramsDesc, 0)
 
-	  if(traceQueries)
-	    traceSink.recordEvent(QueryEvent(queryDesc.queryName, paramsDesc, queryCounter, true))
+	        if(traceQueries)
+	          traceSink.recordEvent(QueryEvent(queryDesc.queryName, paramsDesc, queryCounter, true))
 
-	  val startTime = getTime
-	  try {
-	    querySpec.query.apply(runParams: _*)
-	    val endTime = getTime
+	        val startTime = getTime
+	        try {
+	          val results = querySpec.query.apply(runParams: _*)
+	          queryDesc.resultCardinality = results.size
+	          val endTime = getTime
 
-	    if(traceQueries)
-	      traceSink.recordEvent(QueryEvent(queryDesc.queryName, paramsDesc, queryCounter, false))
+	          if(traceQueries)
+	            traceSink.recordEvent(QueryEvent(queryDesc.queryName, paramsDesc, queryCounter, false))
 
-	    if(!responseTimes.contains(queryDesc)) {
-	      responseTimes.putIfAbsent(queryDesc, Histogram(1, 1000))
-	    }
+	          if(!responseTimes.contains(queryDesc)) {
+	            responseTimes.putIfAbsent(queryDesc, Histogram(1, 1000))
+	          }
 
-	    responseTimes.get(queryDesc) += (endTime - startTime)
-	    queryCounter += 1
-	  } catch {
-	    case e =>
-	      logger.warning(e, "Query %s failed", querySpec.query.name)
-	      if(!failedQueries.contains(queryDesc))
-		failedQueries.putIfAbsent(queryDesc, new AtomicInteger)
-	      failedQueries.get(queryDesc).incrementAndGet
-	  }
-	}
+	          responseTimes.get(queryDesc) += (endTime - startTime)
+	          queryCounter += 1
+	        } catch {
+	          case e =>
+	            logger.warning(e, "Query %s failed", querySpec.query.name)
+	            if(!failedQueries.contains(queryDesc))
+		            failedQueries.putIfAbsent(queryDesc, new AtomicInteger)
+	            failedQueries.get(queryDesc).incrementAndGet
+	        }
+	      }
 	
-	logger.info("Thread %d completed with %d queries.", threadId, queryCounter)
+	      logger.info("Thread %d completed with %d queries.", threadId, queryCounter)
       })
 
       logger.info("Recording results to scads")
       results ++= responseTimes.entrySet.map(e => {
-	val result = Result(e.getKey, hostname, System.currentTimeMillis)
-	result.iteration = iteration
-	result.clientConfig = this
-	result.responseTimes = e.getValue
-	result.failedQueries = Option(failedQueries.get(e.getKey)).map(_.get).getOrElse(0)
-	result
+	      val result = Result(e.getKey, hostname, System.currentTimeMillis)
+	      result.iteration = iteration
+	      result.clientConfig = this
+	      result.responseTimes = e.getValue
+	      result.failedQueries = Option(failedQueries.get(e.getKey)).map(_.get).getOrElse(0)
+	      result
       })
     }
 
