fda620aec5cf88fcaa7aa9d0442eda6517ce1f6d
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-05-02 15:13:23 -0700

    add option to turn mesos FT on and off.

--- deploylib/src/main/scala/mesos/MesosEC2.scala
+++ deploylib/src/main/scala/mesos/MesosEC2.scala
@@ -28,7 +28,7 @@ object ServiceSchedulerDaemon extends optional.Application {
 /**
  * Functions to help maintain a mesos cluster on EC2.
  */
-class Cluster() extends ConfigurationActions {
+class Cluster(useFT: Boolean = false) extends ConfigurationActions {
   val rootDir = new File("/usr/local/mesos/frameworks/deploylib")
 
   val mesosAmi =
@@ -178,7 +178,10 @@ class Cluster() extends ConfigurationActions {
       firstMaster ! "rsync -e 'ssh -o StrictHostKeyChecking=no' -av /usr/local/mesos root@%s:/usr/local/mesos".format(s.publicDnsName))
 
   def clusterUrl: String =
-    "zoo://" + zooKeeperRoot.proxy.servers.mkString(",") + zooKeeperRoot.getOrCreate("mesos").path
+    if(useFT)
+      "zoo://" + zooKeeperRoot.proxy.servers.mkString(",") + zooKeeperRoot.getOrCreate("mesos").path
+    else
+      "1@" + firstMaster.publicDnsName + ":5050"
 
   def restartSlaves: Unit = {
     slaves.pforeach(_ ! "service mesos-slave stop")
@@ -263,15 +266,25 @@ class Cluster() extends ConfigurationActions {
     instances
   }
 
-  def updateConf(instances: Seq[EC2Instance] = (slaves ++ masters)): Unit = {
-    val baseConf = ("work_dir=/mnt" ::
+  val baseConf = ("work_dir=/mnt" ::
       "log_dir=/mnt" ::
       "switch_user=0" ::
       "shares_interval=30" :: Nil)
-    val conf = (baseConf :+ ("url=" + clusterUrl)).mkString("\n")
-    val conffile = new File("/usr/local/mesos/conf/mesos.conf")
+  def slaveConf = (baseConf :+ ("url=" + clusterUrl)).mkString("\n")
+  val conffile = new File("/usr/local/mesos/conf/mesos.conf")
 
-    instances.pforeach(_.createFile(conffile, conf))
+  def updateConf(instances: Seq[EC2Instance] = slaves): Unit = {
+    instances.pforeach(_.createFile(conffile, slaveConf))
+  }
+
+  def updateMasterConf: Unit = {
+    val masterConf =
+      if(useFT)
+        slaveConf
+      else
+        baseConf.mkString("\n")
+
+    masters.pforeach(_.createFile(conffile, masterConf))
   }
 
   //TODO: Doesn't handle non s3 cached jars
