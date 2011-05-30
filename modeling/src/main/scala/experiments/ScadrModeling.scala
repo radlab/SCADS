db876b0f758908e00aecc01c58d9ce9d08cc99d2
Kristal Curtis <kcurtis@eecs.berkeley.edu>
2011-05-30 12:41:14 -0700

    added code for per-interval prediction in scadr modeling

--- modeling/src/main/scala/experiments/ScadrModeling.scala
+++ modeling/src/main/scala/experiments/ScadrModeling.scala
@@ -17,11 +17,13 @@ import scala.collection.mutable.ArrayBuffer
 
 object ScadrModeling {
   object ScadrData {
-    val clusterAddress = "zk://ec2-174-129-157-147.compute-1.amazonaws.com:2181,ec2-50-17-12-53.compute-1.amazonaws.com:2181,ec2-184-72-171-124.compute-1.amazonaws.com:2181/scads/experimentCluster0000000015"
+    val clusterAddress = "zk://ec2-50-17-12-53.compute-1.amazonaws.com:2181,ec2-184-72-171-124.compute-1.amazonaws.com:2181,ec2-174-129-157-147.compute-1.amazonaws.com:2181/scads/experimentCluster0000000063"
+    
 
     def experimentResults = allResults.filter(_.clientConfig.clusterAddress == clusterAddress)
     
-    def goodExperimentResults = experimentResults.filter(_.iteration == 2)//filterNot(_.iteration == 1)
+    val numIntervals = 14
+    def goodExperimentResults = experimentResults.filter(r => r.iteration > 1 && r.iteration <= numIntervals)
     
     val histogramsScadr = queryTypeHistogram(goodExperimentResults.toSeq)
     
@@ -56,7 +58,31 @@ object ScadrModeling {
     
     val actual99th = findUserHist.quantile(0.99)
     val predicted99th = indexLookupUsersHist.quantile(0.99)
-    
+   
+    def predictOneInterval(i: Int, desiredQuantile: Double): Int = {
+      val indexLookupUsersHist = perIterationHistograms((indexLookupUsers, i))
+      indexLookupUsersHist.quantile(desiredQuantile)
+    }
+
+    def getPerIntervalPrediction(quantile: Double = 0.99):(Histogram, Histogram) = {
+      val actualQuantileHist = Histogram(1,1000)
+      val predictedQuantileHist = Histogram(1,1000)
+
+      println("interval, actualQuantile, predictedQuantile")
+
+      (2 to numIntervals).foreach(i => {
+        val actualHist = perIterationHistograms((findUser, i))
+        val actualQuantile = actualHist.quantile(quantile)
+        actualQuantileHist += actualQuantile
+
+        val predictedQuantile = predictOneInterval(i, quantile)
+        predictedQuantileHist += predictedQuantile
+
+        println(List(i, actualQuantile, predictedQuantile).mkString(","))
+      })
+
+      (actualQuantileHist, predictedQuantileHist)
+    }
   }
   
   object ModelMyThoughts {
@@ -74,12 +100,38 @@ object ScadrModeling {
     
     val actual99th = myThoughtsHist.quantile(0.99)
     val predicted99th = indexScanThoughtsHist.quantile(0.99)
+    
+    def predictOneInterval(i: Int, desiredQuantile: Double): Int = {
+      val indexScanThoughtsHist = perIterationHistograms((indexScanThoughts, i))
+      indexScanThoughtsHist.quantile(desiredQuantile)
+    }
+
+    def getPerIntervalPrediction(quantile: Double = 0.99):(Histogram, Histogram) = {
+      val actualQuantileHist = Histogram(1,1000)
+      val predictedQuantileHist = Histogram(1,1000)
+
+      println("interval, actualQuantile, predictedQuantile")
+
+      (2 to numIntervals).foreach(i => {
+        val actualHist = perIterationHistograms((myThoughts, i))
+        val actualQuantile = actualHist.quantile(quantile)
+        actualQuantileHist += actualQuantile
+
+        val predictedQuantile = predictOneInterval(i, quantile)
+        predictedQuantileHist += predictedQuantile
+
+        println(List(i, actualQuantile, predictedQuantile).mkString(","))
+      })
+
+      (actualQuantileHist, predictedQuantileHist)
+    }
   }
   
   object ModelThoughtstream {
     import ScadrData._
+    import TpcwModeling.Util._
     
-    val thoughtstream = QueryDescription("thoughtstream", List(100, 50), 50) // TODO:  choose cardinality here
+    val thoughtstream = QueryDescription("thoughtstream", List(100, 50), 50)
     val thoughtstreamHist = histogramsScadr(thoughtstream)
     
     //scala> res1.thoughtstream.physicalPlan  
@@ -87,15 +139,54 @@ object ScadrModeling {
     //                                             IndexMergeJoin(<Namespace: thoughts>,ArrayBuffer(AttributeValue(0,1)),List(AttributeValue(1,1)),ParameterLimit(1,10000),false,
     //                                             IndexScan(<Namespace: subscriptions>,ArrayBuffer(ParameterValue(0)),FixedLimit(10000),true)))
     
-    val indexMergeJoinThoughts = QueryDescription("indexMergeJoinThoughts", List(100, 50), 50)  // TODO
-    val indexMergeJoinThoughtsHist = histogramsScadr(indexMergeJoinThoughts)
+    val indexMergeJoinThoughts = QueryDescription("indexMergeJoinThoughts", List(100, 50), 50)
+    val indexMergeJoinThoughtsHist = histogramsScadr(indexMergeJoinThoughts).buckets.map(BigInt(_))
     
-    val indexScanSubscriptions = QueryDescription("indexScanSubscriptions", List(100), 100) // TODO
-    val indexScanSubscriptionsHist = histogramsScadr(indexScanSubscriptions)
+    val indexScanSubscriptions = QueryDescription("indexScanSubscriptions", List(100), 100)
+    val indexScanSubscriptionsHist = histogramsScadr(indexScanSubscriptions).buckets.map(BigInt(_))
+    
+    def predictHist = {
+      val res = convolve(indexScanSubscriptionsHist, indexMergeJoinThoughtsHist)
+      res
+    }
+    
+    val actual99th = thoughtstreamHist.quantile(0.99)
+    val predicted99th = quantile(predictHist, 0.99)
+    
+    def predictOneInterval(i: Int, desiredQuantile: Double): Int = {
+      val indexScanSubscriptionsHist = perIterationHistograms((indexScanSubscriptions, i)).buckets.map(BigInt(_))
+      val indexMergeJoinThoughtsHist = perIterationHistograms((indexMergeJoinThoughts, i)).buckets.map(BigInt(_))
+      
+      val res = convolve(indexScanSubscriptionsHist, indexMergeJoinThoughtsHist)
+
+      quantile(res, desiredQuantile)
+    }
+    
+    def getPerIntervalPrediction(quantile: Double = 0.99):(Histogram, Histogram) = {
+      val actualQuantileHist = Histogram(1,1000)
+      val predictedQuantileHist = Histogram(1,1000)
+      
+      println("interval, actualQuantile, predictedQuantile")
+      
+      (2 to numIntervals).foreach(i => {
+        val actualHist = perIterationHistograms((thoughtstream, i))
+        val actualQuantile = actualHist.quantile(quantile)
+        actualQuantileHist += actualQuantile
+        
+        val predictedQuantile = predictOneInterval(i, quantile)
+        predictedQuantileHist += predictedQuantile
+        
+        println(List(i, actualQuantile, predictedQuantile).mkString(","))
+      })
+      
+      (actualQuantileHist, predictedQuantileHist)
+    }
+    
   }
   
   object ModelUsersFollowedBy {
     import ScadrData._
+    import TpcwModeling.Util._
     
     val usersFollowedBy = QueryDescription("usersFollowedBy", List(50), 50) // TODO:  choose cardinality here
     val usersFollowedByHist = histogramsScadr(usersFollowedBy)
@@ -106,10 +197,47 @@ object ScadrModeling {
     //                                             IndexScan(<Namespace: subscriptions>,ArrayBuffer(ParameterValue(0)),ParameterLimit(1,10000),true)))
     
     val indexLookupJoinUsers = QueryDescription("indexLookupJoinUsers", List(50), 50)
-    val indexLookupJoinUsersHist = histogramsScadr(indexLookupJoinUsers)
+    val indexLookupJoinUsersHist = histogramsScadr(indexLookupJoinUsers).buckets.map(BigInt(_))
     
     val indexScanSubscriptions = QueryDescription("indexScanSubscriptions", List(50), 50)
-    val indexScanSubscriptionsHist = histogramsScadr(indexScanSubscriptions)
+    val indexScanSubscriptionsHist = histogramsScadr(indexScanSubscriptions).buckets.map(BigInt(_))
+    
+    def predictHist = {
+      val res = convolve(indexScanSubscriptionsHist, indexLookupJoinUsersHist)
+      res
+    }
+    
+    val actual99th = usersFollowedByHist.quantile(0.99)
+    val predicted99th = quantile(predictHist, 0.99)
+
+    def predictOneInterval(i: Int, desiredQuantile: Double): Int = {
+      val indexScanSubscriptionsHist = perIterationHistograms((indexScanSubscriptions, i)).buckets.map(BigInt(_))
+      val indexLookupJoinUsersHist = perIterationHistograms((indexLookupJoinUsers, i)).buckets.map(BigInt(_))
+      
+      val res = convolve(indexScanSubscriptionsHist, indexLookupJoinUsersHist)
+
+      quantile(res, desiredQuantile)
+    }
+    
+    def getPerIntervalPrediction(quantile: Double = 0.99):(Histogram, Histogram) = {
+      val actualQuantileHist = Histogram(1,1000)
+      val predictedQuantileHist = Histogram(1,1000)
+      
+      println("interval, actualQuantile, predictedQuantile")
+      
+      (2 to numIntervals).foreach(i => {
+        val actualHist = perIterationHistograms((usersFollowedBy, i))
+        val actualQuantile = actualHist.quantile(quantile)
+        actualQuantileHist += actualQuantile
+        
+        val predictedQuantile = predictOneInterval(i, quantile)
+        predictedQuantileHist += predictedQuantile
+        
+        println(List(i, actualQuantile, predictedQuantile).mkString(","))
+      })
+      
+      (actualQuantileHist, predictedQuantileHist)
+    }
   }
   
 }
