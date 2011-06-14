4acd40222cd6048e869bbfbd50a3ea01280501c5
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-06-13 22:55:17 -0700

    remove unneeded log4j use

--- comm/src/main/scala/Test.scala
+++ comm/src/main/scala/Test.scala
@@ -1,7 +1,6 @@
 package edu.berkeley.cs.scads.comm
 
 import net.lag.logging.Logger
-import org.apache.log4j.BasicConfigurator
 
 object EchoActor extends MessageReceiver {
   implicit val remoteActor = MessageHandler.registerService(this)
@@ -13,7 +12,6 @@ object EchoActor extends MessageReceiver {
   }
 
   def main(args: Array[String]): Unit = {
-    BasicConfigurator.configure()
     logger.info("EchoActor running at: " + remoteActor)
   }
 }
