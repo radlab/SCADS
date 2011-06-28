fe0e5a14ed71fa956ce86c13d955467f035c11b8
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-06-28 12:22:32 -0700

    update setup files.

--- deploylib/setup.scala
+++ deploylib/setup.scala
@@ -12,3 +12,5 @@ def debug = Logger("deploylib").setLevel(java.util.logging.Level.FINEST)
 def stopAllInstances: Unit = {
   EC2Instance.activeInstances.pforeach(_.halt)
 }
+
+val cluster = new Cluster()
--- setup.scala
+++ setup.scala
@@ -4,15 +4,6 @@ import edu.berkeley.cs.scads.perf._
 import edu.berkeley.cs.scads.piql._
 import edu.berkeley.cs.scads.piql.scadr._
 import edu.berkeley.cs.scads.piql.tpcw._
-import edu.berkeley.cs.scads.piql.modeling._
-import edu.berkeley.cs.scads.piql.modeling.Experiments._
 import edu.berkeley.cs._
 import deploylib.mesos._
 import deploylib.ec2._
-import QueryRunner._
-import ExperimentUtil._
-import TpcwModeling._
-import TpcwData._
-import ModelOrderDisplayGetLastOrder._
-//import ModelingExperiments._
-import ScadrModeling._
