fc9853076dd3fd64639c21095d92249397becc46
Tim Kraska <kraska@cs.berkeley.edu>
2011-07-13 21:43:46 -0700

    Test commit

--- scalaengine/setup.scala
+++ scalaengine/setup.scala
@@ -1,4 +1,4 @@
-import edu.berkeley.cs.scads.comm._
+ import edu.berkeley.cs.scads.comm._
 import com.googlecode.avro.runtime._
 import com.googlecode.avro.marker._
 import org.apache.avro.generic.GenericData
