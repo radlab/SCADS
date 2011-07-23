31fefaefa1b7231707d405822ca02fe844e21ba1
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-07-23 02:36:53 -0700

    code for running on ec2 and visualizing results.

--- modeling/src/main/scala/experiments/PlanCompare.scala
+++ modeling/src/main/scala/experiments/PlanCompare.scala
@@ -9,25 +9,50 @@ import comm._
 import storage._
 import avro.runtime._
 import avro.marker._
+import org.apache.zookeeper.CreateMode
 
 import edu.berkeley.cs.scads.piql.scadr.Subscription
 
 case class PlanCompareResult(var hostname: String,
 			     var iteration: Int,
 			     var point: Int,
+			     var query: String,
 			     var config: PlanCompareTask) extends AvroPair {
   var responseTimes: Histogram = null
 }
-
-			  
+		  
 object PlanCompare {
   import Experiments._
+
+  def newScadsCluster(size: Int): ScadsCluster = {
+    val clusterRoot = cluster.zooKeeperRoot.getOrCreate("scads").createChild("experimentCluster", mode = CreateMode.PERSISTENT_SEQUENTIAL)
+    val serverProcs = Array.fill(size)(ScalaEngineTask(clusterAddress=clusterRoot.canonicalAddress).toJvmTask)
+
+    cluster.serviceScheduler.scheduleExperiment(serverProcs)
+    new ScadsCluster(clusterRoot)
+  }
+
+  def run: Unit = {
+    val scadsCluster = newScadsCluster(2)
+    val compareTask = PlanCompareTask(
+      clusterAddress = scadsCluster.root.canonicalAddress,
+      resultClusterAddress = resultClusterAddress.canonicalAddress
+    ).toJvmTask
+    serviceScheduler.scheduleExperiment(compareTask :: Nil)
+  }
 
   def testLocal =
     PlanCompareTask(
       clusterAddress = TestScalaEngine.newScadsCluster(2).root.canonicalAddress,
       resultClusterAddress = resultClusterAddress.canonicalAddress
     ).run()
+
+  val results = resultsCluster.getNamespace[PlanCompareResult]("planCompareResults")
+  def allResults = results.iterateOverRange(None,None)
+  def goodResults = allResults
+
+  def graphPoints = goodResults.map(r => (r.point * r.config.scaleStep, r.responseTimes.quantile(0.99))).toSeq
+
 }
    
 case class PlanCompareTask(var clusterAddress: String,
@@ -35,7 +60,7 @@ case class PlanCompareTask(var clusterAddress: String,
 			   var replicationFactor: Int = 2,
 			   var iterations: Int = 5,
 			   var points: Int = 10,
-			   var scaleStep: Int = 10,
+			   var scaleStep: Int = 1,
 			   var runLengthMin: Int = 1) extends AvroTask with AvroRecord {
 
 
@@ -112,6 +137,7 @@ case class PlanCompareTask(var clusterAddress: String,
 	  val result = PlanCompareResult(java.net.InetAddress.getLocalHost.getHostName,
 			    iteration,
 			    point,
+			    query.name.getOrElse("unnamed"),
 			    this)
 	  result.responseTimes = responseTimes
 	  result
