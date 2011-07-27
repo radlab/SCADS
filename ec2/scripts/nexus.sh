1a351463382fcdc4fe6a936af3cd2deb340c4689
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-07-27 00:57:00 -0700

    update location of nexus war

--- ec2/scripts/nexus.sh
+++ ec2/scripts/nexus.sh
@@ -1,5 +1,5 @@
 cd /usr/share/jetty/webapps 
-wget -c http://nexus.sonatype.org/downloads/nexus-webapp-1.6.0.war -O /usr/share/jetty/webapps/nexus.war
+wget -c http://nexus.sonatype.org/downloads/older/nexus-webapp-1.6.0.war -O /usr/share/jetty/webapps/nexus.war
 mkdir -p /home/jetty/nexus
 ln -s /home/jetty/nexus /usr/share/jetty/sonatype-work
 
