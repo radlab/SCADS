abe77a96090b6830f037e7bb609d2774fe7601d5
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-08-05 14:09:16 -0700

    begining of fix to classloader issues when running tests in sbt.

--- comm/src/main/scala/netty/AvroSpecificDecoder.scala
+++ comm/src/main/scala/netty/AvroSpecificDecoder.scala
@@ -15,13 +15,44 @@ import specific._
 
 import net.lag.logging.Logger
 
+/**
+ * Special vesion of SpecificData allows the user to specify the classloader
+ * instead of relying on the classloader used to load SpecificData
+ */
+class CustomLoaderSpecificData(loader: ClassLoader) extends SpecificData {
+  import org.apache.avro.Schema.Type._
+  
+  private val classCache = new java.util.concurrent.ConcurrentHashMap[String, Class[_]]
+  
+  val NO_CLASS = new Object(){}.getClass();
+
+  override def getClass(schema: Schema): Class[_] = schema.getType match {
+    case FIXED | RECORD | ENUM =>
+      val name = schema.getFullName();
+      if (name == null) return null
+      var c = classCache.get(name)
+      if (c == null) {
+        try {
+          c = loader.loadClass(getClassName(schema))
+        } catch {
+          case e: ClassNotFoundException => c = NO_CLASS
+        }
+        classCache.put(name, c)
+      }
+      return if(c == NO_CLASS) null else c
+    case _ => super.getClass(schema)
+  }
+}
+
 class AvroSpecificDecoder[M <: SpecificRecord](implicit m: Manifest[M])
   extends OneToOneDecoder {
 
   private val msgClass = m.erasure.asInstanceOf[Class[M]]
 
+  private val schema = msgClass.newInstance.getSchema
   private val msgReader = 
-    new SpecificDatumReader[M](msgClass.newInstance.getSchema)
+    new SpecificDatumReader[M](schema)
+//Not working yet    new SpecificDatumReader[M](schema, schema, new CustomLoaderSpecificData(msgClass.getClassLoader))
 
   private val logger = Logger()
 
