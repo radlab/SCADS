279c86da2a3fe917bd2285488c50f76b0658393f
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-08-12 13:48:27 -0500

    fix PIQL tests

--- piql/src/test/scala/OptimizerSpec.scala
+++ piql/src/test/scala/OptimizerSpec.scala
@@ -93,7 +93,7 @@ class OptimizerSpec extends Spec with ShouldMatchers {
 
     //Optimize query first so index is created
     val optQuery = query.opt
-    val idx = r2.getOrCreateIndex("f2" :: Nil)
+    val idx = r2.getOrCreateIndex(AttributeIndex("f2") :: Nil)
 
     val plan =
       LocalStopAfter(FixedLimit(10),
