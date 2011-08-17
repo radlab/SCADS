345a75486977ffbce62baa106fc96ed3949d126a
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-08-17 16:16:23 -0700

    switch to new ami with newest version of mesos.

--- deploylib/src/main/scala/mesos/MesosEC2.scala
+++ deploylib/src/main/scala/mesos/MesosEC2.scala
@@ -32,7 +32,7 @@ class Cluster(useFT: Boolean = false) extends ConfigurationActions {
   val rootDir = new File("/usr/local/mesos/frameworks/deploylib")
 
   val mesosAmi =
-    if (EC2Instance.endpoint contains "west") "ami-2b6b386e" else "ami-5af60d33"
+    if (EC2Instance.endpoint contains "west") "ami-2b6b386e" else "ami-2d60a144"
 
   val defaultZone =
     if (EC2Instance.endpoint contains "west") "us-west-1a" else "us-east-1b"
