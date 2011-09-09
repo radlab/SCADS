3699af61f71d4d3763934253b7cb619c7f116d60
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-09-09 00:06:10 -0700

    update usage of scadr loader

--- piql/scadr/src/test/scala/AbstractScadrSpec.scala
+++ piql/scadr/src/test/scala/AbstractScadrSpec.scala
@@ -36,9 +36,9 @@ class LazyScadrSpec extends AbstractScadrSpec {
 abstract class AbstractScadrSpec extends Spec with ShouldMatchers with QueryResultMatchers {
 
   val client: ScadrClient
-  val loader = new ScadrLoader(client, 1, 1)
+  val loader = new ScadrLoader(1, 1)
   val data = loader.getData(0)
-  data.load()
+  data.load(client)
 
   private def usersFollowedBy(username: String) = {
     val subs = data.subscriptionData.filter(_.owner == username).map( _.target).toSet
