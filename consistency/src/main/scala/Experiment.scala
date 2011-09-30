184390bba9a223e2c55e9c93b6c3489345264e82
Gene Pang <gpang@eecs.berkeley.edu>
2011-09-30 14:19:25 -0700

    simple test experiment for cross datacenter deployment.

---
+++ consistency/src/main/scala/Experiment.scala
@@ -0,0 +1,51 @@
+package edu.berkeley.cs.scads.consistency
+
+import deploylib.ec2._
+import deploylib.mesos._
+import edu.berkeley.cs.scads.comm._
+import edu.berkeley.cs.scads.config._
+import edu.berkeley.cs.scads.storage._
+import edu.berkeley.cs.avro.runtime._
+import edu.berkeley.cs.avro.marker._
+
+import org.apache.zookeeper.CreateMode
+
+import java.io.File
+import net.lag.logging.Logger
+
+class ExperimentalScadsCluster(root: ZooKeeperProxy#ZooKeeperNode) extends ScadsCluster(root) {
+  def blockUntilReady(clusterSize: Int): Unit = {
+    while(getAvailableServers.size < clusterSize) {
+      logger.info("Waiting for cluster to start " + cluster.getAvailableServers.size + " of " + clusterSize + " ready.")
+      Thread.sleep(1000)
+    }
+  }
+}
+
+trait ExperimentBase {
+  var resultClusterAddress = Config.config.getString("scads.perf.resultZooKeeperAddress").getOrElse(sys.error("need to specify scads.perf.resultZooKeeperAddress")) + "home/" + System.getenv("USER") + "/deploylib/"
+  val resultCluster = new ScadsCluster(ZooKeeperNode(resultClusterAddress))
+
+  implicit def productSeqToExcel(lines: Seq[Product]) = new {
+    import java.io._
+    def toExcel: Unit = {
+      val file = File.createTempFile("scadsOut", ".csv")
+      val writer = new FileWriter(file)
+
+      lines.map(_.productIterator.mkString(",") + "\n").foreach(writer.write)
+      writer.close
+
+      Runtime.getRuntime.exec(Array("/usr/bin/open", file.getCanonicalPath))
+    }
+  }
+}
+
+trait TaskBase {
+  def newScadsCluster(size: Int)(implicit cluster: Cluster, classSource: Seq[ClassSource]): ScadsCluster = {
+    val clusterRoot = cluster.zooKeeperRoot.getOrCreate("scads").createChild("experimentCluster", mode = CreateMode.PERSISTENT_SEQUENTIAL)
+    val serverProcs = Array.fill(size)(ScalaEngineTask(clusterAddress=clusterRoot.canonicalAddress).toJvmTask)
+
+    cluster.serviceScheduler.scheduleExperiment(serverProcs)
+    new ScadsCluster(clusterRoot)
+  }
+}
---
+++ consistency/src/main/scala/SimpleCrossDatacenterTest.scala
@@ -0,0 +1,129 @@
+package edu.berkeley.cs
+package scads
+package consistency
+
+import deploylib._
+import deploylib.mesos._
+import avro.marker._
+import comm._
+import config._
+import storage._
+
+import net.lag.logging.Logger
+
+import edu.berkeley.cs.scads.storage.FieldAnnotations._
+
+case class Result(var hostname: String,
+		  var timestamp: Long) extends AvroPair {
+  var rows: Int = _
+}
+
+object Experiment extends ExperimentBase {
+
+  def reset(implicit cluster: deploylib.mesos.Cluster, classSource: Seq[ClassSource]) = {
+    results.delete()
+    results.open()
+    cluster.restart
+  }
+
+  lazy val results = resultCluster.getNamespace[Result]("singleNodeResult")
+  def goodResults = results.iterateOverRange(None,None)
+
+
+  def main(args: Array[String]): Unit = {
+//     val cluster = TestScalaEngine.newScadsCluster(2)
+//     val task = new Task(cluster.root.canonicalAddress,
+//			  cluster.root.canonicalAddress).run()
+
+//    graphPoints.toSeq.sortBy(r => (r._2, r._1)).foreach(println)
+  }
+}
+
+case class KeyRec(var x: Int) extends AvroRecord
+
+case class ValueRec(var s: String,
+                    @FieldGT(1)
+                    @FieldGE(2)
+                    @FieldLT(3)
+                    @FieldLE(4)
+                    var i: Int,
+                    @FieldGT(1)
+                    @FieldGE(1)
+                    @FieldLT(4)
+                    @FieldLE(4)
+                    var a: Long,
+                    var b: Float,
+                    var c: Double) extends AvroRecord
+
+case class Task(var replicationFactor: Int = 2)
+     extends AvroTask with AvroRecord with TaskBase {
+  
+  var resultClusterAddress: String = _
+  var clusterAddress: String = _
+
+  def schedule(resultClusterAddress: String)(implicit cluster: deploylib.mesos.Cluster, classSource: Seq[ClassSource]): Unit = {
+    val scadsCluster = newScadsCluster(2)
+    clusterAddress = scadsCluster.root.canonicalAddress
+    this.resultClusterAddress = resultClusterAddress
+    val task = this.toJvmTask
+    cluster.serviceScheduler.scheduleExperiment(task :: Nil)
+  }
+
+  def run(): Unit = {
+    val logger = Logger()
+    val cluster = new ExperimentalScadsCluster(ZooKeeperNode(clusterAddress))
+    cluster.blockUntilReady(replicationFactor)
+
+    val resultCluster = new ScadsCluster(ZooKeeperNode(resultClusterAddress))
+    val results = resultCluster.getNamespace[Result]("singleNodeResult")
+
+
+    val ns = new SpecificNamespace[KeyRec, ValueRec]("testns", cluster, cluster.namespaces) with Transactions[KeyRec, ValueRec] {
+      override val protocolType = TxProtocol2pc()
+    }
+    ns.open()
+
+    ns.put(KeyRec(1), ValueRec("A", 1, 1, 1.0.floatValue, 1.0))
+    ns.put(KeyRec(2), ValueRec("B", 1, 1, 1.0.floatValue, 1.0))
+    ns.put(KeyRec(3), ValueRec("C", 1, 1, 1.0.floatValue, 1.0))
+    ns.put(KeyRec(4), ValueRec("D", 1, 1, 1.0.floatValue, 1.0))
+
+    val tx1 = new Tx(100) ({
+      List.range(5, 5 + 4).foreach(x => ns.put(KeyRec(x),
+                                               ValueRec("G", 1, 1, 1.0.floatValue, 1.0)))
+    }).Execute()
+
+    val tx2 = new Tx(100) ({
+      List.range(9, 9 + 4).foreach(x => ns.put(KeyRec(x),
+                                               ValueRec("H", 1, 1, 1.0.floatValue, 1.0)))
+    }).Execute()
+
+    val tx3 = new Tx(100) ({
+      List.range(7, 7 + 4).foreach(x => ns.put(KeyRec(x),
+                                               ValueRec("I", 1, 1, 1.0.floatValue, 1.0)))
+    }).Execute()
+
+    println("delete 1")
+    val tx4 = new Tx(100) ({
+      // need to read your writes...
+      ns.get(KeyRec(1))
+      ns.put(KeyRec(1), None)
+    }).Execute()
+
+    println("running some logical updates")
+    val tx5 = new Tx(100) ({
+      ns.putLogical(KeyRec(12), ValueRec("", 2, 3, 2.1.floatValue, 0.2))
+    })
+    tx5.Execute()
+    tx5.Execute()
+    tx5.Execute()
+    tx5.Execute()
+    tx5.Execute()
+    ns.getRange(None, None).foreach(x => println(x))
+
+    val hostname = java.net.InetAddress.getLocalHost.getHostName
+    val result = Result(hostname, System.currentTimeMillis)
+    result.rows = ns.getRange(None, None).size
+    results.put(result)
+  }
+}
