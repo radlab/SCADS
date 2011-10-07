bccf383691175209276c729b19a9a651dbb02730
Michael Armbrust <marmbrus@cs.berkeley.edu>
2011-10-06 17:43:23 -0700

    bump timeout (all tests passing!)

--- scalaengine/src/main/scala/client/QuorumProtocol.scala
+++ scalaengine/src/main/scala/client/QuorumProtocol.scala
@@ -132,7 +132,7 @@ trait QuorumProtocol
     val (servers, quorum) = writeQuorumForKey(key)
     val putRequest = PutRequest(key, value.map(createMetadata))
     val responses = serversForKey(key).map(_ !! putRequest)
-    responses.blockFor(quorum, 500, TimeUnit.MILLISECONDS)
+    responses.blockFor(quorum, 5000, TimeUnit.MILLISECONDS)
   }
 
   /* Buffers KV tuples until the average buffer size for all the servers is
