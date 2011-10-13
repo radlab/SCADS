240bdb1ebeb1c779a9d6a5978765f79c15c45273
Gene Pang <gpang@eecs.berkeley.edu>
2011-10-13 15:47:32 -0700

    interface and some implementation of LUB, GLB, subset, provedSafe.

--- scalaengine/src/main/scala/transactions/conflict/ConflictResolver.scala
+++ scalaengine/src/main/scala/transactions/conflict/ConflictResolver.scala
@@ -4,6 +4,7 @@ package conflict
 
 import actors.threadpool.ThreadPoolExecutor.AbortPolicy
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashSet
 
 import java.util.concurrent.ConcurrentHashMap
 import java.util.Arrays
@@ -19,8 +20,134 @@ import org.apache.avro.specific.{SpecificDatumWriter, SpecificDatumReader, Speci
 import org.apache.avro.Schema
 
 class ConflictResolver(val valueSchema: Schema, val ics: FieldICList) {
-//  def getLUB(cstructs: Array[CStruct]): CStruct
-//  def getGLB(cstructs: Array[CStruct]): CStruct
+  type CommandSets = ArrayBuffer[HashSet[CStructCommand]]
+
+  def getLUB(cstructs: Seq[CStruct]): CStruct = {
+    if (cstructs.length == 1) {
+      cstructs.head
+    } else {
+      val head = cstructs.head
+      val result = convertToSets(head.commands)
+
+      // Iterate over the rest of the cstructs.
+      cstructs.tail.foreach(x => {
+        updateMerge(result, convertToSets(x.commands), intersect=false)
+      })
+
+      CStruct(head.value, convertToSeq(result))
+    }
+  }
+
+  def getGLB(cstructs: Seq[CStruct]): CStruct = {
+    if (cstructs.length == 1) {
+      cstructs.head
+    } else {
+      val head = cstructs.head
+      val result = convertToSets(head.commands)
+
+      // Iterate over the rest of the cstructs.
+      cstructs.tail.foreach(x => {
+        updateMerge(result, convertToSets(x.commands))
+      })
+
+      CStruct(head.value, convertToSeq(result))
+    }
+  }
+
+  // Returns true if c1 is a subset of c2.  If strict is true,
+  // only returns true if c1 is a strict subset of c2.
+  def isSubset(c1: CStruct, c2: CStruct, strict: Boolean = false): Boolean = {
+    val cs1 = convertToSets(c1.commands)
+    val cs2 = convertToSets(c2.commands)
+    true
+  }
+
+  def isStrictSubset(cstruct1: CStruct, cstruct2: CStruct) = isSubset(cstruct1, cstruct2, true)
+
+  def provedSafe(cstructs: Seq[CStruct], fastQuorumSize: Int,
+                 classicQuorumSize: Int): CStruct = {
+    // TODO: does cstructs require fastQuorumSize number of elements??
+
+
+    cstructs.head
+  }
+
+  // Modifies first CommandSet to be merged with the second CommandSet.
+  // If intersect is true, computes the intersection.  Otherwise, computes the
+  // union.
+  private def updateMerge(result: CommandSets, c2: CommandSets,
+                          intersect: Boolean = true) = {
+    // Iterator for result.
+    var i = 0
+    // Iterator for c2.
+    var j = 0
+
+    while (i < result.length) {
+      val currentSet = result(i)
+      // Find the corresponding HashSet in c2.
+      val newSet = c2.indexWhere(!_.intersect(currentSet).isEmpty, j) match {
+        case -1 => {
+          if (intersect) {
+            currentSet.clear
+          }
+          currentSet
+        }
+        case x => {
+          if (intersect) {
+            currentSet.intersect(c2(x))
+          } else {
+            currentSet.union(c2(x))
+          }
+        }
+      }
+      // TODO: If there is an empty intersection, should it short circuit the
+      //       rest of the comparisons?
+      result.update(i, newSet)
+      i += 1
+    }
+  }
+
+  private def convertToSeq(c: CommandSets): Seq[CStructCommand] = {
+    val result = new ArrayBuffer[CStructCommand]
+    c.foreach(x => {
+      result.appendAll(x.toList)
+    })
+    result
+  }
+
+  private def convertToSets(c: Seq[CStructCommand]): CommandSets = {
+    val result = new CommandSets()
+    var isLogical = false
+    c.foreach(x => {
+      x.command match {
+        case up: LogicalUpdate => {
+          if (!isLogical) {
+            result.append(new HashSet[CStructCommand]())
+          }
+          result.last.add(x)
+          isLogical = true
+        }
+        case up: PhysicalUpdate => {
+          result.append(new HashSet[CStructCommand]())
+          result.last.add(x)
+          isLogical = false
+        }
+        case _ =>
+      }
+    })
+    result
+  }
+
+
+
+
+
+
+
+
+/***********************************************************************
+ **************** old code, will probably go away **********************
+ ********************************************************************* */
 
   def isCompatible(cstructs: Seq[CStruct]): Boolean = {
     if (cstructs.length <= 1) {
