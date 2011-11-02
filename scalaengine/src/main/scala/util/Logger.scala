68a9ce279da12a25d653607e64a6c546445bcd48
Tim Kraska <kraska@cs.berkeley.edu>
2011-11-02 11:11:40 -0700

    Better logging

---
+++ scalaengine/src/main/scala/util/Logger.scala
@@ -0,0 +1,15 @@
+package edu.berkeley.cs.scads.util
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tim
+ * Date: 11/2/11
+ * Time: 11:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object Logger {
+
+  def apply[T](cls : Class[T]) : net.lag.logging.Logger = net.lag.logging.Logger(cls.toString.replaceAll("class ", "") + ".t")
+
+}
