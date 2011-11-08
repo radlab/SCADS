bcd1af3fdb09d9976b33a9370d3cf6c94fdb3301
Gene Pang <gpang@eecs.berkeley.edu>
2011-11-07 21:39:14 -0800

    small changes to the test.

--- scalaengine/src/main/scala/transactions/TestTx.scala
+++ scalaengine/src/main/scala/transactions/TestTx.scala
@@ -40,11 +40,11 @@ class TestTx {
   def run() {
     val cluster = TestScalaEngine.newScadsCluster(4)
 
-    //val ns = new SpecificNamespace[KeyRec, ValueRec]("testns", cluster, cluster.namespaces) with Transactions[KeyRec, ValueRec] {
-    //  override lazy val protocolType = NSTxProtocolMDCC()
-    //}
-    //ns.open()
-    //ns.setPartitionScheme(List((None, cluster.getAvailableServers)))
+    val ns = new SpecificNamespace[KeyRec, ValueRec]("testns", cluster, cluster.namespaces) with Transactions[KeyRec, ValueRec] {
+     override lazy val protocolType = NSTxProtocol2pc()
+    }
+    ns.open()
+    ns.setPartitionScheme(List((None, cluster.getAvailableServers)))
 
     val nsPair = cluster.getNamespace[DataRecord]("testnsPair", NSTxProtocolMDCC())
     nsPair.setPartitionScheme(List((None, cluster.getAvailableServers)))
@@ -62,73 +62,79 @@ class TestTx {
         nsPair.put(dr)
       })
     }).Execute()
+
+    // Sleep for a little bit to wait for the commits.
+    Thread.sleep(1000)
+
     nsPair.getRange(None, None).foreach(x => println(x))
-    println("nsPair.getRecord: " + nsPair.getRecord(DataRecord(2)))
-//
-//    new Tx(100) ({
-//      ns.put(KeyRec(1), ValueRec("A", 1, 1, 1.0.floatValue, 1.0))
-//      ns.put(KeyRec(2), ValueRec("B", 1, 1, 1.0.floatValue, 1.0))
-//      ns.put(KeyRec(3), ValueRec("C", 1, 1, 1.0.floatValue, 1.0))
-//      ns.put(KeyRec(4), ValueRec("D", 1, 1, 1.0.floatValue, 1.0))
-//    }).Execute()
-//
-//    val tx1 = new Tx(100) ({
-//      List.range(5, 5 + 4).foreach(x => ns.put(KeyRec(x),
-//                                               ValueRec("G", 1, 1, 1.0.floatValue, 1.0)))
-//    }).Accept(0.90) {
-//    }.Commit( success => {
-//    })
-//    tx1.Execute()
-//
-//    val tx2 = new Tx(100) ({
-//      List.range(9, 9 + 4).foreach(x => ns.put(KeyRec(x),
-//                                               ValueRec("H", 1, 1, 1.0.floatValue, 1.0)))
-//    }).Accept(0.90) {
-//    }.Commit( success => {
-//    })
-//    tx2.Execute()
-//
-//    val tx3 = new Tx(100) ({
-//      List.range(7, 7 + 4).foreach(x => {
-//        ns.get(KeyRec(x))
-//        ns.put(KeyRec(x), ValueRec("I", 1, 1, 1.0.floatValue, 1.0))})
-//    }).Accept(0.90) {
-//    }.Commit( success => {
-//    })
-//    tx3.Execute()
-//
-//    // Delete something
-//    println("delete 1")
-//
-////    ns.put(KeyRec(1), None)
-//
-//    val tx4 = new Tx(100) ({
-//      // need to read your writes...
-//      ns.get(KeyRec(1))
-//      ns.put(KeyRec(1), None)
-//    }).Accept(0.90) {
-//    }.Commit( success => {
-//    })
-//    tx4.Execute()
-//
-//    println("    get 1")
-//    println(ns.get(KeyRec(1)))
-//    println("    get range")
-//    ns.getRange(None, None).foreach(x => println(x))
-//
-//    // Logical updates
-//    println("running some logical updates")
-//    val tx5 = new Tx(100) ({
-//      ns.putLogical(KeyRec(12), ValueRec("", 2, 3, 2.1.floatValue, 0.2))
-//    }).Accept(0.90) {
-//    }.Commit( success => {
-//    })
-//    tx5.Execute()
-//    tx5.Execute()
-//    tx5.Execute()
-//    tx5.Execute()
-//    tx5.Execute()
-//    ns.getRange(None, None).foreach(x => println(x))
+    println("nsPair.getRecord 3: " + nsPair.getRecord(DataRecord(3)))
+    println("nsPair.getRecord 4: " + nsPair.getRecord(DataRecord(4)))
+/*
+   new Tx(100) ({
+     ns.put(KeyRec(1), ValueRec("A", 1, 1, 1.0.floatValue, 1.0))
+     ns.put(KeyRec(2), ValueRec("B", 1, 1, 1.0.floatValue, 1.0))
+     ns.put(KeyRec(3), ValueRec("C", 1, 1, 1.0.floatValue, 1.0))
+     ns.put(KeyRec(4), ValueRec("D", 1, 1, 1.0.floatValue, 1.0))
+   }).Execute()
+
+   val tx1 = new Tx(100) ({
+     List.range(5, 5 + 4).foreach(x => ns.put(KeyRec(x),
+                                              ValueRec("G", 1, 1, 1.0.floatValue, 1.0)))
+   }).Accept(0.90) {
+   }.Commit( success => {
+   })
+   tx1.Execute()
+
+   val tx2 = new Tx(100) ({
+     List.range(9, 9 + 4).foreach(x => ns.put(KeyRec(x),
+                                              ValueRec("H", 1, 1, 1.0.floatValue, 1.0)))
+   }).Accept(0.90) {
+   }.Commit( success => {
+   })
+   tx2.Execute()
+
+   val tx3 = new Tx(100) ({
+     List.range(7, 7 + 4).foreach(x => {
+       ns.get(KeyRec(x))
+       ns.put(KeyRec(x), ValueRec("I", 1, 1, 1.0.floatValue, 1.0))})
+   }).Accept(0.90) {
+   }.Commit( success => {
+   })
+   tx3.Execute()
+
+   // Delete something
+   println("delete 1")
+
+   ns.put(KeyRec(1), None)
+
+   val tx4 = new Tx(100) ({
+     // need to read your writes...
+     ns.get(KeyRec(1))
+     ns.put(KeyRec(1), None)
+   }).Accept(0.90) {
+   }.Commit( success => {
+   })
+   tx4.Execute()
+
+   println("    get 1")
+   println(ns.get(KeyRec(1)))
+   println("    get range")
+   ns.getRange(None, None).foreach(x => println(x))
+
+   // Logical updates
+   println("running some logical updates")
+   val tx5 = new Tx(100) ({
+     ns.putLogical(KeyRec(12), ValueRec("", 2, 3, 2.1.floatValue, 0.2))
+   }).Accept(0.90) {
+   }.Commit( success => {
+   })
+   tx5.Execute()
+   tx5.Execute()
+   tx5.Execute()
+   tx5.Execute()
+   tx5.Execute()
+   ns.getRange(None, None).foreach(x => println(x))
+*/
   }
 }
 
