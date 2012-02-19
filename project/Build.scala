31a5b1ff6c4169f38a816f084030a5ae04539fc9
Eric Liang <ericliang@berkeley.edu>
2012-02-18 20:28:01 -0800

    use AWS_DEFAULT_REGION env var for implicit cluster again

--- project/Build.scala
+++ project/Build.scala
@@ -281,7 +281,7 @@ object DeployConsole extends BuildCommon {
           "import deploylib.mesos._",
           "val allJars = " + allJars.map(f => "new java.io.File(\"%s\")".format(f.getCanonicalPath)).mkString("Seq(", ",", ")"),
           "deploylib.mesos.MesosCluster.jarFiles = allJars",
-          "implicit val cluster = new Cluster(USEast1)",
+          "implicit val cluster = new Cluster()", // defaults to USEast1 unless overriden by AWS_DEFAULT_REGION env variable
           "val clusterWest = new Cluster(USWest1)",
           "implicit def zooKeeperRoot = cluster.zooKeeperRoot",
           "implicit def classSource = cluster.classSource",
