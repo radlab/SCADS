4fba1125b0e548f3c746c5598c919cda1d4786aa
Gene Pang <gpang@eecs.berkeley.edu>
2012-09-12 00:27:03 -0700

    maybe futures make creating partitions faster.

--- scalaengine/src/main/scala/client/Routable.scala
+++ scalaengine/src/main/scala/client/Routable.scala
@@ -10,6 +10,9 @@ import java.util.{ Arrays => JArrays }
 import org.apache.avro._
 import io._
 
+import scala.actors.Future
+import scala.actors.Futures._
+
 object DefaultKeyRoutableLike {
   val ZOOKEEPER_ROUTING_TABLE = "routingtable"
   val ZOOKEEPER_PARTITION_ID = "partitionid"
@@ -150,7 +153,8 @@ trait DefaultKeyRoutableLike
 
   private def createPartitions(startKey: Option[Array[Byte]], endKey: Option[Array[Byte]], servers: Seq[StorageService]): Seq[PartitionService] = {
     val createReq = CreatePartitionRequest(name, partitionType, startKey, endKey, getTxProtocolType)
-    waitForAndThrowException(servers.map(server => (server !! createReq, server)), timeout=timeoutCreatePartition) {
+//    waitForAndThrowException(servers.map(server => (server !! createReq, server)), timeout=timeoutCreatePartition) {
+    waitForAndThrowException(servers.map(server => future { (server !! createReq, server)}).map(_()), timeout=timeoutCreatePartition) {
       case (CreatePartitionResponse(partitionActor), _) => partitionActor
     }
   }
