6c84d3f17cabe88e440d0f22ea368e47de2c0434
Eric Liang <ericliang@berkeley.edu>
2012-09-18 02:57:53 -0700

    Add additional logging to view manager

--- scalaengine/src/main/scala/client/ViewManager.scala
+++ scalaengine/src/main/scala/client/ViewManager.scala
@@ -13,8 +13,10 @@ import scala.collection.mutable.LinkedHashMap
 import edu.berkeley.cs.scads.comm._
 import edu.berkeley.cs.avro.marker._
 import org.apache.avro.util.Utf8
+import net.lag.logging.Logger
 
 trait ViewManager[BulkType <: AvroPair] extends RangeKeyValueStoreLike[IndexedRecord, IndexedRecord, BulkType] {
+  private val logging = Logger("edu.berkeley.cs.scads.storage.client.index.ViewManager")
 
   type ViewDelta = (IndexedRecord) => Seq[IndexedRecord]
 
@@ -70,12 +72,20 @@ trait ViewManager[BulkType <: AvroPair] extends RangeKeyValueStoreLike[IndexedRe
     // but we need to update views serially in the general case.
     // TODO future optimization: insert barriers to parallelize safely
     for ((view, delta) <- rules) {
+      var deltaTime = 0.0
+      val start = System.currentTimeMillis
       for (t <- records) {
-        for (u <- delta(t)) {
+        val deltaStart = System.currentTimeMillis
+        val tmp = delta(t, -1) // TODO insert the right stripe number
+        deltaTime += System.currentTimeMillis - deltaStart
+        for (u <- tmp) {
           view.putBulkBytes(view.keyToBytes(u), valueBytes)
         }
       }
       view.flushBulkBytes
+      val writeTime = System.currentTimeMillis - start - deltaTime
+      logging.info("delta_read_time=" + deltaTime + "," +
+                   "view_write_time=" + writeTime)
     }
   }
 
