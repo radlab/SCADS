8cd6682326330020c8c92582829965cd307205c1
Eric Liang <ericliang@berkeley.edu>
2012-10-12 00:17:43 -0700

    remove hack that reordered index attributes

--- piql/src/main/scala/QueryPlan.scala
+++ piql/src/main/scala/QueryPlan.scala
@@ -174,8 +174,7 @@ case class ScadsRelation(ns: IndexedNamespace, alias: Option[String] = None) ext
   def index(attrs: Seq[QualifiedAttributeValue]): TupleProvider = {
     val remainingKeyFields = keyAttributes.filterNot(attrs contains _)
     val a = attrs ++ remainingKeyFields
-    // Attributes are sorted by field name so the special _stripe field occurs first.
-    val idx = ns.getOrCreateIndex(attrs.sortBy(_.field.name).map(a => AttributeIndex(a.fieldName)))
+    val idx = ns.getOrCreateIndex(attrs.map(a => AttributeIndex(a.fieldName)))
     ScadsIndex(idx)
   }
 }
