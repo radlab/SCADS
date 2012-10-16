57a34ae4dcfa1dba6c228b350ee38a1298678855
Michael Armbrust <marmbrus@cs.berkeley.edu>
2012-10-16 16:47:04 -0700

    allow deactivation of tiggers / override asyncPut

--- scalaengine/src/main/scala/client/TriggerManager.scala
+++ scalaengine/src/main/scala/client/TriggerManager.scala
@@ -9,6 +9,7 @@ import avro.runtime._
 import avro.marker.AvroPair
 import org.apache.avro.generic.IndexedRecord
 import storage.PairNamespace
+import comm.ScadsFuture
 
 /**
  * Simple support for calling user defined trigger functions when records are added or deleted.
@@ -21,33 +22,66 @@ trait TriggerManager[PairType <: AvroPair] extends RangeKeyValueStoreLike[Indexe
 
   var addTriggers = List.empty[AddTriggerFunction]
   var deleteTriggers = List.empty[DeleteTriggerFunction]
+  var triggersActive = true
 
   override abstract def put(key: IndexedRecord, value: Option[IndexedRecord]): Unit = {
-    // put/maintain order is important for self-joins
-    value match {
-      case None =>
-        deleteTriggers.foreach(_(Seq(key)))
-        super.put(key, value)
-      case Some(v) =>
-        /* OH GOD WHY?!?! This isn't very efficient, but I'm not really sure how to do this more elegantly */
-        /* Clearly if you are using the single put interface you don't care about efficiency though... right? */
-        val rec = pairManifest.erasure.newInstance().asInstanceOf[PairType]
-        rec.key.parse(key.toBytes)
-        rec.value.parse(v.toBytes)
-        addTriggers.foreach(_(Seq(rec)))
-        super.put(key, value)
-    }
+    if(triggersActive)
+      // put/maintain order is important for self-joins
+      value match {
+        case None =>
+          deleteTriggers.foreach(_(Seq(key)))
+          super.put(key, value)
+        case Some(v) =>
+          /* OH GOD WHY?!?! This isn't very efficient, but I'm not really sure how to do this more elegantly */
+          /* Clearly if you are using the single put interface you don't care about efficiency though... right? */
+          val rec = pairManifest.erasure.newInstance().asInstanceOf[PairType]
+          rec.key.parse(key.toBytes)
+          rec.value.parse(v.toBytes)
+          addTriggers.foreach(_(Seq(rec)))
+          super.put(key, value)
+      }
+    else
+      super.put(key, value)
+  }
+
+  override abstract def asyncPut(key: IndexedRecord, value: Option[IndexedRecord]): ScadsFuture[Unit] = {
+    if(triggersActive)
+      // put/maintain order is important for self-joins
+      value match {
+        case None =>
+          deleteTriggers.foreach(_(Seq(key)))
+          super.asyncPut(key, value)
+        case Some(v) =>
+          /* OH GOD WHY?!?! This isn't very efficient, but I'm not really sure how to do this more elegantly */
+          /* Clearly if you are using the single put interface you don't care about efficiency though... right? */
+          val rec = pairManifest.erasure.newInstance().asInstanceOf[PairType]
+          rec.key.parse(key.toBytes)
+          rec.value.parse(v.toBytes)
+          addTriggers.foreach(_(Seq(rec)))
+          super.asyncPut(key, value)
+      }
+    else
+      super.asyncPut(key, value)
   }
 
   override abstract def ++=(that: TraversableOnce[PairType]): Unit = {
-    val traversable = that.toBuffer
-    super.++=(traversable)
-    addTriggers.foreach(_(traversable))
+    if(triggersActive) {
+      val traversable = that.toBuffer
+      super.++=(traversable)
+      addTriggers.foreach(_(traversable))
+    }
+    else
+      super.++=(that)
+
   }
 
   override abstract def --=(that: TraversableOnce[PairType]): Unit = {
-    val traversable = that.toBuffer
-    deleteTriggers.foreach(_(traversable.map(_.key)))
-    super.--=(traversable)
+    if(triggersActive) {
+      val traversable = that.toBuffer
+      deleteTriggers.foreach(_(traversable.map(_.key)))
+      super.--=(traversable)
+    }
+    else
+      super.--=(that)
   }
 }
